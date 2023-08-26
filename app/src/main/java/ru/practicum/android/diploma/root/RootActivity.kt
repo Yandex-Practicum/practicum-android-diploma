b6c77e5b00e597892616fccf15d896b77da03db5
OlegKostinDS <olegkostinds@gmail.com>
2023-08-26 16:49:19 +0300

    fixed mirror bugs

--- app/src/main/java/ru/practicum/android/diploma/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/RootActivity.kt
@@ -3,19 +3,26 @@ package ru.practicum.android.diploma.root
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import ru.practicum.android.diploma.BuildConfig
+import ru.practicum.android.diploma.LoggerImpl
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
+import ru.practicum.android.diploma.util.thisName
 
 class RootActivity : AppCompatActivity() {
-    private var _binding: ActivityRootBinding? = null
-    val binding get() = _binding!!
+    private val binding by lazy{ActivityRootBinding.inflate(layoutInflater)}
+
+    private val logger = LoggerImpl()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
-        val navController = findNavController(R.id.rootFragmentContainerView)
+        setContentView(binding.root)
+        logger.log(thisName,"onCreate()")
 
-
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
     }
