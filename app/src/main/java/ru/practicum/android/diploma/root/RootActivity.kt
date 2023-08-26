ce2fa4215dfb4672fc2fd2cf088359f330501504
OlegKostinDS <olegkostinds@gmail.com>
2023-08-26 16:05:46 +0300

    added navController and navHostFragment

--- app/src/main/java/ru/practicum/android/diploma/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/RootActivity.kt
@@ -2,13 +2,19 @@ package ru.practicum.android.diploma.root
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+    private var _binding: ActivityRootBinding? = null
+    val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
+        val navController = findNavController(R.id.rootFragmentContainerView)
+
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
