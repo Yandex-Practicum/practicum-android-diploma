b879493b7e4fc2f80761d29231901ce62a0480be
mvrlrd <mvrlrd@yandex.ru>
2023-08-26 16:12:50 +0300

    marked LoggerImpl as Inject

--- app/src/debug/java/ru/practicum/android/diploma/LoggerImpl.kt
+++ app/src/debug/java/ru/practicum/android/diploma/LoggerImpl.kt
@@ -1,8 +1,9 @@
 package ru.practicum.android.diploma
 
 import android.util.Log
+import javax.inject.Inject
 
-class LoggerImpl: Logger {
+class LoggerImpl @Inject constructor(): Logger {
     override fun log(className: String, method: String) {
         Log.d("debug", "$className -> $method")
     }
