f7b06ac666da13c4cf7157be161f2d4c4609fbc6
pogonat <aimagenta81@gmail.com>
2023-08-27 03:44:42 +0600

    GH-9: create interfaces for network

---
+++ app/src/main/java/ru/practicum/android/diploma/root/data/network/HeadHunterApi.kt
@@ -0,0 +1,9 @@
+package ru.practicum.android.diploma.root.data.network
+
+interface HeadHunterApi {
+
+// waiting for token. далее использовать в формате Authorization: Bearer ACCESS_TOKEN,
+// передавая токен в методы текущего класса в качестве первого параметра в формате
+//    (@Header("Authorization") token: String, ...)
+
+}
