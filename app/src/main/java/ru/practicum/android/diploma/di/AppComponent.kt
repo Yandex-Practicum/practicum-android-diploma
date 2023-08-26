75539123b9f1609841404993be534d3071487503
mvrlrd <mvrlrd@yandex.ru>
2023-08-26 18:54:15 +0300

    added dagger factory with context

--- app/src/main/java/ru/practicum/android/diploma/di/AppComponent.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/AppComponent.kt
@@ -1,9 +1,21 @@
 package ru.practicum.android.diploma.di
 
+import android.content.Context
+import dagger.BindsInstance
 import dagger.Component
+import dagger.Provides
+import ru.practicum.android.diploma.Logger
 import ru.practicum.android.diploma.app.App
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [LoggerModule::class])
-abstract class AppComponent {
-    abstract fun inject(app: App)
+interface AppComponent {
+    fun inject(app: App)
+     fun provideLogger(): Logger
+
+    @Component.Factory
+    interface AppComponentFactory{
+        fun create(@BindsInstance context: Context): AppComponent
+    }
 }
