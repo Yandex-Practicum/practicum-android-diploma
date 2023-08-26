b0ed19b783413d450d1fc8c66efe0ae6a688fc3c
mvrlrd <mvrlrd@yandex.ru>
2023-08-26 16:13:45 +0300

    injected logger to App with component

--- app/src/main/java/ru/practicum/android/diploma/app/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/app/App.kt
@@ -1,13 +1,21 @@
 package ru.practicum.android.diploma.app
 
 import android.app.Application
+import ru.practicum.android.diploma.Logger
 import ru.practicum.android.diploma.LoggerImpl
+import ru.practicum.android.diploma.di.DaggerAppComponent
 import ru.practicum.android.diploma.util.thisName
+import javax.inject.Inject
 
 class App: Application() {
 
-    private val logger = LoggerImpl()
+    @Inject
+    lateinit var logger: Logger
+    val component by lazy {
+        DaggerAppComponent.create()
+    }
     override fun onCreate() {
+        component.inject(this)
         super.onCreate()
         logger.log(thisName, "onCreate()")
 
