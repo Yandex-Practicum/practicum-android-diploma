6a597c344e6b5fd2bd0712632fa61ec51cf05491
mvrlrd <mvrlrd@yandex.ru>
2023-08-26 20:39:24 +0300

    Provider<viewModel> as a value. this for getting a new instance of viewModel each time it creates

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelFactory.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelFactory.kt
@@ -3,12 +3,14 @@ package ru.practicum.android.diploma.di
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import javax.inject.Inject
+import javax.inject.Provider
 
+@ApplicationScope
 class ViewModelFactory @Inject constructor(
-    private val viewModels: @JvmSuppressWildcards Map<String, ViewModel>
+    private val viewModelProviders: @JvmSuppressWildcards Map<String, Provider<ViewModel>>
 ) : ViewModelProvider.Factory{
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return viewModels[modelClass.simpleName] as T
+        return viewModelProviders[modelClass.simpleName]?.get() as T
     }
 }
