8ab8076d7b9c5848d091e1be28336d73e0cef4b6
mvrlrd <mvrlrd@yandex.ru>
2023-08-27 09:43:05 +0300

    some di's refactoring in FavoriteFragment.kt

--- app/src/main/java/ru/practicum/android/diploma/favorite/ui/FavoriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorite/ui/FavoriteFragment.kt
@@ -1,41 +1,32 @@
 package ru.practicum.android.diploma.favorite.ui
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import ru.practicum.android.diploma.Logger
 import ru.practicum.android.diploma.R
-import ru.practicum.android.diploma.app.App
 import ru.practicum.android.diploma.databinding.FragmentFavoriteBinding
-import ru.practicum.android.diploma.di.ViewModelFactory
+import ru.practicum.android.diploma.root.RootActivity
+import ru.practicum.android.diploma.util.thisName
 import ru.practicum.android.diploma.util.viewBinding
 import javax.inject.Inject
 
 class FavoriteFragment : Fragment(R.layout.fragment_favorite) {
-
-    private val component by lazy {
-        (requireActivity().application as App).component
-            .activityComponentFactory()
-            .create()
-    }
-    @Inject
-    lateinit var viewModelFactory: ViewModelFactory
-
     @Inject
     lateinit var logger: Logger
-
-    private val viewModel by lazy {
-        ViewModelProvider(this, viewModelFactory)[FavoriteViewModel::class.java]
-    }
-
+    private val viewModel: FavoriteViewModel by viewModels { (activity as RootActivity).viewModelFactory }
     private val binding by viewBinding<FragmentFavoriteBinding>()
 
-
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (activity as RootActivity).component.inject(this)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        component.inject(this)
         super.onViewCreated(view, savedInstanceState)
+        logger.log(thisName, "onViewCreated   $viewModel")
     }
 
 }
