913ce58237649552d4518def0e941d72a3ee514c
OlegKostinDS <olegkostinds@gmail.com>
2023-08-27 15:14:44 +0300

    made layout

---
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/CountryFilterFragment.kt
@@ -0,0 +1,18 @@
+package ru.practicum.android.diploma.filter.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentCountryFilterBinding
+import ru.practicum.android.diploma.util.viewBinding
+
+
+class CountryFilterFragment : Fragment(R.layout.fragment_country_filter) {
+    private val binding by viewBinding<FragmentCountryFilterBinding>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+    }
