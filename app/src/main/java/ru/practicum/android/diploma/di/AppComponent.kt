210db7d55a7658e4443ebd9e6780ac13f26e2d3a
OlegKostinDS <olegkostinds@gmail.com>
2023-08-27 16:02:15 +0300

    added placeholder container

--- app/src/main/java/ru/practicum/android/diploma/di/AppComponent.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/AppComponent.kt
@@ -5,15 +5,22 @@ import dagger.BindsInstance
 import dagger.Component
 import ru.practicum.android.diploma.app.App
 import ru.practicum.android.diploma.di.annotations.ApplicationScope
+import ru.practicum.android.diploma.di.annotations.BaseUrl
 import ru.practicum.android.diploma.di.modules.LoggerModule
+import ru.practicum.android.diploma.search.di.SearchDataModule
+import ru.practicum.android.diploma.search.di.SearchDomainModule
 
 @ApplicationScope
-@Component(modules = [LoggerModule::class])
+@Component(modules = [LoggerModule::class, SearchDataModule::class, SearchDomainModule::class])
 interface AppComponent {
     fun inject(app: App)
     fun activityComponentFactory(): ActivityComponent.Factory
+
     @Component.Factory
-    interface AppComponentFactory{
-      fun create(@BindsInstance context: Context): AppComponent
+    interface AppComponentFactory {
+        fun create(
+            @BindsInstance context: Context,
+            @BindsInstance @BaseUrl baseUrl: String
+        ): AppComponent
     }
 }
---
+++ app/src/main/java/ru/practicum/android/diploma/search/di/SearchDataModule.kt
@@ -0,0 +1,59 @@
+package ru.practicum.android.diploma.search.di
+
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.di.annotations.ApplicationScope
+import ru.practicum.android.diploma.di.annotations.BaseUrl
+import ru.practicum.android.diploma.search.data.network.HhApiService
+import ru.practicum.android.diploma.search.data.network.NetworkClient
+import ru.practicum.android.diploma.search.data.network.RetrofitClient
+
+@Module
+class SearchDataModule {
+    @ApplicationScope
+    @Provides
+    fun createApiService(retrofit: Retrofit): HhApiService {
+        return retrofit.create(HhApiService::class.java)
+    }
+
+    @Provides
+    fun bindNetworkClient(retrofitClient: RetrofitClient): NetworkClient{
+        return retrofitClient
+    }
+
+    @Provides
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory,
+        @BaseUrl baseUrl: String
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(gsonConverterFactory)
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    fun provideConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+}
+
+
