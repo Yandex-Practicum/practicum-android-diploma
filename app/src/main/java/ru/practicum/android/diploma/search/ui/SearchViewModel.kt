610db41e7c9f9f754e04a393a41f640f250ca27e
mvrlrd <mvrlrd@yandex.ru>
2023-08-27 12:18:59 +0300

    add SearchViewModel.kt

---
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/SearchViewModel.kt
@@ -0,0 +1,8 @@
+package ru.practicum.android.diploma.search.ui
+
+import ru.practicum.android.diploma.Logger
+import ru.practicum.android.diploma.root.BaseViewModel
+import javax.inject.Inject
+
+class SearchViewModel @Inject constructor(logger: Logger): BaseViewModel(logger) {
+}
