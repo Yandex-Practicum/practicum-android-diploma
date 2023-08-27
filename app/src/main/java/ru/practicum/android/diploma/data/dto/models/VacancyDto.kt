842dffa0c44642543f97a395bf961d964560ef1d
Дмитрий Харакшинов <d.kharakshinov@mail.ru>
2023-08-27 21:25:59 +0300

    Обновил сокращенную модель Вакансии

--- app/src/main/java/ru/practicum/android/diploma/data/dto/models/VacancyDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/models/VacancyDto.kt
@@ -3,7 +3,9 @@ package ru.practicum.android.diploma.data.dto.models
 data class VacancyDto (
     val id: String,
     val name: String,
+    val city: String?,
     val employerName: String,
+    val employerLogoUrl: String?,
     val salaryCurrency: String?,
     val salaryFrom: Int?,
     val salaryTo: Int?,
