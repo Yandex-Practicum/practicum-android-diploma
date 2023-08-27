92100c8a25e14f844a34785c6d3af14ba5c7d771
Aleksey Putilov <putilov0407@gmail.com>
2023-08-27 18:11:25 +0500

    add Retrofit

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -2,7 +2,19 @@ package ru.practicum.android.diploma.di
 
 import com.google.gson.Gson
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val dataModule = module {
     factory { Gson() }
+
+    //TODO "add name interface Api":
+    // single<iTunesSearchAPITO> {
+        Retrofit.Builder()
+            .baseUrl("https://itunes.apple.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    //TODO "add name interface Api":
+    // .create(iTunesSearchAPI::class.java)
+    // }
 }
