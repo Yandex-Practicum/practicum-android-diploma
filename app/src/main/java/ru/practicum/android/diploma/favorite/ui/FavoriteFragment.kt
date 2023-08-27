f861431e26f6291bc36fce1148aaf7fec6779d27
mvrlrd <mvrlrd@yandex.ru>
2023-08-27 09:46:51 +0300

    put log logic into viewmodel from FavoriteFragment.kt

--- app/src/main/java/ru/practicum/android/diploma/favorite/ui/FavoriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorite/ui/FavoriteFragment.kt
@@ -5,17 +5,13 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.fragment.app.viewModels
-import ru.practicum.android.diploma.Logger
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFavoriteBinding
 import ru.practicum.android.diploma.root.RootActivity
 import ru.practicum.android.diploma.util.thisName
 import ru.practicum.android.diploma.util.viewBinding
-import javax.inject.Inject
 
 class FavoriteFragment : Fragment(R.layout.fragment_favorite) {
-    @Inject
-    lateinit var logger: Logger
     private val viewModel: FavoriteViewModel by viewModels { (activity as RootActivity).viewModelFactory }
     private val binding by viewBinding<FragmentFavoriteBinding>()
 
@@ -26,7 +22,6 @@ class FavoriteFragment : Fragment(R.layout.fragment_favorite) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        logger.log(thisName, "onViewCreated   $viewModel")
+        viewModel.log(thisName, "onViewCreated   $viewModel")
     }
-
 }
--- app/src/main/java/ru/practicum/android/diploma/favorite/ui/FavoriteViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorite/ui/FavoriteViewModel.kt
@@ -5,5 +5,7 @@ import ru.practicum.android.diploma.Logger
 import javax.inject.Inject
 
 class FavoriteViewModel @Inject constructor(private val logger: Logger) : ViewModel() {
-
+    fun log(name: String, text: String){
+        logger.log(name, text)
+    }
 }
