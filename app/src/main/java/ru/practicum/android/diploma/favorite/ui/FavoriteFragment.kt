eb9278e0a3f4d34eeaa65bf1280673f65560cdfb
mvrlrd <mvrlrd@yandex.ru>
2023-08-27 18:48:31 +0300

    inherit DebouncingFragment by FavoriteFragment.kt

--- app/src/main/java/ru/practicum/android/diploma/favorite/ui/FavoriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorite/ui/FavoriteFragment.kt
@@ -3,18 +3,17 @@ package ru.practicum.android.diploma.favorite.ui
 import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFavoriteBinding
+import ru.practicum.android.diploma.root.DebouncingFragment
 import ru.practicum.android.diploma.root.RootActivity
 import ru.practicum.android.diploma.search.ui.SearchAdapter
 import ru.practicum.android.diploma.util.thisName
 import ru.practicum.android.diploma.util.viewBinding
-import javax.inject.Inject
 
-class FavoriteFragment : Fragment(R.layout.fragment_favorite) {
+class FavoriteFragment : DebouncingFragment(R.layout.fragment_favorite) {
 
     //@Inject
     var vacancyAdapter: SearchAdapter? = null
