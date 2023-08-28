39ab56e78823edc16beac6fe9babbc978cf77b35
mvrlrd <mvrlrd@yandex.ru>
2023-08-28 13:26:26 +0300

    remove detailsviewmodel from favoritesfragment

--- app/src/main/java/ru/practicum/android/diploma/favorite/ui/FavoriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorite/ui/FavoriteFragment.kt
@@ -1,53 +1,34 @@
 package ru.practicum.android.diploma.favorite.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFavoriteBinding
-import ru.practicum.android.diploma.details.ui.DetailsViewModel
 import ru.practicum.android.diploma.root.DebouncingFragment
 import ru.practicum.android.diploma.root.RootActivity
-import ru.practicum.android.diploma.search.domain.Vacancy
 import ru.practicum.android.diploma.search.ui.SearchAdapter
 import ru.practicum.android.diploma.util.thisName
 import ru.practicum.android.diploma.util.viewBinding
 
 class FavoriteFragment : DebouncingFragment(R.layout.fragment_favorite) {
 
-    //@Inject
-    var vacancyAdapter: SearchAdapter? = null
+    private var vacancyAdapter: SearchAdapter? = null
     private val viewModel: FavoriteViewModel by viewModels { (activity as RootActivity).viewModelFactory }
     private val binding by viewBinding<FragmentFavoriteBinding>()
-    private val detailsViewModel : DetailsViewModel by viewModels { (activity as RootActivity).viewModelFactory }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (activity as RootActivity).component.inject(this)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        (activity as RootActivity).component.inject(this)
         viewModel.log(thisName, "onViewCreated()")
-
         binding.recycler.adapter = vacancyAdapter
-//        detailsViewModel.addToFavorites(Vacancy(11L,"url","android-developer", "yandex","3000$",123L))
-//        detailsViewModel.addToFavorites(Vacancy(12L,"url","java-developer", "google","3000$",123L))
-//        detailsViewModel.addToFavorites(Vacancy(14L,"url","python-developer", "tesla","3000$",123L))
-//        detailsViewModel.addToFavorites(Vacancy(15L,"url","kotlin-developer", "tinkoff","3000$",123L))
-//        detailsViewModel.deleteVacancy(14L)
-
-        detailsViewModel.favs
-            .onEach {
-                viewModel.log(thisName,"______")
-                it.forEach {
-                viewModel.log(thisName, "vac = ${it.title}")
-            } }
-            .launchIn(viewLifecycleOwner.lifecycleScope)
-
-
         initListeners()
     }
 
