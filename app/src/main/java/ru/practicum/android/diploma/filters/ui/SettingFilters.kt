defe2d4f39bed3e08d41e765488f8d713b8b0315
MajoritySky2496 <gzag2496@gmail.com>
2023-08-28 12:48:43 +0500

    filters#23  Создал фрагмент "SettingFiltres"  Добавил зависимоть ViewModel  Создал viewModel "FiltresViewModel"  Создал layout setting_filtres  Добавил экран в Navigation

---
+++ app/src/main/java/ru/practicum/android/diploma/filters/ui/SettingFilters.kt
@@ -0,0 +1,27 @@
+package ru.practicum.android.diploma.filters.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.databinding.FragmentSettingFiltersBinding
+import ru.practicum.android.diploma.filters.presentation.FiltersViewModel
+import ru.practicum.android.diploma.util.BindingFragment
+
+class SettingFilters:BindingFragment<FragmentSettingFiltersBinding>() {
+
+    val viewModel by viewModel<FiltersViewModel>()
+
+
+    override fun createBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentSettingFiltersBinding {
+        return FragmentSettingFiltersBinding.inflate(layoutInflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
