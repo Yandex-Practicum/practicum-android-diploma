64687b82dbbad1c77c0a6797befd62dd4055fe94
mvrlrd <mvrlrd@yandex.ru>
2023-08-29 23:47:21 +0300

    add getFullInfo method in repository

--- app/src/main/java/ru/practicum/android/diploma/details/domain/DetailsRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/details/domain/DetailsRepository.kt
@@ -1,9 +1,12 @@
 package ru.practicum.android.diploma.details.domain
 
 import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.search.domain.models.FetchResult
 import ru.practicum.android.diploma.search.domain.models.Vacancy
 
 interface DetailsRepository {
     suspend fun removeVacancyFromFavorite(id: Long): Flow<Int>
     suspend fun addVacancyToFavorite(vacancy: Vacancy): Flow<Long>
+    suspend fun getFullVacancyInfo(id: Long): Flow<FetchResult>
+
 }
