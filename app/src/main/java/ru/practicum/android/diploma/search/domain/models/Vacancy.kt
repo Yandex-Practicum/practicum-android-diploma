5d4d3758a1a4ac0258661642da16284951f71323
mvrlrd <mvrlrd@yandex.ru>
2023-08-29 18:35:32 +0300

    add methods to convert tracks to vacancy to test app

--- app/src/main/java/ru/practicum/android/diploma/search/domain/models/Vacancy.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/models/Vacancy.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma.search.domain.models
 
-import kotlin.random.Random
+import ru.practicum.android.diploma.di.annotations.TestClass
+import ru.practicum.android.diploma.search.data.network.test.TrackDto
 
 data class Vacancy(
     val id: Long,
@@ -10,6 +11,21 @@ data class Vacancy(
     val salary: String = "",
     val date : Long = 0L
 )
-
+@TestClass
+fun TrackDto.toVacancy(): Vacancy{
+    return with(this) {
+        Vacancy(id = trackId,
+            iconUri = image?:"",
+            title = trackName?:"",
+            company = artistName?:"",
+            salary = year.toString()?:"",
+            date = 0L
+            )
+    }
+}
+@TestClass
+fun mapTracksToVacancies(tracks: List<TrackDto>): List<Vacancy>{
+    return tracks.map { it.toVacancy() }
+}
 
 
