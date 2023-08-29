bbae96a8b3679a0430b0e442d3aa5240f0622047
mvrlrd <mvrlrd@yandex.ru>
2023-08-29 18:30:31 +0300

    do real request in retrofit client with mapping track into vacancy

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/RetrofitClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/RetrofitClient.kt
@@ -1,15 +1,18 @@
 package ru.practicum.android.diploma.search.data.network
 
 import android.util.Log
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import ru.practicum.android.diploma.search.domain.models.FetchResult
+import ru.practicum.android.diploma.search.domain.models.mapTracksToVacancies
 import javax.inject.Inject
 
 class RetrofitClient @Inject constructor(
     private val hhApiService: HhApiService,
     private val internetController: InternetController
 ) : NetworkClient {
-    override suspend fun doRequest(any: Any): Response {
+    override suspend fun doRequest(any: Any): Flow<FetchResult> {
         val response = hhApiService.search(any as String)
-        Log.e("TAG", "doRequest: ${response.results}", )
-        return Response()
+        return flowOf(FetchResult.Success(data = mapTracksToVacancies(response.results.toList())))
     }
 }
