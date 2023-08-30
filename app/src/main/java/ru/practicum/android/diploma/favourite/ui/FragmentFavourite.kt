bb4b56119eafe8ce389fe08d74b790b231de4fe3
Aleksey Putilov <putilov0407@gmail.com>
2023-08-30 06:56:17 +0500

    corrected the comments

--- app/src/main/java/ru/practicum/android/diploma/favourite/ui/FragmentFavourite.kt
+++ app/src/main/java/ru/practicum/android/diploma/favourite/ui/FragmentFavourite.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import androidx.recyclerview.widget.RecyclerView
 import ru.practicum.android.diploma.adapter.VacancyAdapter
 import ru.practicum.android.diploma.databinding.FragmentFavouriteBinding
 import ru.practicum.android.diploma.domain.models.Vacancy
@@ -13,9 +11,7 @@ import ru.practicum.android.diploma.util.BindingFragment
 
 class FragmentFavourite : BindingFragment<FragmentFavouriteBinding>() {
 
-    lateinit var recyclerViewFavourite: RecyclerView
     lateinit var vacancyAdapter: VacancyAdapter
-    lateinit var placeholderFavourite: LinearLayout
     override fun createBinding(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,21 +22,14 @@ class FragmentFavourite : BindingFragment<FragmentFavouriteBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initViews()
-
         initAdapter()
 
         setListeners()
     }
 
-    private fun initViews() {
-        recyclerViewFavourite = binding.recyclerView
-        placeholderFavourite = binding.placeholderFavourite
-    }
-
     private fun initAdapter() {
         vacancyAdapter = VacancyAdapter(ArrayList<Vacancy>())
-        recyclerViewFavourite.adapter = vacancyAdapter
+        binding.recyclerView.adapter = vacancyAdapter
     }
 
     private fun setListeners() {
