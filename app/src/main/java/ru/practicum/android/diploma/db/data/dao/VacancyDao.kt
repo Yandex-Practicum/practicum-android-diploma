3ccbe81d80ea5846e709e3e12aadad58322a0285
Aleksey Putilov <putilov0407@gmail.com>
2023-08-30 20:59:42 +0500

    add Vacancy Data Base

---
+++ app/src/main/java/ru/practicum/android/diploma/db/data/dao/VacancyDao.kt
@@ -0,0 +1,23 @@
+package ru.practicum.android.diploma.db.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ru.practicum.android.diploma.db.data.entity.VacancyEntity
+
+@Dao
+interface VacancyDao {
+    @Insert(entity = VacancyEntity::class, onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertVacancy(vacancyEntity: VacancyEntity)
+
+    @Delete(entity = VacancyEntity::class)
+    suspend fun deleteVacancy(vacancyEntity: VacancyEntity)
+
+    @Query("SELECT * FROM vacancy_table")
+    suspend fun getFavouriteVacancy(): List<VacancyEntity>
+
+    @Query("SELECT * FROM vacancy_table WHERE id = :vacancyId")
+    suspend fun getFavouriteVacancyById(vacancyId: Int): VacancyEntity
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/db/data/entity/VacancyEntity.kt
@@ -0,0 +1,16 @@
+package ru.practicum.android.diploma.db.data.entity
+
+import androidx.room.Entity
+
+@Entity(tableName = "vacancy_table")
+data class VacancyEntity(
+    val id: String,
+    val name: String,
+    val city: String?,
+    val employerName: String,
+    val employerLogoUrl: String?,
+    val salaryCurrency: String?,
+    val salaryFrom: Int?,
+    val salaryTo: Int?,
+    val currentDate: String
+)
