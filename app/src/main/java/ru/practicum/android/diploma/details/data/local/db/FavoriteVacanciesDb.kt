83ac53b7217b96d58956fadc9fe2cc2e143d51be
mvrlrd <mvrlrd@yandex.ru>
2023-08-30 16:49:55 +0300

    changed bd version to 2

--- app/src/main/java/ru/practicum/android/diploma/details/data/local/db/FavoriteVacanciesDb.kt
+++ app/src/main/java/ru/practicum/android/diploma/details/data/local/db/FavoriteVacanciesDb.kt
@@ -3,7 +3,7 @@ package ru.practicum.android.diploma.details.data.local.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import ru.practicum.android.diploma.details.data.local.model.VacancyEntity
-@Database(version = 1, entities = [VacancyEntity::class])
+@Database(version = 2, entities = [VacancyEntity::class])
 abstract class FavoriteVacanciesDb : RoomDatabase() {
     abstract fun getDao(): FavoriteDao
 }
