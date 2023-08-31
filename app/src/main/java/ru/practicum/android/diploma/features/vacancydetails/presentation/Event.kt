718009428ea2861a03cce1d8dc343cd5fb6d4ed2
pogonat <aimagenta81@gmail.com>
2023-08-31 19:03:42 +0600

    GH-18: create event wrapper for events

---
+++ app/src/main/java/ru/practicum/android/diploma/features/vacancydetails/presentation/Event.kt
@@ -0,0 +1,16 @@
+package ru.practicum.android.diploma.features.vacancydetails.presentation
+
+class Event<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+        private set
+
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+}
