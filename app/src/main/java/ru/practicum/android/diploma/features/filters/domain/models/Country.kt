445e01c104bccdfd64b4a1c26734fecbe78650a5
InnaLapwa <inna27.02@yandex.ru>
2023-09-01 23:48:15 +0500

    Создала модельки для фильтра

---
+++ app/src/main/java/ru/practicum/android/diploma/features/filters/domain/models/Country.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.features.filters.domain.models
+
+data class Country(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/ru/practicum/android/diploma/features/filters/domain/models/Region.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.features.filters.domain.models
+
+data class Region(
+    val id: Int,
+    val name: String
+)
