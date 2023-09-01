a25507f18f7687f32e717abc2583f7dfdea77c3a
mvrlrd <mvrlrd@yandex.ru>
2023-09-01 21:06:52 +0300

    add setOnClickListener on lottie heart button

--- app/src/main/java/ru/practicum/android/diploma/details/ui/DetailsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/details/ui/DetailsFragment.kt
@@ -2,10 +2,11 @@ package ru.practicum.android.diploma.details.ui
 
 import android.os.Bundle
 import android.view.View
-import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import kotlinx.coroutines.Dispatchers
@@ -13,7 +14,6 @@ import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentDetailsBinding
 import ru.practicum.android.diploma.root.RootActivity
-import ru.practicum.android.diploma.search.domain.models.VacancyFullInfoModel
 import ru.practicum.android.diploma.util.setImage
 import ru.practicum.android.diploma.util.thisName
 import ru.practicum.android.diploma.util.viewBinding
@@ -27,52 +27,32 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
         viewModel.log(thisName, "onViewCreated()")
         drawMainInfo()
         viewModel.getVacancyByID(args.vacancy.id)
         collector()
         pressSimilarVacanciesButton()
+        initAddToFavorite()
+    }
+
+    private fun initAddToFavorite(){
+        binding.lottieHeart.setOnClickListener {heartButton ->
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED){
+                    viewModel.handleAddToFavsButton()
+                }
+            }
+        }
     }
 
     private fun collector() {
         viewLifecycleOwner.lifecycleScope.launch(Dispatchers.Main) {
             viewModel.uiState.collect { state ->
                 viewModel.log(thisName, "uiState.collect { state -> ${state.thisName}")
-                when (state) {
-                    is DetailsScreenState.Empty -> Unit
-                    is DetailsScreenState.Content -> {
-                        showDescription(state.result)
-                    }
-                }
+                state.render(binding)
             }
         }
     }
-
-    private fun showDescription(vacancy: VacancyFullInfoModel) {
-        with(binding) {
-            viewModel.log(thisName, "showDescription()")
-            val tvSchedule = vacancy.employment?.name + ". " + vacancy.schedule?.name
-            val formattedDescription =
-                HtmlCompat.fromHtml(vacancy.description!!, HtmlCompat.FROM_HTML_MODE_LEGACY)
-            val tvKeySkillsList =
-                vacancy.keySkills?.mapIndexed { _, skill -> "• ${skill.name}" }?.joinToString("\n")
-            val phoneList = vacancy.contacts?.phones?.mapIndexed { _, phone ->
-                "+" + phone.country +
-                        " (${phone.city}) " + phone.number
-            }?.joinToString("\n")
-
-            tvExperience.text = vacancy.experience?.name
-            tvScheduleEmployment.text = tvSchedule
-            tvDescription.text = formattedDescription
-            tvKeySkills.text = tvKeySkillsList
-            tvContactsName.text = vacancy.contacts?.name
-            tvContactsEmail.text = vacancy.contacts?.email
-            tvContactsPhone.text = phoneList
-        }
-    }
-
 
     private fun pressSimilarVacanciesButton() {
         binding.buttonSameVacancy.setOnClickListener {
