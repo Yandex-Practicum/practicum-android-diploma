fe9c69bda10d3366a79417d70bbaf0a17a92904c
OlegKostinDS <olegkostinds@gmail.com>
2023-09-02 10:15:43 +0300

    added navigation and fixed bugs

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/fragments/BaseFilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/fragments/BaseFilterFragment.kt
@@ -10,15 +10,20 @@ import ru.practicum.android.diploma.util.viewBinding
 
 
 class BaseFilterFragment : Fragment(R.layout.fragment_filter_base) {
-private val binding by viewBinding<FragmentFilterBaseBinding>()
+    private val binding by viewBinding<FragmentFilterBaseBinding>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initListeners()
     }
 
-    private fun initListeners(){
-        binding.filterToolbar.setNavigationOnClickListener{
+    private fun initListeners() {
+        binding.filterToolbar.setNavigationOnClickListener {
             findNavController().popBackStack()
+        }
+        binding.workPlaceContainer.setOnClickListener {
+            findNavController().navigate(
+                resId = R.id.action_filterBaseFragment_to_workPlaceFilterFragment
+            )
         }
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/filter/ui/fragments/WorkPlaceFilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/fragments/WorkPlaceFilterFragment.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma.filter.ui.fragments
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
@@ -10,15 +11,24 @@ import ru.practicum.android.diploma.util.viewBinding
 
 class WorkPlaceFilterFragment : Fragment(R.layout.fragment_work_place_filter) {
     private val binding by viewBinding<FragmentWorkPlaceFilterBinding>()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         initListeners()
     }
-
 
     private fun initListeners() {
         binding.filterToolbar.setNavigationOnClickListener {
             findNavController().popBackStack()
+        }
+        binding.countryContainer.setOnClickListener {
+            findNavController().navigate(
+                resId = R.id.action_workPlaceFilterFragment_to_countryFilterFragment
+            )
+        }
+        binding.regionContainer.setOnClickListener {
+            findNavController().navigate(
+                resId = R.id.action_workPlaceFilterFragment_to_regionFragment
+            )
         }
     }
 }
