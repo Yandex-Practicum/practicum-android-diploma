c4d90e66a061b0ed94e564e22999cbb035797d70
Margarita Bespalova <margo.ivi@yandex.ru>
2023-09-03 16:29:50 +0300

    [GH-Search-109]: fix image bug

--- app/src/main/java/ru/practicum/android/diploma/search/ui/fragment/SearchViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/fragment/SearchViewHolder.kt
@@ -2,12 +2,12 @@ package ru.practicum.android.diploma.search.ui.fragment
 
 import androidx.recyclerview.widget.RecyclerView
 import ru.practicum.android.diploma.R
-import ru.practicum.android.diploma.databinding.ItemDescriptionBinding
+import ru.practicum.android.diploma.databinding.ItemSearchBinding
 import ru.practicum.android.diploma.search.domain.models.Vacancy
 import ru.practicum.android.diploma.util.setImage
 
 class SearchViewHolder(
-    private val binding: ItemDescriptionBinding
+    private val binding: ItemSearchBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(vacancy: Vacancy) {
