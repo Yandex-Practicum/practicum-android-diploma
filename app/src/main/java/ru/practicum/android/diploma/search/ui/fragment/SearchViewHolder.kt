ffda59cc11c06686e5aecb4b921cd6f6c9d8ffe5
Margarita Bespalova <margo.ivi@yandex.ru>
2023-09-03 14:48:43 +0300

    [GH-Search-109]: fix corner radius

--- app/src/main/java/ru/practicum/android/diploma/search/ui/fragment/SearchViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/fragment/SearchViewHolder.kt
@@ -1,27 +1,30 @@
 package ru.practicum.android.diploma.search.ui.fragment
 
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.ItemDescriptionBinding
 import ru.practicum.android.diploma.search.domain.models.Vacancy
+import ru.practicum.android.diploma.util.setImage
 
 class SearchViewHolder(
     private val binding: ItemDescriptionBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(vacancy: Vacancy) {
-        val titleAndArea: String = if (vacancy.area.isNotEmpty()) "${vacancy.title}, ${vacancy.area}"
-        else vacancy.title
+    
+        val titleAndArea: String =
+            if (vacancy.area.isNotEmpty()) "${vacancy.title}, ${vacancy.area}"
+            else vacancy.title
+    
+        val cornerRadius = itemView.resources.getDimensionPixelSize(R.dimen.size_12dp)
+    
         binding.title.text = titleAndArea
         binding.company.text = vacancy.company
         binding.value.text = vacancy.salary
-        Glide.with(itemView.context)
-            .load(vacancy.iconUri)
-            .placeholder(R.drawable.ic_placeholder_company)
-            .transform(CenterCrop(),RoundedCorners(R.dimen.size_12dp))
-            .into(binding.image)
+        binding.image.setImage(
+            url = vacancy.iconUri,
+            placeholder = R.drawable.ic_placeholder_company,
+            cornerRadius = cornerRadius
+        )
     }
 }
