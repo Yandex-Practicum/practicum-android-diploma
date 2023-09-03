32f3cecc007bac8200e169ee5772fbe8ed0f355b
Margarita Bespalova <margo.ivi@yandex.ru>
2023-09-03 19:28:28 +0300

    [GH-Search-115]: Навела порядок в DTO классах

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/dto/Branding.kt
+++
@@ -1,9 +0,0 @@
-package ru.practicum.android.diploma.search.data.network.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Branding(
-    val tariff: String? = "",
-    val type: String? = "",
-)
