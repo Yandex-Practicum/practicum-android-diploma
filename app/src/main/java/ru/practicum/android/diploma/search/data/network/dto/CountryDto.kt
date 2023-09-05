bc58e452cc6cdcf1b8b3ba5709b0707a042f59e1
OlegKostinDS <olegkostinds@gmail.com>
2023-09-05 19:48:27 +0300

    reworked issues

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/dto/CountryDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/dto/CountryDto.kt
@@ -1,12 +1,10 @@
 package ru.practicum.android.diploma.search.data.network.dto
 
-import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class CountryDto(
-
     val id: String? = "",
     var name: String? = "",
-    @SerializedName("areas")var areas: List<RegionArea?>
+    var areas: List<RegionArea?>
 )
