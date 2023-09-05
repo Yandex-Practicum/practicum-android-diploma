d62bbdfeb99b68aab0fb17189ff2b08c84143339
OlegKostinDS <olegkostinds@gmail.com>
2023-09-05 19:24:59 +0300

    commit

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/dto/CountryDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/dto/CountryDto.kt
@@ -1,10 +1,12 @@
 package ru.practicum.android.diploma.search.data.network.dto
 
+import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class CountryDto(
-    val url: String? = "",
+
     val id: String? = "",
     var name: String? = "",
+    @SerializedName("areas")var areas: List<RegionArea?>
 )
