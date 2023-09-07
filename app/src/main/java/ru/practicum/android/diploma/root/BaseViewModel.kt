d12cad1a5204bce521120ce4a06c177bc4c35a80
mvrlrd <mvrlrd@yandex.ru>
2023-09-07 22:56:04 +0300

    upgrade BaseViewModel.kt with open handleFailure method

--- app/src/main/java/ru/practicum/android/diploma/root/BaseViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/BaseViewModel.kt
@@ -1,9 +1,16 @@
 package ru.practicum.android.diploma.root
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import ru.practicum.android.diploma.Logger
+import ru.practicum.android.diploma.util.functional.Failure
+import ru.practicum.android.diploma.util.thisName
 
 abstract class BaseViewModel(private val logger: Logger) : ViewModel() {
+    open fun handleFailure(failure: Failure) {
+        Log.e(thisName, "FAILURE: ${failure.code} ", )
+    }
+
     fun log(name: String, text: String) {
         logger.log(name, text)
     }
