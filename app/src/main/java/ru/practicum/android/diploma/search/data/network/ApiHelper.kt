3729db612ec9352878d010bf56c6094c61714ac4
mvrlrd <mvrlrd@yandex.ru>
2023-09-08 00:18:37 +0300

    add logger to apiHelper

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/ApiHelper.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/ApiHelper.kt
@@ -1,16 +1,19 @@
 package ru.practicum.android.diploma.search.data.network
 
 import retrofit2.Response
+import ru.practicum.android.diploma.Logger
 import ru.practicum.android.diploma.di.annotations.NewResponse
 import ru.practicum.android.diploma.search.data.network.dto.response.VacanciesResponse
 import ru.practicum.android.diploma.util.functional.Either
 import ru.practicum.android.diploma.util.functional.Failure
+import ru.practicum.android.diploma.util.thisName
 import javax.inject.Inject
 
 @NewResponse
 class ApiHelper @Inject constructor(
     private val apiService: HhApiService,
-    private val networkHandler: InternetController
+    private val networkHandler: InternetController,
+    private val logger: Logger
 ):  AlternativeRemoteDataSource{
     override suspend fun getVacancies(query: String): Either<Failure, VacanciesResponse> {
         return requestData(VacanciesResponse.empty) {
@@ -18,12 +21,13 @@ class ApiHelper @Inject constructor(
         }
     }
 
-    private fun <T> responseHandle(response: Response<T>, default: T): Either<Failure, T> =
-        when (response.isSuccessful) {
+    private fun <T> responseHandle(response: Response<T>, default: T): Either<Failure, T> {
+        logger.log(thisName, "responseHandle: ${response.code()}",)
+        return when (response.isSuccessful) {
             true -> Either.Right(response.body() ?: default)
             false -> Either.Left(Failure.ServerError(response.code()))
         }
-
+    }
 
     private suspend fun <T> requestData(default: T, request: suspend () -> Response<T>) =
         when (networkHandler.isInternetAvailable()) {
