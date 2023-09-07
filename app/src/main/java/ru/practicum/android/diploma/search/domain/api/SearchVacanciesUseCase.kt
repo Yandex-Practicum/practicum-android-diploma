c364bf2e83442da02ecf69460b99ba916587d3c8
mvrlrd <mvrlrd@yandex.ru>
2023-09-07 22:17:47 +0300

    update searchUseCase

--- app/src/main/java/ru/practicum/android/diploma/search/domain/api/SearchVacanciesUseCase.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/api/SearchVacanciesUseCase.kt
@@ -1,8 +1,15 @@
 package ru.practicum.android.diploma.search.domain.api
 
 import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.di.annotations.NewResponse
 import ru.practicum.android.diploma.search.domain.models.FetchResult
+import ru.practicum.android.diploma.search.domain.models.Vacancy
+import ru.practicum.android.diploma.util.functional.Either
+import ru.practicum.android.diploma.util.functional.Failure
 
 interface SearchVacanciesUseCase {
     suspend fun search(query: String): Flow<FetchResult>
+
+    @NewResponse
+    suspend fun searchVacancies(query: String): Either<Failure, List<Vacancy>>
 }
--- app/src/main/java/ru/practicum/android/diploma/search/domain/impl/SearchVacanciesUseCaseImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/impl/SearchVacanciesUseCaseImpl.kt
@@ -2,9 +2,13 @@ package ru.practicum.android.diploma.search.domain.impl
 
 import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.Logger
+import ru.practicum.android.diploma.di.annotations.NewResponse
 import ru.practicum.android.diploma.search.domain.api.SearchRepository
 import ru.practicum.android.diploma.search.domain.api.SearchVacanciesUseCase
 import ru.practicum.android.diploma.search.domain.models.FetchResult
+import ru.practicum.android.diploma.search.domain.models.Vacancy
+import ru.practicum.android.diploma.util.functional.Either
+import ru.practicum.android.diploma.util.functional.Failure
 import ru.practicum.android.diploma.util.thisName
 import javax.inject.Inject
 
@@ -13,9 +17,14 @@ class SearchVacanciesUseCaseImpl @Inject constructor(
     private val logger: Logger,
 ):
     SearchVacanciesUseCase {
-    
+
     override suspend fun search(query: String): Flow<FetchResult> {
         logger.log(thisName, "fun search($query: String): Flow<FetchResult>")
         return repository.search(query)
+    }
+
+    @NewResponse
+    override suspend fun searchVacancies(query: String): Either<Failure, List<Vacancy>> {
+        return repository.searchVacancies(query)
     }
 }
