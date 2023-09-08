0a02fccdf0056137d9b0b15a4e233e2b0258932a
mvrlrd <mvrlrd@yandex.ru>
2023-09-08 10:32:38 +0300

    add new Failures

--- app/src/main/java/ru/practicum/android/diploma/util/functional/Failure.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/functional/Failure.kt
@@ -4,8 +4,10 @@ import ru.practicum.android.diploma.di.annotations.NewResponse
 
 @NewResponse
 sealed class Failure(val code: Int) {
-    class NetworkConnection(code: Int) : Failure(code)
+    class NetworkConnection(code: Int = 408) : Failure(code)
     class ServerError(code: Int) : Failure(code)
+    class UnknownError(code: Int = 111): Failure(code)
     abstract class AppFailure(code: Int) : Failure(code)
+    class NotFound(code: Int = 204): Failure(code = code)
 
 }
