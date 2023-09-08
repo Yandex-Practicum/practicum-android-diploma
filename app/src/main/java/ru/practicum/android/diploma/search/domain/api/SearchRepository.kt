9b5b89b7371a92500dd268a5c7f245866f646ab7
mvrlrd <mvrlrd@yandex.ru>
2023-09-08 10:33:07 +0300

    remove old search method

--- app/src/main/java/ru/practicum/android/diploma/search/domain/api/SearchRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/api/SearchRepository.kt
@@ -5,7 +5,6 @@ import ru.practicum.android.diploma.di.annotations.NewResponse
 import ru.practicum.android.diploma.filter.domain.models.NetworkResponse
 import ru.practicum.android.diploma.filter.domain.models.Country
 import ru.practicum.android.diploma.filter.domain.models.Region
-import ru.practicum.android.diploma.search.domain.models.FetchResult
 import ru.practicum.android.diploma.search.domain.models.Vacancies
 import ru.practicum.android.diploma.util.functional.Either
 import ru.practicum.android.diploma.util.functional.Failure
@@ -14,7 +13,6 @@ interface SearchRepository {
 
     @NewResponse
     suspend fun searchVacancies(query: String): Either<Failure, Vacancies>
-    suspend fun search(query: String): Flow<FetchResult>
 
     suspend fun getCountries(): Flow<NetworkResponse<List<Country>>>
     suspend fun getRegionsById(countryId: String): Flow<NetworkResponse<List<Region>>>
