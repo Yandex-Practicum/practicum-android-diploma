daf9d8ccfe22a504a7435131dd2325c504030c3a
OlegKostinDS <olegkostinds@gmail.com>
2023-09-09 12:06:15 +0300

    reworked issue

--- app/src/main/java/ru/practicum/android/diploma/filter/data/model/IndustryAreaDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/data/model/IndustryAreaDto.kt
@@ -2,12 +2,14 @@ package ru.practicum.android.diploma.filter.data.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 
-@Parcelize
 @Serializable
 data class IndustryAreaDto(
+    @SerialName("id")
     val id: String?,
+    @SerialName("name")
     val name: String?,
-) : Parcelable
+)
