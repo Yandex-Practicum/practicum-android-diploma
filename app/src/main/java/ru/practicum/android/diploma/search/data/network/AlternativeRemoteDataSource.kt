bd20b1af080441a18382dbe49300f286efe404f9
mvrlrd <mvrlrd@yandex.ru>
2023-09-09 12:03:45 +0300

    add doRequest to AlternativeRemoteDataSource

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/AlternativeRemoteDataSource.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/AlternativeRemoteDataSource.kt
@@ -1,13 +1,11 @@
 package ru.practicum.android.diploma.search.data.network
 
 
-import ru.practicum.android.diploma.filter.data.model.CountryDto
-import ru.practicum.android.diploma.search.data.network.dto.response.VacanciesResponse
+import ru.practicum.android.diploma.search.data.network.dto.request.Request
 import ru.practicum.android.diploma.util.functional.Either
 import ru.practicum.android.diploma.util.functional.Failure
 
 //@NewResponse
 interface AlternativeRemoteDataSource {
-    suspend fun getVacancies(query: String): Either<Failure, VacanciesResponse>
-    suspend fun getAllCountries() : Either<Failure, List<CountryDto>>
+    suspend fun doRequest(request: Request): Either<Failure, Any>
 }
