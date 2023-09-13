c9e50e7b00dbc8f8c1b37b0da9892fb1ad2b4996
mvrlrd <mvrlrd@yandex.ru>
2023-09-13 12:46:49 +0300

    deleted unused files

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/Vacancy.kt
+++
@@ -1,8 +0,0 @@
-package ru.practicum.android.diploma.search.data.network
-
-
-sealed interface Vacancy {
-    class FullInfoRequest(val id : String): Vacancy
-    class SearchRequest(val query: String): Vacancy
-    class SimilarVacanciesRequest(val id: String): Vacancy
-}
