90d73fd3d370b764b8120f20db44c2c5f4630fde
Vasil9202 <118492858+Vasil9202@users.noreply.github.com>
2023-10-29 15:06:18 +0300

    RootActivity и BottomViewNav созданы

--- app/src/main/java/ru/practicum/android/diploma/core/root/presentation/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/root/presentation/ui/RootActivity.kt
@@ -3,15 +3,29 @@ package ru.practicum.android.diploma.core.root.presentation.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.practicum.android.diploma.BuildConfig
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRootBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
+
     }
 
     private fun networkRequestExample(accessToken: String) {
