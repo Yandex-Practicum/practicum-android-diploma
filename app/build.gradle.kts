e1163f9d17e028a5a066772dd8e1a37c0a421884
alferac@inbox.ru <dmitr-nik.k@yandex.ru>
2023-10-30 23:56:30 +0500

    DIPLOMA-11: add build dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,12 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("ru.practicum.android.diploma.plugins.developproperties")
+    id ("kotlin-kapt")
 }
 
 android {
     namespace = "ru.practicum.android.diploma"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ru.practicum.android.diploma"
@@ -27,19 +28,43 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     buildFeatures {
         buildConfig = true
+        viewBinding = true
     }
 }
 
 dependencies {
+
+    implementation ("androidx.fragment:fragment-ktx:1.6.1")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.4")
+    implementation ("androidx.fragment:fragment-ktx:1.6.1")
+
+    implementation ("com.github.bumptech.glide:glide:4.15.1")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.google.code.gson:gson:2.10.1")
+
+    implementation ("io.insert-koin:koin-android:3.5.0")
+
+    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+
+    implementation("androidx.room:room-runtime:2.6.0")
+    implementation("androidx.room:room-ktx:2.6.0")
+
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -48,4 +73,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
 }
