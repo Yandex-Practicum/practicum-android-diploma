d617bbfc6b0f26747c0724e65750ebf2de4491c2
YAn <yantsabut777@yandex.ru>
2023-11-09 15:42:34 +0300

    work 21

--- app/src/main/java/ru/practicum/android/diploma/presentation/filter/ChooseWorkplaceFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filter/ChooseWorkplaceFragment.kt
@@ -1,10 +1,10 @@
 package ru.practicum.android.diploma.presentation.filter
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentChooseWorkplaceBinding
@@ -36,7 +36,6 @@ class ChooseWorkplaceFragment : Fragment() {
         binding.regionTextInputEditText.setOnClickListener {
             findNavController().navigate(R.id.action_chooseWorkplaceFragment_to_chooseAreaFragment)
         }
-
 
     }
 
