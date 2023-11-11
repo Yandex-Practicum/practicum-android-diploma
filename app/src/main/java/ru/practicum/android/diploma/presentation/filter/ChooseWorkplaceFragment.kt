ee0aac0beab9ef8c4f1f80162d97b96239355f35
Your Name <you@example.com>
2023-11-11 17:11:49 +0400

    The country field is filled in

--- app/src/main/java/ru/practicum/android/diploma/presentation/filter/ChooseWorkplaceFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filter/ChooseWorkplaceFragment.kt
@@ -1,18 +1,23 @@
 package ru.practicum.android.diploma.presentation.filter
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentChooseWorkplaceBinding
+import ru.practicum.android.diploma.domain.models.filter.Country
+import ru.practicum.android.diploma.presentation.filter.chooseArea.ChooseCountryViewModel
 
 class ChooseWorkplaceFragment : Fragment() {
 
     private var _binding: FragmentChooseWorkplaceBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: ChooseCountryViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +29,11 @@ class ChooseWorkplaceFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val savedCountry: Country? = viewModel.interactor.getSelectedCountry()
+        val countryText: String = savedCountry?.name.orEmpty()
+
+        binding.countryTextInputEditText.setText(countryText)
 
         binding.settingsBackArrowImageview.setOnClickListener {
             findNavController().popBackStack()
