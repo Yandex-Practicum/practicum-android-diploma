add4ba6aaf3dcc78f0f44a0c0dd13a159652e3c1
YAn <yantsabut777@yandex.ru>
2023-11-19 22:21:10 +0300

    вернул init

--- app/src/main/java/ru/practicum/android/diploma/presentation/favourite/FavouriteViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/favourite/FavouriteViewModel.kt
@@ -11,6 +11,9 @@ import ru.practicum.android.diploma.domain.models.Vacancy
 
 class FavouriteViewModel(val interactor: FavouriteInteractor) : ViewModel() {
 
+    init {
+        fill()
+    }
 
     private fun fill() {
         viewModelScope.launch {
