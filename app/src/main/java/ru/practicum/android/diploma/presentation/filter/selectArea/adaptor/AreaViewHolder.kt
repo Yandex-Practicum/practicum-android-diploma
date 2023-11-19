836ba91a556ddfd82f93d390ad6a35320b7c6b76
YAn <yantsabut777@yandex.ru>
2023-11-19 18:39:56 +0300

    перерисовал экран без радиокнопки и доработал функцию множественного выбора

--- app/src/main/java/ru/practicum/android/diploma/presentation/filter/selectArea/adaptor/AreaViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filter/selectArea/adaptor/AreaViewHolder.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma.presentation.filter.selectArea.adaptor
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.RadioButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -12,12 +13,17 @@ class AreaViewHolder(itemView: View, private val clickListener: (Area) -> Unit) 
     private val name: TextView = itemView.findViewById(R.id.industry_region_title_textview)
     private val radioButton: RadioButton =
         itemView.findViewById(R.id.industry_region_check_radiobutton)
+    private val rightArrow: ImageView = itemView.findViewById(R.id._region_check_radiobutton)
+
+
 
     fun bind(item: Area) {
         name.text = item.name
+        radioButton.visibility = View.GONE
+        rightArrow.visibility = View.VISIBLE
         itemView.setOnClickListener {
             clickListener.invoke(item)
         }
-        radioButton.isChecked = item.isChecked
+
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/presentation/filter/selectArea/adaptor/IndystryViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filter/selectArea/adaptor/IndystryViewHolder.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma.presentation.filter.selectArea.adaptor
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.RadioButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -14,12 +15,19 @@ class IndystryViewHolder(itemView: View, private val clickListener: (Industry) -
     private val name: TextView = itemView.findViewById(R.id.industry_region_title_textview)
     private val radioButton: RadioButton =
         itemView.findViewById(R.id.industry_region_check_radiobutton)
-
+    private val rightArrow: ImageView = itemView.findViewById(R.id._region_check_radiobutton)
     fun bind(item: Industry) {
         name.text = item.name
-        itemView.setOnClickListener {
-            clickListener.invoke(item)
-        }
+        radioButton.visibility = View.VISIBLE
+        rightArrow.visibility = View.GONE
         radioButton.isChecked = item.isChecked
+        radioButton.setOnClickListener {
+             if (radioButton!!.isChecked) {
+                 clickListener.invoke(item)
+             } else {
+                 clickListener.invoke(item)
+             }
+        }
+
     }
 }
