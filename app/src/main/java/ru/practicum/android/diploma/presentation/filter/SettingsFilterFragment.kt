2b93a0da8fd2249c9e1f374824a0a397146abddb
Your Name <you@example.com>
2023-11-21 17:57:57 +0400

    Заменил ndystryiewolder исправил exception

--- app/src/main/java/ru/practicum/android/diploma/presentation/filter/SettingsFilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filter/SettingsFilterFragment.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -31,7 +32,7 @@ class SettingsFilterFragment : Fragment() {
         if (context is BottomNavigationVisibilityListener) {
             bottomNavigationVisibilityListener = context
         } else {
-            throw RuntimeException("$context must implement BottomNavigationVisibilityListener")
+            Log.e("SettingsFilterFragment", "$context must implement BottomNavigationVisibilityListener")
         }
     }
 
