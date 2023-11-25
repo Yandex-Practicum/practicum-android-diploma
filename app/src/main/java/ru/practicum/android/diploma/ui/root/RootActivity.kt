265db221f86b7eb80a63d98a581efad5de530583
p.strelchenko <p.strelchenko@hh.ru>
2023-11-25 21:02:23 +0600

    коммент6

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -15,8 +15,17 @@ class RootActivity : AppCompatActivity() {
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
     }
 
+    private fun sss2() {
+        // do nothing
+    }
+
     private fun networkRequestExample(accessToken: String) {
         // ...
     }
+
+    private fun sss() {
+        // do nothing
+    }
+
 
 }
