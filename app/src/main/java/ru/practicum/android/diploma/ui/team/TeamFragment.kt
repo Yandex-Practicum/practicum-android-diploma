99b525f34b2790f22e19c919a43e87ad412f6947
Sergun <ya.tarannov@yandex.ru>
2024-01-23 20:10:04 +0300

    Настроил навигацию и заглушки

--- app/src/main/java/ru/practicum/android/diploma/ui/team/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/team/TeamFragment.kt
@@ -1,21 +1,15 @@
 package ru.practicum.android.diploma.ui.team
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.practicum.android.diploma.R
 
 class TeamFragment : Fragment() {
-
+/*
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
+        arguments?.let {}
     }
 
     override fun onCreateView(
@@ -34,4 +28,6 @@ class TeamFragment : Fragment() {
                 }
             }
     }
+
+ */
 }
