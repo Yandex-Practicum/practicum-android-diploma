0a69f09c6233c9fd0ba086f999f907238b73b9ca
Sergun <ya.tarannov@yandex.ru>
2024-02-02 02:14:52 +0300

    merge from task_23_conflikt.

--- app/src/main/java/ru/practicum/android/diploma/domain/models/Vacancy.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/Vacancy.kt
@@ -2,7 +2,6 @@ package ru.practicum.android.diploma.domain.models
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
-import ru.practicum.android.diploma.domain.models.`object`.ClusterGroup
 
 @Parcelize
 data class Vacancy(
