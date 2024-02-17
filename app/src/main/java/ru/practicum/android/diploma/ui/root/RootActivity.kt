1e81171f785ce55570c38504abb66efd2ed02eb8
Practicum <practicum@ya.ru>
2024-02-17 20:12:43 +0700

    HHit-6: Поправила ошибки detekt

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -11,13 +11,13 @@ import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRootBinding
+    private var binding: ActivityRootBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Привязываем вёрстку к экрану
         binding = ActivityRootBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding?.root)
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
@@ -26,20 +26,20 @@ class RootActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
 
-        binding.bottomNavigationView.let {
-            it.setupWithNavController(navController)
-            it.selectedItemId = R.id.searchFragment2
+        binding?.bottomNavigationView.let {
+            it?.setupWithNavController(navController)
+            it?.selectedItemId = R.id.searchFragment2
         }
 
         // Панель меню не отображается для фрагмента вакансий и фильтров
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.filterFragment, R.id.vacancyFragment3 -> {
-                    binding.bottomNavigationView.isVisible = false
+                    binding?.bottomNavigationView?.isVisible = false
                 }
 
                 else -> {
-                    binding.bottomNavigationView.isVisible = true
+                    binding?.bottomNavigationView?.isVisible = true
                 }
             }
         }
