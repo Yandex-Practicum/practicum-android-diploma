951687110c3bc691e2e10880610b8eec113d5d5e
Practicum <practicum@ya.ru>
2024-02-17 17:14:20 +0700

    HHit-6: Добавила BottomNavigationView

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -2,16 +2,47 @@ package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRootBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        // Привязываем вёрстку к экрану
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.let {
+            it.setupWithNavController(navController)
+            it.selectedItemId = R.id.searchFragment2
+        }
+
+        // Панель меню не отображается для фрагмента вакансий и фильтров
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.filterFragment, R.id.vacancyFragment3-> {
+                    binding.bottomNavigationView.isVisible = false
+                }
+
+                else -> {
+                    binding.bottomNavigationView.isVisible = true
+                }
+            }
+        }
     }
 
     private fun networkRequestExample(accessToken: String) {
