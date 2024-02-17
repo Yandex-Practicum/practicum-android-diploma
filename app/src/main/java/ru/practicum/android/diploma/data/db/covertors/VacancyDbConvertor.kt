ae54c0b1051e3e80c6a21b925d68117f31fe4408
Sergun <ya.tarannov@yandex.ru>
2024-02-17 10:54:47 +0300

    База Даных

---
+++ app/src/main/java/ru/practicum/android/diploma/data/db/covertors/VacancyDbConvertor.kt
@@ -0,0 +1,17 @@
+package ru.practicum.android.diploma.data.db.covertors
+
+class VacancyDbConvertor {
+    /*
+    fun map(vacancy: VacancyDto): VacancyEntity {
+        return VacancyEntity(
+            id = vacancy.id
+        )
+    }
+
+    fun map(vacancy: VacancyEntity): VacancyDto {
+        return VacancyEntity(
+            id = vacancy.id
+        )
+    }
+         */
+}
