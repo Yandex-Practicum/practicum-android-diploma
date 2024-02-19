308398232673983b0e9fe65521f45ecab4dbe2f0
Виктор Бляхеров <viktor.blyakherov@yandex.ru>
2024-02-19 15:22:58 +0300

    avoid lateinit

--- app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
@@ -12,30 +12,30 @@ import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRootBinding
+    private var binding: ActivityRootBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityRootBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding!!.root)
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
 
         val navHostFragment =
-            supportFragmentManager.findFragmentById(binding.rootFragmentContainerView.id) as NavHostFragment
+            supportFragmentManager.findFragmentById(binding!!.rootFragmentContainerView.id) as NavHostFragment
         val navController = navHostFragment.navController
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(binding.bottomNavigationView.id)
+        val bottomNavigationView = findViewById<BottomNavigationView>(binding!!.bottomNavigationView.id)
         bottomNavigationView.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.searchFragment, R.id.favouritesFragment, R.id.teamFragment -> {
-                    binding.bottomNavigationView.visibility = View.VISIBLE
+                    binding!!.bottomNavigationView.visibility = View.VISIBLE
                 }
                 else -> {
-                    binding.bottomNavigationView.visibility = View.GONE
+                    binding!!.bottomNavigationView.visibility = View.GONE
                 }
             }
         }
