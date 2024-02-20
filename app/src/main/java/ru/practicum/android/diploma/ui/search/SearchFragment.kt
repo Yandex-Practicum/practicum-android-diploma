ec3e9d85da840d2d273fc8a33829b508c501b24a
Stuysha <styusha.kinski@yandex.ru>
2024-02-21 00:03:49 +0300

    Фильтр экраны 10-16

--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
@@ -1,10 +1,11 @@
 package ru.practicum.android.diploma.ui.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
 
 class SearchFragment : Fragment() {
@@ -19,6 +20,7 @@ class SearchFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        findNavController().navigate(R.id.action_searchFragment_to_filterFragmentt)
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
