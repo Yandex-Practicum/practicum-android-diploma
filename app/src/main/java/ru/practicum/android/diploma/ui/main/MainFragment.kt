28a7f28a83b782e305dd9f4d9bcd851a0277128c
ValentinPside <91324342+ValentinPside@users.noreply.github.com>
2024-02-21 19:48:19 +0300

    Сделана кликабельная кнопка перехода на фрагмент с фильтром.
     При вводе текста в поисковую строку отображается крестик при не пустом editText.
    
    Реализовано нажатие крестика для очистки поля в EditText.

--- app/src/main/java/ru/practicum/android/diploma/ui/main/MainFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/main/MainFragment.kt
@@ -1,27 +1,72 @@
 package ru.practicum.android.diploma.ui.main
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputLayout
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentMainBinding
 import ru.practicum.android.diploma.ui.main.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     private val viewModel by viewModel<MainViewModel>()
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.searchEditText.addTextChangedListener(simpleTextWatcher())
+
+        binding.clearButton.setOnClickListener{
+            clearSearchText()
+        }
+
+        binding.filterImageView.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_filtersFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun clearSearchText(){
+        binding.searchEditText.setText("")
+        binding.searchContainer.endIconMode = TextInputLayout.END_ICON_CUSTOM
+        binding.searchContainer.endIconDrawable = requireContext().getDrawable(R.drawable.ic_search)
+        binding.clearButton.isEnabled = false
+    }
+
+    private fun simpleTextWatcher() = object: TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        @SuppressLint("UseCompatLoadingForDrawables")
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            binding.searchContainer.endIconMode = TextInputLayout.END_ICON_CLEAR_TEXT
+            binding.searchContainer.endIconDrawable = requireContext().getDrawable(R.drawable.ic_clear)
+            binding.clearButton.isEnabled = true
+        }
+
+        override fun afterTextChanged(s: Editable?) {}
+    }
 }
