be3ecca70459858597dfc0d8040f74c2d4b576a3
Artem Grigorash <artem.s.grigorash@gmail.com>
2024-02-24 14:45:14 +0200

    Реализаация репозитория

--- app/src/main/java/ru/practicum/android/diploma/domain/models/main/SearchInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/main/SearchInteractor.kt
@@ -4,5 +4,5 @@ import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.domain.api.Resource
 
 interface SearchInteractor {
-    fun searchTrack(term: String): Flow<Resource<List<Vacancy>>>
+    fun searchTrack(term: String): Flow<List<Vacancy>>
 }
--- app/src/main/java/ru/practicum/android/diploma/domain/models/main/impl/SearchInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/main/impl/SearchInteractorImpl.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma.domain.models.main.impl
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import ru.practicum.android.diploma.data.vacancylist.dto.VacanciesSearchRequest
 import ru.practicum.android.diploma.domain.api.Resource
@@ -11,16 +12,17 @@ import ru.practicum.android.diploma.domain.models.main.Vacancy
 class SearchInteractorImpl (
     val repository: SearchRepository
 ): SearchInteractor{
-    override fun searchTrack(term: String): Flow<List<Vacancy>> {
-        return repository.makeRequest(VacanciesSearchRequest(term)).map { result ->
-            when(result) {
-                is Resource.Success -> {
-                    result.data
-                }
-                is Resource.Error -> {
-                    result.message
-                }
-            }
-        }
+    override fun searchTrack(term: String): Flow<List<Vacancy>> = flow{
+//        return repository.makeRequest(VacanciesSearchRequest(term)).map { result ->
+//            when(result) {
+//                is Resource.Success -> {
+//                    result.data
+//                }
+//                is Resource.Error -> {
+//                    result.message
+//                }
+//            }
+//        }
+        //TODO: Доделать реализацию интерактора
     }
 }
