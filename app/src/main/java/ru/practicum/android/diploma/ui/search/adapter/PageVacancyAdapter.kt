052d8875f1312781d5ce85130b0632a9db7cfbc0
minul <minulin.misha@bk.ru>
2024-02-25 23:24:41 +0300

    добавил работу с сетью 2

--- app/src/main/java/ru/practicum/android/diploma/ui/search/adapter/PageVacancyAdapter.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/adapter/PageVacancyAdapter.kt
@@ -8,11 +8,11 @@ import androidx.recyclerview.widget.DiffUtil
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.domain.models.Vacancy
 
-class PageVacancyAdapter(context: Context) :
+class PageVacancyAdapter :
     PagingDataAdapter<Vacancy, VacancyViewHolder>(ArticleDiffItemCallback) {
-    private val layoutInflater: LayoutInflater = LayoutInflater.from(context)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VacancyViewHolder {
-        return VacancyViewHolder(layoutInflater.inflate(R.layout.vacancy, parent, false))
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.vacancy, parent, false)
+        return  VacancyViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: VacancyViewHolder, position: Int) {
