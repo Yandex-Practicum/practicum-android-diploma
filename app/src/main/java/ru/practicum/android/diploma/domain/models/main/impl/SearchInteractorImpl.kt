099f9f58c2110b7238d18d9cd9a3e366ad8ed405
Demonided <Demonided@mail.ru>
2024-02-27 08:55:27 +0300

    update token@

--- app/src/main/java/ru/practicum/android/diploma/domain/models/main/impl/SearchInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/main/impl/SearchInteractorImpl.kt
@@ -1,11 +1,7 @@
 package ru.practicum.android.diploma.domain.models.main.impl
 
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.withContext
-import ru.practicum.android.diploma.data.vacancylist.dto.VacanciesSearchRequest
 import ru.practicum.android.diploma.domain.api.Resource
 import ru.practicum.android.diploma.domain.api.SearchRepository
 import ru.practicum.android.diploma.domain.models.main.SearchInteractor
@@ -17,10 +13,11 @@ class SearchInteractorImpl(
 
     override suspend fun searchTrack(queryMap: Map<String, String>): Flow<Pair<List<Vacancy>?, Int?>> {
         return repository.makeRequest(queryMap).map {
-            when(it) {
+            when (it) {
                 is Resource.Success -> {
                     Pair(it.data, null)
                 }
+
                 is Resource.Error -> {
                     Pair(null, it.message)
                 }
