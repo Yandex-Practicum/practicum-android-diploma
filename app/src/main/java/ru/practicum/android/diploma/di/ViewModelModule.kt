bdcc32404cc5b17fffe2c335eb4aef6218f2a4d1
cerj275 <cerj275@gmail.com>
2024-02-28 21:36:30 +0300

    Fixed detekt errors

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -3,8 +3,8 @@ package ru.practicum.android.diploma.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.favourites.presentation.FavouritesViewModel
-import ru.practicum.android.diploma.vacancy.presentation.VacancyViewModel
 import ru.practicum.android.diploma.search.presentation.SearchViewModel
+import ru.practicum.android.diploma.vacancy.presentation.VacancyViewModel
 
 val viewModelModule = module {
     viewModel { (id: Long) ->
