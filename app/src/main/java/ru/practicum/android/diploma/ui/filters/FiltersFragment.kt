6744cd1fab3d480f020dd8fd6c8d80a9ba00ec37
Artem Grigorash <artem.s.grigorash@gmail.com>
2024-03-02 16:24:53 +0200

    Исправления

--- app/src/main/java/ru/practicum/android/diploma/ui/filters/FiltersFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filters/FiltersFragment.kt
@@ -2,20 +2,13 @@ package ru.practicum.android.diploma.ui.filters
 
 import android.os.Bundle
 
-import android.text.Layout
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-
 import ru.practicum.android.diploma.R
-import ru.practicum.android.diploma.databinding.FiltersItemBinding
 import ru.practicum.android.diploma.databinding.FragmentFiltersBinding
 
 class FiltersFragment : Fragment() {
