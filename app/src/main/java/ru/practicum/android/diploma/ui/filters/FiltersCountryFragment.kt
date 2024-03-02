bd56c9dbc7765cdf4b8a67b65b8369182f8ddfe2
el-pirats-bueno <el-pirats-bueno@yandex.ru>
2024-03-02 13:40:01 +0300

    Fix UI and add navigation in filter place of work

--- app/src/main/java/ru/practicum/android/diploma/ui/filters/FiltersCountryFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filters/FiltersCountryFragment.kt
@@ -6,39 +6,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentFilterChooseCountryBinding
 import ru.practicum.android.diploma.databinding.FragmentFilterChoosePlaceOfWorkBinding
 
-class FiltersPlaceOfWorkFragment : Fragment() {
+class FiltersCountryFragment : Fragment() {
 
-    private var _binding: FragmentFilterChoosePlaceOfWorkBinding? = null
+    private var _binding: FragmentFilterChooseCountryBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentFilterChoosePlaceOfWorkBinding.inflate(inflater, container, false)
+        _binding = FragmentFilterChooseCountryBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initListeners()
-    }
-
-    private fun initListeners() {
         binding.arrowBackButton.setOnClickListener {
             findNavController().navigateUp()
-        }
-
-        binding.country.setOnClickListener {
-            findNavController().navigate(R.id.action_filterPlaceOfWorkFragment_to_filtersCountryFragment)
-        }
-
-        binding.region.setOnClickListener {
-            findNavController().navigate(R.id.action_filterPlaceOfWorkFragment_to_filtersRegionFragment)
         }
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/filters/FiltersRegionFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filters/FiltersRegionFragment.kt
@@ -6,39 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import ru.practicum.android.diploma.R
-import ru.practicum.android.diploma.databinding.FragmentFilterChoosePlaceOfWorkBinding
+import ru.practicum.android.diploma.databinding.FragmentFilterChooseRegionBinding
 
-class FiltersPlaceOfWorkFragment : Fragment() {
+class FiltersRegionFragment : Fragment() {
 
-    private var _binding: FragmentFilterChoosePlaceOfWorkBinding? = null
+    private var _binding: FragmentFilterChooseRegionBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentFilterChoosePlaceOfWorkBinding.inflate(inflater, container, false)
+        _binding = FragmentFilterChooseRegionBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initListeners()
-    }
-
-    private fun initListeners() {
         binding.arrowBackButton.setOnClickListener {
             findNavController().navigateUp()
-        }
-
-        binding.country.setOnClickListener {
-            findNavController().navigate(R.id.action_filterPlaceOfWorkFragment_to_filtersCountryFragment)
-        }
-
-        binding.region.setOnClickListener {
-            findNavController().navigate(R.id.action_filterPlaceOfWorkFragment_to_filtersRegionFragment)
         }
     }
 }
