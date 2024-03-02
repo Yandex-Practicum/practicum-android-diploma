2092685e8fd75c95362506e918298c72132cf14f
Alex <alexsoutes@yandex.ru>
2024-03-02 18:05:08 +0300

    Поиск отраслей

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/ChooseIndustryFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/ChooseIndustryFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.databinding.FragmentFilterChooseIndustryBinding
@@ -25,6 +26,9 @@ class ChooseIndustryFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel.getIndustries()
         binding.industryList.adapter = this.adapter
+        binding.expectedSalary1.doOnTextChanged { text, _, _, _ ->
+            viewModel.filterIndustries(text.toString())
+        }
         viewModel.industriesState.observe(viewLifecycleOwner) { state ->
             when(state) {
                 IndustriesState.Error -> {}
--- app/src/main/java/ru/practicum/android/diploma/ui/filter/ChooseIndustryViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/ChooseIndustryViewModel.kt
@@ -7,19 +7,28 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.data.Constant.SUCCESS_RESULT_CODE
 import ru.practicum.android.diploma.domain.api.IndustriesRepository
+import ru.practicum.android.diploma.domain.models.Industry
 
 class ChooseIndustryViewModel(private val repository: IndustriesRepository): ViewModel() {
     private val _industriesState = MutableLiveData<IndustriesState>()
     val industriesState: LiveData<IndustriesState> = _industriesState
+    var currentList = emptyList<Industry>()
     fun getIndustries() {
         viewModelScope.launch {
             _industriesState.value = IndustriesState.Loading
             val response = repository.get()
             if(response.resultCode == SUCCESS_RESULT_CODE) {
                 _industriesState.value = IndustriesState.Success(response.list)
+                currentList = response.list
             } else {
                 _industriesState.value = IndustriesState.Error
             }
         }
+    }
+    fun filterIndustries(editText: String) {
+        val filteredList = currentList.filter {
+            it.name.contains(editText, ignoreCase = true)
+        }
+        _industriesState.value = IndustriesState.Success(filteredList)
     }
 }
