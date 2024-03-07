3a55c1adfd1804f61b3e13744aa50f0a3722315f
Cdarius <cdarius21@yandex.ru>
2024-03-07 21:55:33 +0300

    Fix

--- app/src/main/java/ru/practicum/android/diploma/filter/area/data/AreaRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/area/data/AreaRepositoryImpl.kt
@@ -6,7 +6,6 @@ import ru.practicum.android.diploma.core.data.NetworkClient
 import ru.practicum.android.diploma.core.data.mapper.mapToDomain
 import ru.practicum.android.diploma.core.data.network.dto.AreasDto
 import ru.practicum.android.diploma.core.data.network.dto.GetAreasResponse
-import ru.practicum.android.diploma.core.data.network.dto.Response
 import ru.practicum.android.diploma.filter.area.domain.api.AreaRepository
 import ru.practicum.android.diploma.filter.area.domain.model.Area
 import ru.practicum.android.diploma.filter.area.domain.model.AreaError
@@ -16,20 +15,14 @@ class AreaRepositoryImpl(
     private val networkClient: NetworkClient
 ) : AreaRepository {
     override fun getAreas(id: String): Flow<Result<List<Area>, AreaError>> = flow {
-        val response: Response
-        response = networkClient.getAreas()
+        val response = networkClient.getAreas()
 
         when (response.resultCode) {
             NetworkClient.SUCCESSFUL_CODE -> {
                 var data = (response as GetAreasResponse).areas
 
                 if (!id.isNullOrEmpty()) {
-                    for (area in data) {
-                        if (area.id.equals(id)) {
-                            data = listOf(area)
-                            break
-                        }
-                    }
+                    data = getCountryRegions(data, id)
                 }
                 data = unCoverList(data)
                 if (!id.isNullOrEmpty()) {
@@ -44,14 +37,23 @@ class AreaRepositoryImpl(
         }
     }
 
+    private fun getCountryRegions(data: List<AreasDto>, countryId: String): List<AreasDto> {
+        for (area in data) {
+            if (area.id == countryId) {
+                return listOf(area)
+            }
+        }
+        return data
+    }
+
     private fun unCoverList(data: List<AreasDto>): List<AreasDto> {
         var newData = data
-        while (isExistNested(data)) {
+        while (isExistNested(newData)) {
             newData = newData.flatMap {
                 if (it.areas.isNullOrEmpty()) {
                     listOf(it)
                 } else {
-                    it.areas ?: emptyList()
+                    it.areas
                 }
             }
         }
--- app/src/main/java/ru/practicum/android/diploma/filter/area/ui/RegionFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/area/ui/RegionFragment.kt
@@ -40,7 +40,7 @@ class RegionFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentRegionBinding.inflate(inflater, container, false)
         return binding.root
     }
