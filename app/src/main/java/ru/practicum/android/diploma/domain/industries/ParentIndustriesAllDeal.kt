0c7f2f047888b6792d271d4790a3e3997408db20
Demonided <Demonided@mail.ru>
2024-03-12 12:42:21 +0300

    update ischecked

--- app/src/main/java/ru/practicum/android/diploma/domain/industries/ParentIndustriesAllDeal.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/industries/ParentIndustriesAllDeal.kt
@@ -3,5 +3,5 @@ package ru.practicum.android.diploma.domain.industries
 data class ParentIndustriesAllDeal(
     val id: String,
     val name: String,
-    var isChecked: Boolean = false
+    val isChecked: Boolean = false
 )
