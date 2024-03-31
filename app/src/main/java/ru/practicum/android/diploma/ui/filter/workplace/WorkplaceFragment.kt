762634c4b7ce8c83ac910b35f117964bf13a7618
demonided <demonided@mail.ru>
2024-03-31 15:31:56 +0300

    update WorkPlaceFragment and IndustriesFragment

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/workplace/WorkplaceFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/workplace/WorkplaceFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentWorkplaceBinding
 
 class WorkplaceFragment : Fragment() {
@@ -18,6 +20,14 @@ class WorkplaceFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.workplaceCount.setOnClickListener {
+            findNavController().navigate(R.id.action_workplaceFragment_to_countFragment)
+        }
+
+        binding.workplaceRegion.setOnClickListener {
+            findNavController().navigate(R.id.action_workplaceFragment_to_regionFragment)
+        }
     }
 
     override fun onDestroyView() {
