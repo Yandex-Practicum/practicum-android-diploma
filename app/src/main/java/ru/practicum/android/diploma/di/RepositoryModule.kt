3e6dbb8c79fbc2be818f88fbefa7afa5678abde3
Kekuz <anton.edlenko@yandex.ru>
2024-04-02 00:11:29 +0300

    Добавил зависимости в DI

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -1,12 +1,18 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.vacancies.VacanciesSearchRepositoryImpl
 import ru.practicum.android.diploma.data.vacancies.VacancyDetailsRepositoryImpl
 import ru.practicum.android.diploma.domain.api.details.VacancyDetailsRepository
+import ru.practicum.android.diploma.domain.api.search.VacanciesSearchRepository
 
 val repositoryModule = module {
 
     factory<VacancyDetailsRepository> {
         VacancyDetailsRepositoryImpl(get())
+    }
+
+    single<VacanciesSearchRepository> {
+        VacanciesSearchRepositoryImpl(get())
     }
 }
