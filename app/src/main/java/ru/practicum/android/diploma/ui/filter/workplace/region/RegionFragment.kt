2d81dba797b075484b06270bd00170410a916c45
void-85 <zGonzo@doctor.com>
2024-04-06 05:54:45 +0500

    detekt...

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/workplace/region/RegionFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/workplace/region/RegionFragment.kt
@@ -25,7 +25,11 @@ class RegionFragment : Fragment() {
     private var _binding: FragmentRegionBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentRegionBinding.inflate(layoutInflater)
         return binding.root
     }
