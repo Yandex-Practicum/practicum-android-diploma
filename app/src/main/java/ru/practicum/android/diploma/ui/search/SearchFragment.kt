33831ff689b2045b235de3e9b154b2d76640bf5e
Kekuz <anton.edlenko@yandex.ru>
2024-04-07 23:28:04 +0300

    Добавил прогресс бар при поисковом запросе

--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchFragment.kt
@@ -14,6 +14,7 @@ import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.paging.LoadState
 import androidx.paging.PagingData
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -53,11 +54,18 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        vacancyAdapter.addLoadStateListener {
+            binding.rvVacancy.isVisible = it.refresh != LoadState.Loading
+            binding.progressBar.isVisible = it.refresh == LoadState.Loading
+        }
+
         binding.rvVacancy.adapter = vacancyAdapter
 
         bindKeyboardSearchButton()
         bindTextWatcher()
         bindCrossButton()
+
+
 
         binding.searchFilter.setOnClickListener {
             findNavController().navigate(R.id.action_searchFragment_to_filterAllFragment)
