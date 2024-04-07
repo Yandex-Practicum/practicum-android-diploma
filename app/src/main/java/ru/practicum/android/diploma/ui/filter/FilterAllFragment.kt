fa1880f00c328db5702052e2eca3ce0bcb6c2b30
demonided <demonided@mail.ru>
2024-04-07 09:34:17 +0300

    update filter

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/FilterAllFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/FilterAllFragment.kt
@@ -1,14 +1,17 @@
 package ru.practicum.android.diploma.ui.filter
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentAllFilterBinding
+import ru.practicum.android.diploma.domain.filter.datashared.SalaryBooleanShared
 
 class FilterAllFragment : Fragment() {
 
@@ -22,15 +25,28 @@ class FilterAllFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n", "SuspiciousIndentation")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.countryState.observe(viewLifecycleOwner) { country ->
+            if (country != null) {
+                binding.filterTextWorkplace.text = country.countryName
+                binding.filterIcWorkplace.setImageResource(R.drawable.ic_close_24px)
+                binding.filterIcWorkplace.isClickable = true
+                binding.filterVisibleWorkplace.visibility = View.VISIBLE
 
-        }
-
-        viewModel.regionState.observe(viewLifecycleOwner) { region ->
-
+                viewModel.regionState.observe(viewLifecycleOwner) { region ->
+                    if (region != null)
+                        binding.filterTextWorkplace.text = country.countryName + ", " + region.regionName
+                }
+            } else {
+                binding.filterTextWorkplace.text = "Место работы"
+                binding.filterTextWorkplace.setTextColor(ContextCompat.getColor(requireContext(), R.color.grey))
+                binding.filterIcWorkplace.setImageResource(R.drawable.arrow_forward_24px)
+                binding.filterIcWorkplace.isClickable = false
+                binding.filterVisibleWorkplace.visibility = View.GONE
+            }
         }
 
         viewModel.industriesState.observe(viewLifecycleOwner) { industries ->
@@ -38,11 +54,39 @@ class FilterAllFragment : Fragment() {
         }
 
         viewModel.salarySum.observe(viewLifecycleOwner) { salarySum ->
-
+            if (salarySum?.salary?.isNotEmpty() == true) {
+                binding.expectedSalary.setTextColor(ContextCompat.getColor(requireContext(), R.color.blue))
+                binding.filterTextSalary.setText(salarySum.salary)
+                binding.filterTextSalary.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+                binding.filterSalaryClear.visibility = View.VISIBLE
+                binding.filterFunctionButton.visibility = View.VISIBLE
+            } else {
+                binding.expectedSalary.setTextColor(ContextCompat.getColor(requireContext(), R.color.grey))
+                binding.filterTextSalary.setText("")
+                binding.filterTextSalary.setTextColor(ContextCompat.getColor(requireContext(), R.color.grey))
+                binding.filterSalaryClear.visibility = View.GONE
+            }
         }
 
         viewModel.salaryBoolean.observe(viewLifecycleOwner) { salaryBoolean ->
 
+        }
+
+        binding.filterIcWorkplace.setOnClickListener {
+            viewModel.setCountryInfo(null)
+            viewModel.setRegionInfo(null)
+        }
+
+        binding.filterIcIndustries.setOnClickListener {
+            viewModel.setIndustriesInfo(null)
+        }
+
+        binding.filterFunctionRemove.setOnClickListener {
+            viewModel.setCountryInfo(null)
+            viewModel.setRegionInfo(null)
+            viewModel.setIndustriesInfo(null)
+            viewModel.setSalarySumInfo(null)
+            viewModel.setSalaryBooleanInfo(SalaryBooleanShared(false))
         }
 
         binding.filterWorkplace.setOnClickListener {
