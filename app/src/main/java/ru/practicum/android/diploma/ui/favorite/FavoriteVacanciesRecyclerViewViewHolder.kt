2bc0a6439c79f548997602f55d3fd8e49fb8cc12
void-85 <zGonzo@doctor.com>
2024-04-15 23:00:41 +0500

    Исправление FavoriteVacancies форматирование чисел

--- app/src/main/java/ru/practicum/android/diploma/ui/favorite/FavoriteVacanciesRecyclerViewViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/favorite/FavoriteVacanciesRecyclerViewViewHolder.kt
@@ -8,6 +8,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.FitCenter
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.domain.models.VacancyDetails
+import ru.practicum.android.diploma.util.SalaryFormatter
 
 class FavoriteVacanciesRecyclerViewViewHolder(
     itemView: View,
@@ -23,13 +24,25 @@ class FavoriteVacanciesRecyclerViewViewHolder(
         vacancyTitle.text = model.name
         vacancyCompany.text = model.employer?.name ?: ""
 
-        // добавить форматтер з/п
-        vacancySalary.text =
-            "от ${model.salary?.from ?: ""} до ${model.salary?.to ?: ""} ${model.salary?.currency ?: ""}END"
-                .replace("от  до  END", "Зарплата не указана")
-                .replace("от  ", "")
-                .replace("до  ", "")
-                .replace("END", "")
+        val salaryFrom = SalaryFormatter.format(model.salary?.from?.toString() ?: "")
+        val salaryTo = SalaryFormatter.format(model.salary?.to?.toString() ?: "")
+
+        if (salaryFrom != salaryTo) {
+            vacancySalary.text =
+                "от ${salaryFrom} до ${salaryTo} ${model.salary?.currency ?: ""}END"
+                    .replace("от  до  END", itemView.resources.getString(R.string.salary_not_specified))
+                    .replace("от  ", "")
+                    .replace("до  ", "")
+                    .replace("END", "")
+                    .replace("от", itemView.resources.getString(R.string.from))
+                    .replace("до", itemView.resources.getString(R.string.to))
+        } else {
+            vacancySalary.text =
+                "${salaryTo} ${model.salary?.currency ?: ""}END"
+                    .replace(" END", itemView.resources.getString(R.string.salary_not_specified))
+                    .replace("END", "")
+        }
+
         Glide
             .with(itemView)
             .load(model.employer?.logoUrls?.original ?: "")
