95ba0266b44c2ffc1c250b9167137a6925feeedf
ignatov-ai <ignatov-ai@yandex.ru>
2024-06-08 10:25:23 +0300

    hhp-16-toolbar

--- app/src/main/java/ru/practicum/android/diploma/ui/favorite/VacancyFavoriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/favorite/VacancyFavoriteFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentVacancyFavoriteBinding
 import ru.practicum.android.diploma.ui.root.RootActivity
