36447d0ea4a3c1acf22de1edc7c177da581c3531
Polina Gazarova <polina.gazarova@gmail.com>
2024-06-09 15:54:28 +0300

    Issue-10: app database creation

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/Vacancy.kt
@@ -0,0 +1,22 @@
+package ru.practicum.android.diploma.domain.models
+
+data class Vacancy(
+    val vacancyId: String,
+    val name: String,
+    val city: String?,
+    val area: String?,
+    val salaryFrom: Int?,
+    val salaryTo: Int?,
+    val salaryCurrency: String?,
+    val employerName: String?,
+    val employerLogoUrl: String?,
+    val experience: String?,
+    val employment: String?,
+    val schedule: String?,
+    val description: String,
+    val skills: List<String>,
+    val contactEmail: String?,
+    val contactName: String?,
+    val contactPhoneNumbers: List<String>,
+    val contactComment: List<String>,
+)
