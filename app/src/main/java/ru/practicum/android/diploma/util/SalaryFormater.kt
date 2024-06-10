edb6311c3c33cfe7479630f407660626b7be3f56
ignatov-ai <ignatov-ai@yandex.ru>
2024-06-10 23:58:19 +0300

    hhp-19-salary_formater
    detect fix

--- app/src/main/java/ru/practicum/android/diploma/util/SalaryFormater.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/SalaryFormater.kt
@@ -6,23 +6,22 @@ import java.text.NumberFormat
 import java.util.Locale
 
 object SalaryFormater {
-    fun salaryFormater(context: Context, from: Int?, to: Int?, currency: String?): String{
+    fun formaterForSalary(context: Context, from: Int?, to: Int?, currency: String?): String {
         val fromOnly = context.getString(R.string.salary_half_mask)
         val toOnly = context.getString(R.string.salary_half_mask)
         val fromAndTo = context.getString(R.string.salary_full_mask)
 
         val numberFormat: NumberFormat = NumberFormat.getInstance(Locale("ru", "RU"))
 
-
-        return when{
+        return when {
             fromOnly != null -> {
-                fromOnly.format(numberFormat.format(from), currency)}
+                fromOnly.format(numberFormat.format(from), currency) }
 
             toOnly != null -> {
-                toOnly.format(numberFormat.format(to), currency)}
+                toOnly.format(numberFormat.format(to), currency) }
 
             fromOnly != null && toOnly != null -> {
-                fromAndTo.format(numberFormat.format(from), numberFormat.format(to), currency)}
+                fromAndTo.format(numberFormat.format(from), numberFormat.format(to), currency) }
 
             else -> "Зарплата не указана"
         }
