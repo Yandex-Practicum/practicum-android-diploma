283e857c21b3cfe8315d7f1f89f04799672aca73
Nadezhda-Gurova <yudinanade@yandex.ru>
2024-06-10 19:23:33 +0300

    -

--- app/src/main/java/ru/practicum/android/diploma/data/network/HeadHunterRetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/HeadHunterRetrofitNetworkClient.kt
@@ -6,14 +6,20 @@ import ru.practicum.android.diploma.data.dto.VacancyResponse
 
 class HeadHunterRetrofitNetworkClient(private val api: HeadHunterApi) : HeadHunterNetworkClient {
     companion object {
-        private const val AUTHORIZATION_HEADER = "Bearer APPLRO2GGE350U5M54G5SAOIA52SMR6DH2RDCT4AH2I6O59JEOE6GETL8R0QJE2J"
+        private const val AUTHORIZATION_HEADER = "Bearer TOKEN"
     }
 
     override suspend fun getVacancies(filters: Map<String, String>): Response<VacancyResponse> {
-        return api.getVacancies(AUTHORIZATION_HEADER, filters)
+        return api.getVacancies(
+            AUTHORIZATION_HEADER,
+            filters
+        )
     }
 
     override suspend fun getVacancy(id: String): Response<VacancyDetails> {
-        return api.getVacancyDetails(AUTHORIZATION_HEADER, id)
+        return api.getVacancyDetails(
+            AUTHORIZATION_HEADER,
+            id
+        )
     }
 }
