d36caaeb86bc28a15f1202503826285335236e92
alekla0126 <alekla0126@gmail.com>
2024-06-11 12:38:58 +0300

    Resolved

--- app/src/main/java/ru/practicum/android/diploma/util/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/App.kt
@@ -6,9 +6,6 @@ import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class App : Application() {
-    // private var darkTheme = false
-
-    // private var sharedPref: SharedPreferences = getSharedPreferences(PRACTICUM_EXAMPLE_PREFERENCES, MODE_PRIVATE)
     override fun onCreate() {
         super.onCreate()
         startKoin {
@@ -16,28 +13,5 @@ class App : Application() {
             androidContext(this@App)
             modules()
         }
-        initTheme()
     }
-
-    fun initTheme() {
-//        if (sharedPref.contains(_themeKey)) {
-//            darkTheme = sharedPref.getBoolean(_themeKey, false)
-//            switchTheme(darkTheme)
-//        } else {
-//            darkTheme = false
-//        }
-    }
-
-//    private fun switchTheme(darkThemeEnabled: Boolean) {
-//        darkTheme = darkThemeEnabled
-//        AppCompatDelegate.setDefaultNightMode(
-//            if (darkThemeEnabled) {
-//                AppCompatDelegate.MODE_NIGHT_YES
-//            } else {
-//                AppCompatDelegate.MODE_NIGHT_NO
-//            }
-//        )
-//        getSharedPreferences(PRACTICUM_EXAMPLE_PREFERENCES, MODE_PRIVATE).edit().putBoolean(_themeKey, darkThemeEnabled)
-//            .apply()
-//    }
 }
