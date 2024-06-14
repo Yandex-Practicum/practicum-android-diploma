90068c82e4fd7898b775d9801c1c6f5a1a308d2e
Kate Mahova <kate.mahova@yandex.ru>
2024-06-14 21:41:50 +0300

    fix: di for Details Layout

--- app/src/main/java/ru/practicum/android/diploma/di/VacancyModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/VacancyModule.kt
@@ -1,7 +1,12 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.VacancyDetailsRepositoryImpl
+import ru.practicum.android.diploma.domain.api.VacancyDetailsInteractor
+import ru.practicum.android.diploma.domain.api.VacancyDetailsRepository
+import ru.practicum.android.diploma.domain.impl.VacancyDetailsInteractorImpl
 
 val vacancyModule = module {
-
+    single<VacancyDetailsRepository> { VacancyDetailsRepositoryImpl(get(), get(), get()) }
+    factory<VacancyDetailsInteractor> { VacancyDetailsInteractorImpl(get()) }
 }
