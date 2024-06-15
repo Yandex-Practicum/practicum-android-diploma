1b314bc6421e8fd31299e80ebc8bec2440dbec55
Kate Mahova <kate.mahova@yandex.ru>
2024-06-15 13:20:08 +0300

    feat: UI for Favorite Vacancies Layout

--- app/src/main/java/ru/practicum/android/diploma/di/FavoriteModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/FavoriteModule.kt
@@ -1,14 +1,17 @@
 package ru.practicum.android.diploma.di
 
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.data.FavoritesRepositoryImpl
 import ru.practicum.android.diploma.data.db.converters.VacancyEntityConverter
 import ru.practicum.android.diploma.domain.api.favorite.FavoritesInteractor
 import ru.practicum.android.diploma.domain.api.favorite.FavoritesRepository
 import ru.practicum.android.diploma.domain.impl.FavoritesInteractorImpl
+import ru.practicum.android.diploma.ui.favorite.FavoriteViewModel
 
 val favoriteModule = module {
     factory { VacancyEntityConverter() }
     single<FavoritesRepository> { FavoritesRepositoryImpl(get(), get()) }
     single<FavoritesInteractor> { FavoritesInteractorImpl(get()) }
+    viewModel { FavoriteViewModel(get(), get()) }
 }
