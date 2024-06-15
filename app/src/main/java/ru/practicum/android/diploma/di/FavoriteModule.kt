ef2c4c0c7e58c67b2ffc3f313bf831f30973c107
Kate Mahova <kate.mahova@yandex.ru>
2024-06-15 13:31:46 +0300

    fix: detect

--- app/src/main/java/ru/practicum/android/diploma/di/FavoriteModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/FavoriteModule.kt
@@ -2,8 +2,8 @@ package ru.practicum.android.diploma.di
 
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-import ru.practicum.android.diploma.data.repository.FavoritesRepositoryImpl
 import ru.practicum.android.diploma.data.db.converters.VacancyEntityConverter
+import ru.practicum.android.diploma.data.repository.FavoritesRepositoryImpl
 import ru.practicum.android.diploma.domain.api.favorite.FavoritesInteractor
 import ru.practicum.android.diploma.domain.api.favorite.FavoritesRepository
 import ru.practicum.android.diploma.domain.impl.favorite.FavoritesInteractorImpl
