82b31e9413489d0830237a524f959f04237759ff
dencharski <dencharski@yandex.ru>
2024-06-16 20:19:56 +0200

    -add favoriteInteractor,di interactorModule. some edit3

--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -1,8 +1,6 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
-import ru.practicum.android.diploma.domain.api.FavoritesVacancyInteractor
-import ru.practicum.android.diploma.domain.impl.FavoritesVacancyInteractorImpl
 
 val interactorModule = module {
     // single<FavoritesVacancyInteractor> { FavoritesVacancyInteractorImpl(get()) }
--- app/src/main/java/ru/practicum/android/diploma/domain/impl/FavoritesVacancyInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/impl/FavoritesVacancyInteractorImpl.kt
@@ -5,21 +5,21 @@ import ru.practicum.android.diploma.data.db.entity.FavoriteVacancyEntity
 import ru.practicum.android.diploma.domain.api.FavoritesVacancyInteractor
 import ru.practicum.android.diploma.domain.models.Vacancy
 
-class FavoritesVacancyInteractorImpl(
-    //private val favoritesVacancyRepository: FavoritesVacancyRepository
-) : FavoritesVacancyInteractor {
+class FavoritesVacancyInteractorImpl : FavoritesVacancyInteractor {
+    // (private val favoritesVacancyRepository: FavoritesVacancyRepository)
+
     override suspend fun getAllFavoritesVacancy(): Flow<List<Vacancy>> {
         TODO()
-        //return favoritesVacancyRepository.getAllFavoritesVacancy()
+        // return favoritesVacancyRepository.getAllFavoritesVacancy()
     }
 
     override suspend fun getOneFavoriteVacancy(vacancyId: String): Vacancy {
         TODO()
-        //return favoritesVacancyRepository.getOneFavoriteVacancy(vacancyId)
+        // return favoritesVacancyRepository.getOneFavoriteVacancy(vacancyId)
     }
 
     override suspend fun deleteFavoriteVacancy(vacancy: FavoriteVacancyEntity): Int {
         TODO()
-        //return favoritesVacancyRepository.deleteFavoriteVacancy(vacancy)
+        // return favoritesVacancyRepository.deleteFavoriteVacancy(vacancy)
     }
 }
