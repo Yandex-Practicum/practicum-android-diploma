ba660866a58b92a9b127528251354a268cb33575
dencharski <dencharski@yandex.ru>
2024-06-17 17:44:29 +0200

    - add FavoritesViewModel,FavoritesVacancyViewState. edit1

--- app/src/main/java/ru/practicum/android/diploma/presentation/favorites/FavoritesViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/favorites/FavoritesViewModel.kt
@@ -8,29 +8,25 @@ import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.domain.models.FavoritesVacancyViewState
 
 class FavoritesViewModel : ViewModel() {
-// (private val favoritesVacancyInteractor: FavoritesVacancyInteractor)
-
+    // (private val favoritesVacancyInteractor: FavoritesVacancyInteractor)
     private val _favoriteVacancyScreenState = MutableLiveData<FavoritesVacancyViewState>()
     val favoriteVacancyScreenState: LiveData<FavoritesVacancyViewState> get() = _favoriteVacancyScreenState
     fun getAllFavoritesVacancy() {
-
-        viewModelScope.launch {
-            /*
-            favoritesVacancyInteractor.getAllFavoritesVacancy().collect {
-                 try {
-                     if (it.isEmpty()) {
-                         _favoriteVacancyScreenState.postValue(FavoritesVacancyViewState.FavoritesVacancyEmptyDataResult)
-                     } else {
-                         _favoriteVacancyScreenState.postValue(
-                             FavoritesVacancyViewState.FavoritesVacancyDataResult(listOfFavoriteVacancy = it)
-                         )
+        /*  viewModelScope.launch {
+                    favoritesVacancyInteractor.getAllFavoritesVacancy().collect {
+                         try {
+                             if (it.isEmpty()) {
+                                 _favoriteVacancyScreenState.postValue(FavoritesVacancyViewState.FavoritesVacancyEmptyDataResult)
+                             } else {
+                                 _favoriteVacancyScreenState.postValue(
+                                     FavoritesVacancyViewState.FavoritesVacancyDataResult(listOfFavoriteVacancy = it)
+                                 )
+                             }
+                         } catch (e: Exception) {
+                             _favoriteVacancyScreenState.postValue(FavoritesVacancyViewState.FavoritesVacancyError)
+                         }
                      }
-                 } catch (e: Exception) {
-                     _favoriteVacancyScreenState.postValue(FavoritesVacancyViewState.FavoritesVacancyError)
-                 }
-             }
-             */
-        }
+                }
+                */
     }
-
 }
