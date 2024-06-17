47707b74c3acd4aafd5f2a5d9c19302a29d7bb94
Milan ISAEV <isaev.milan@gmail.com>
2024-06-17 18:47:51 +0300

    Hide share and favorite icons
    -some fix
    -detect fix

--- app/src/main/java/ru/practicum/android/diploma/ui/details/VacancyDetailsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/details/VacancyDetailsFragment.kt
@@ -2,7 +2,6 @@ package ru.practicum.android.diploma.ui.details
 
 import android.os.Bundle
 import android.text.Html
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
