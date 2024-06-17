17a34eec05b7bea874f970a25fc97500c289c09d
alekla0126 <alekla0126@gmail.com>
2024-06-17 14:05:50 +0300

    Hide keyboard added in SearchFragment.kt

--- app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
@@ -1,9 +1,11 @@
 package ru.practicum.android.diploma.presentation.search
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 
@@ -19,6 +21,13 @@ class SearchFragment : Fragment() {
     ): View? {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    fun Fragment.hideKeyboard() {
+        val inputMethodManager =
+            requireContext().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        val view = activity?.currentFocus ?: View(requireContext())
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     override fun onDestroyView() {
