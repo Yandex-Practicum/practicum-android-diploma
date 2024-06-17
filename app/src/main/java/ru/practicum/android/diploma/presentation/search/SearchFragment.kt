4dc6b00ea3c8050c5614c2868b8f5286a723cb12
Akhmed <shahidow@mail.ru>
2024-06-17 14:17:18 +0200

    adding SearchFragment

--- app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
@@ -19,6 +21,14 @@ class SearchFragment : Fragment() {
     ): View? {
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.filterButton.setOnClickListener{
+            findNavController().navigate(R.id.action_searchFragment_to_filtrationFragment)
+        }
     }
 
     override fun onDestroyView() {
