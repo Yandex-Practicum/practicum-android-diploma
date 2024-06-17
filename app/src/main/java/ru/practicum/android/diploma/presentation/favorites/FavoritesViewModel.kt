72b60c35a69052534d3ea61368a094be23cde314
dencharski <dencharski@yandex.ru>
2024-06-17 17:35:26 +0200

    - add FavoritesViewModel,FavoritesVacancyViewState, di viewModelModule

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/favorites/FavoritesViewModel.kt
@@ -0,0 +1,36 @@
+package ru.practicum.android.diploma.presentation.favorites
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import ru.practicum.android.diploma.domain.models.FavoritesVacancyViewState
+
+class FavoritesViewModel : ViewModel() {
+// (private val favoritesVacancyInteractor: FavoritesVacancyInteractor)
+
+    private val _favoriteVacancyScreenState = MutableLiveData<FavoritesVacancyViewState>()
+    val favoriteVacancyScreenState: LiveData<FavoritesVacancyViewState> get() = _favoriteVacancyScreenState
+    fun getAllFavoritesVacancy() {
+
+        viewModelScope.launch {
+            /*
+            favoritesVacancyInteractor.getAllFavoritesVacancy().collect {
+                 try {
+                     if (it.isEmpty()) {
+                         _favoriteVacancyScreenState.postValue(FavoritesVacancyViewState.FavoritesVacancyEmptyDataResult)
+                     } else {
+                         _favoriteVacancyScreenState.postValue(
+                             FavoritesVacancyViewState.FavoritesVacancyDataResult(listOfFavoriteVacancy = it)
+                         )
+                     }
+                 } catch (e: Exception) {
+                     _favoriteVacancyScreenState.postValue(FavoritesVacancyViewState.FavoritesVacancyError)
+                 }
+             }
+             */
+        }
+    }
+
+}
