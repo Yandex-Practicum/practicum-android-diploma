0b9eafd8155f66435753caab8b2544deda446318
dencharski <dencharski@yandex.ru>
2024-06-18 13:10:24 +0200

    - add observerViewState in SearchFragment

--- app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
@@ -12,6 +12,7 @@ import android.view.inputmethod.InputMethodManager
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
@@ -39,6 +40,7 @@ class SearchFragment : Fragment(), VacancyAdapter.ItemVacancyClickInterface {
         val view = activity?.currentFocus ?: View(requireContext())
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -82,5 +84,119 @@ class SearchFragment : Fragment(), VacancyAdapter.ItemVacancyClickInterface {
 
     override fun onItemVacancyClick(vacancy: Vacancy) {
         Log.d("search", "vacancy = ${vacancy.vacancyId}, ${vacancy.name}")
+    }
+
+    /*    fun observeViewStateValues() {
+           searchViewModel.searchViewModelState.observe(viewLifecycleOwner) {
+               when (it) {
+                   StateNetworkError -> {
+                       setStateNetworkError()
+                   }
+
+                   StateServerError -> {
+                       setStateServerError()
+                   }
+
+                   StateDefault -> {
+                       setStateDefault()
+                   }
+
+                   StateIsLoading -> {
+                       setStateIsLoading()
+                   }
+
+                   StateEmptyResult -> {
+                       setStateEmptyResult()
+                   }
+
+                   StateIsData -> {
+                       setSateIsData(it)
+                   }
+
+                   else -> {
+                   // это пустой метод
+                   }
+               }
+           }
+       }
+   */
+    private fun setStateNetworkError() {
+        binding.foundResultsMessage.visibility = View.GONE
+        binding.searchRecyclerView.visibility = View.GONE
+        binding.searchProgressBar.visibility = View.GONE
+        binding.searchPlaceholderLayout.visibility = View.VISIBLE
+        Glide.with(this)
+            .load(R.drawable.placeholder_skull)
+            .centerCrop()
+            .into(binding.searchPlaceholderImage)
+        binding.searchPlaceholderMessage.text = this.getString(R.string.no_internet_connection)
+    }
+
+    private fun setStateServerError() {
+        binding.foundResultsMessage.visibility = View.GONE
+        binding.searchRecyclerView.visibility = View.GONE
+        binding.searchProgressBar.visibility = View.GONE
+        binding.searchPlaceholderLayout.visibility = View.VISIBLE
+        Glide.with(this)
+            .load(R.drawable.placeholder_server_error_towel)
+            .centerCrop()
+            .into(binding.searchPlaceholderImage)
+        binding.searchPlaceholderMessage.text = this.getString(R.string.server_error)
+    }
+
+    private fun setStateDefault() {
+        binding.foundResultsMessage.visibility = View.GONE
+        binding.searchRecyclerView.visibility = View.GONE
+        binding.searchProgressBar.visibility = View.GONE
+        binding.searchPlaceholderLayout.visibility = View.VISIBLE
+        Glide.with(this)
+            .load(R.drawable.placeholder_man_search)
+            .centerCrop()
+            .into(binding.searchPlaceholderImage)
+        binding.searchPlaceholderMessage.text = ""
+    }
+
+    private fun setStateIsLoading() {
+        binding.foundResultsMessage.visibility = View.GONE
+        binding.searchRecyclerView.visibility = View.GONE
+        binding.searchProgressBar.visibility = View.VISIBLE
+        binding.searchPlaceholderLayout.visibility = View.GONE
+    }
+
+    private fun setStateEmptyResult() {
+        binding.foundResultsMessage.visibility = View.VISIBLE
+        binding.searchRecyclerView.visibility = View.GONE
+        binding.searchProgressBar.visibility = View.GONE
+        binding.searchPlaceholderLayout.visibility = View.VISIBLE
+        Glide.with(this)
+            .load(R.drawable.placeholder_cat)
+            .centerCrop()
+            .into(binding.searchPlaceholderImage)
+        binding.searchPlaceholderMessage.text = this.getString(R.string.count_get_list_of_vacancies)
+        binding.foundResultsMessage.text = this.getString(R.string.no_such_vacancies)
+    }
+
+    private fun setSateIsData(vacancyList: List<Vacancy>) {
+        binding.foundResultsMessage.visibility = View.VISIBLE
+        binding.searchRecyclerView.visibility = View.VISIBLE
+        binding.searchProgressBar.visibility = View.GONE
+        binding.searchPlaceholderLayout.visibility = View.GONE
+        Glide.with(this)
+            .load(R.drawable.placeholder_cat)
+            .centerCrop()
+            .into(binding.searchPlaceholderImage)
+        binding.searchPlaceholderMessage.text = this.getString(R.string.count_get_list_of_vacancies)
+        binding.foundResultsMessage.text = java.lang.String(
+            this.getString(R.string.found)
+                + whitespace
+                + vacancyList.size.toString()
+                + whitespace
+                + this.getString(R.string.vacancy)
+        )
+        vacancyAdapter?.setVacancyList(ArrayList(vacancyList))
+    }
+
+    companion object {
+        const val whitespace = " "
     }
 }
