b7ac664afb08d88c356b2fb3e7a5718ff084c5e9
Nadezhda-Gurova <yudinanade@yandex.ru>
2024-06-18 13:03:17 +0200

    60 Interactor

--- app/src/main/java/ru/practicum/android/diploma/domain/interactors/GetVacancyDetailsInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/interactors/GetVacancyDetailsInteractor.kt
@@ -1,20 +1,28 @@
 package ru.practicum.android.diploma.domain.interactors
 
+import retrofit2.HttpException
 import ru.practicum.android.diploma.data.repository.VacancyRepository
 import ru.practicum.android.diploma.domain.models.Vacancy
+import java.io.IOException
 
 class GetVacancyDetailsInteractor(private val repository: VacancyRepository) {
 
     suspend fun execute(vacancyId: String): Result<Vacancy> {
         return try {
             val response = repository.getVacancyDetails(vacancyId)
-            if (response.resultCode == 200) {
+            if (response.resultCode == SUCCESS_CODE) {
                 response.data?.let { Result.success(it) } ?: Result.failure(Exception("Invalid data format"))
             } else {
                 Result.failure(Exception(response.errorMessage))
             }
-        } catch (e: Exception) {
-            Result.failure(e)
+        } catch (e: IOException) {
+            Result.failure(Exception("Network error: ${e.message}"))
+        } catch (e: HttpException) {
+            Result.failure(Exception("HTTP error: ${e.message}"))
         }
+    }
+
+    companion object {
+        private const val SUCCESS_CODE = 200
     }
 }
