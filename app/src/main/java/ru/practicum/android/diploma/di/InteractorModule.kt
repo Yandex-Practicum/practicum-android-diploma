58ab711382a86df0a9c44ce8a7f34a85042b6872
alekla0126 <alekla0126@gmail.com>
2024-06-19 19:03:01 +0300

    Resolved

--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -1,12 +1,8 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
-import ru.practicum.android.diploma.domain.api.FavoritesVacancyInteractor
-import ru.practicum.android.diploma.domain.api.SearchInteractor
-import ru.practicum.android.diploma.domain.impl.FavoritesVacancyInteractorImpl
-import ru.practicum.android.diploma.domain.impl.SearchInteractorImpl
 
 val interactorModule = module {
-    single<FavoritesVacancyInteractor> { FavoritesVacancyInteractorImpl() }
-    single<SearchInteractor> { SearchInteractorImpl(get()) }
+    // single<FavoritesVacancyInteractor> { FavoritesVacancyInteractorImpl() }
+    // single<SearchInteractor> { SearchInteractorImpl(get()) }
 }
