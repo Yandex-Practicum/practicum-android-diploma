5d90ed61f39a776ee2427663d2dce81395cfeac8
dencharski <dencharski@yandex.ru>
2024-06-20 21:01:17 +0200

    -create FavoriteVacancyRepositoryImpl, edtit FavoritesFragment and others. some edit6

--- app/src/main/java/ru/practicum/android/diploma/data/repository/FavoritesVacancyRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/repository/FavoritesVacancyRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package ru.practicum.android.diploma.data.repository
 
+import android.media.MediaParser.ParsingException
+import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
@@ -10,19 +12,18 @@ import ru.practicum.android.diploma.domain.models.Vacancy
 import java.io.IOException
 
 class FavoritesVacancyRepositoryImpl(
-    private val appDatabase: AppDatabase,
-    private val vacancyConverter: VacancyConverter
+    private val appDatabase: AppDatabase, private val vacancyConverter: VacancyConverter
 ) : FavoritesVacancyRepository {
+    private val teg = "favorites"
     override suspend fun getAllFavoritesVacancy() = flow {
-
         emit(withContext(Dispatchers.IO) {
             try {
                 appDatabase.favoriteVacancyDao().getFavorites().map { vacancy -> vacancyConverter.map(vacancy) }
             } catch (e: IOException) {
+                Log.e(teg, "Caught exception:  ${e.message}")
                 null
             }
         })
-
     }
 
     override suspend fun getOneFavoriteVacancy(vacancyId: String): Vacancy {
