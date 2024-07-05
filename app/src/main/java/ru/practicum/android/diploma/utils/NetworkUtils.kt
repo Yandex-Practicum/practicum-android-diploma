6ce9a6884ee49fe45813652a377314760dc3e30d
PavlovAlex <al3ksei.pav@yandex.ru>
2024-07-05 23:00:00 +0300

    функция проверки доступности интернета #32.3

--- app/src/main/java/ru/practicum/android/diploma/utils/NetworkUtils.kt
+++ app/src/main/java/ru/practicum/android/diploma/utils/NetworkUtils.kt
@@ -18,16 +18,23 @@ object NetworkUtils {
 
     fun isInternetAvailable(context: Context): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        var result = false
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val network = connectivityManager.activeNetwork ?: return false
-            val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
-            return networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
-                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
-                networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+            val network = connectivityManager.activeNetwork
+            if (network != null) {
+                val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+                if (networkCapabilities != null) {
+                    result = networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                        networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                        networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+                }
+            }
         } else {
             val activeNetworkInfo = connectivityManager.activeNetworkInfo
-            return activeNetworkInfo != null && activeNetworkInfo.isConnected
+            result = activeNetworkInfo != null && activeNetworkInfo.isConnected
         }
+
+        return result
     }
 }
