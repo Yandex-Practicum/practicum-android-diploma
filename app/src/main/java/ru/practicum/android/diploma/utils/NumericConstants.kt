28d30cec685f6f2ce1e441a427640d08d10229ed
NekhoroshkinaElena <muxaewaelena225@yandex.ru>
2024-07-05 21:08:45 +0300

    Добавила строковые ресурсы

--- app/src/main/java/ru/practicum/android/diploma/utils/NumericConstants.kt
+++ app/src/main/java/ru/practicum/android/diploma/utils/NumericConstants.kt
@@ -1,8 +1,7 @@
 package ru.practicum.android.diploma.utils
 
 object NumericConstants {
-    // время
-    const val HALF_SECOND = 500L // кулдаун для клика
-    const val ONE_SECOND = 1000L // одна секунда
-    const val TWO_SECONDS = 2000L // две секунды
+    const val HALF_SECOND = 500L
+    const val ONE_SECOND = 1000L
+    const val TWO_SECONDS = 2000L
 }
