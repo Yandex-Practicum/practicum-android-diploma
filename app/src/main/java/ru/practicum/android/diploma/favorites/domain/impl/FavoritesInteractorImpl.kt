3ae75a351b28ed42d2a6a177aea8b59f4ab6280d
Сергей Михалёв <snt_mail@bk.ru>
2024-07-05 19:25:08 +0500

    исправления detekt

--- app/src/main/java/ru/practicum/android/diploma/favorites/domain/impl/FavoritesInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/domain/impl/FavoritesInteractorImpl.kt
@@ -2,5 +2,4 @@ package ru.practicum.android.diploma.favorites.domain.impl
 
 import ru.practicum.android.diploma.favorites.domain.api.FavoritesInteractor
 
-class FavoritesInteractorImpl : FavoritesInteractor {
-}
+class FavoritesInteractorImpl : FavoritesInteractor
