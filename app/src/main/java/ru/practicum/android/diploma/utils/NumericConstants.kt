b4f49b7ace1a50620eae31bcb1444712f384a4a0
PavlovAlex <al3ksei.pav@yandex.ru>
2024-07-05 19:48:18 +0300

    добавил в Utils функцию debounce (~ 0,5 часа) #18

---
+++ app/src/main/java/ru/practicum/android/diploma/utils/NumericConstants.kt
@@ -0,0 +1,8 @@
+package ru.practicum.android.diploma.utils
+
+object NumericConstants {
+    // время
+    const val HALF_SECOND = 500L // кулдаун для клика
+    const val ONE_SECOND = 1000L // одна секунда
+    const val TWO_SECONDS = 2000L // две секунды
+}
