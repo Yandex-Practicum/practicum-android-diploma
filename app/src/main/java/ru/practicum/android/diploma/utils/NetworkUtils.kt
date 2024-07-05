52d149e84c27dea47450113279bff48bde4cfcf3
PavlovAlex <al3ksei.pav@yandex.ru>
2024-07-05 22:51:13 +0300

    функция проверки доступности интернета #32

--- app/src/main/java/ru/practicum/android/diploma/utils/NetworkUtils.kt
+++ app/src/main/java/ru/practicum/android/diploma/utils/NetworkUtils.kt
@@ -7,27 +7,27 @@ import android.os.Build
 
 object NetworkUtils {
 
-/** Утилита проверки доступности интернета
- * пример:
- *             if (NetworkUtils.isInternetAvailable(this)) {
- *                 Log.d("=== LOG ===", "интернет есть, запускаем поиск")
- *             } else {
- *                 Log.d("=== LOG ===", "интернета нет, вызываем электрика")
- *             }
-*/
+    /** Утилита проверки доступности интернета
+     * пример:
+     *             if (NetworkUtils.isInternetAvailable(this)) {
+     *                 Log.d("=== LOG ===", "интернет есть, запускаем поиск")
+     *             } else {
+     *                 Log.d("=== LOG ===", "интернета нет, вызываем электрика")
+     *             }
+     */
 
     fun isInternetAvailable(context: Context): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             val network = connectivityManager.activeNetwork ?: return false
             val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
-            networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+            return networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
                 networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
                 networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
         } else {
             val activeNetworkInfo = connectivityManager.activeNetworkInfo
-            activeNetworkInfo != null && activeNetworkInfo.isConnected
+            return activeNetworkInfo != null && activeNetworkInfo.isConnected
         }
     }
 }
