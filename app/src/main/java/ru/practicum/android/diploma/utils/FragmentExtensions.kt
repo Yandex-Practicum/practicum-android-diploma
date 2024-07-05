44e902a4d0fc158c18cfeb22949b47713dccf45a
PavlovAlex <al3ksei.pav@yandex.ru>
2024-07-05 21:37:02 +0300

    Подготовить snackbar (~2 часа) #20

---
+++ app/src/main/java/ru/practicum/android/diploma/utils/FragmentExtensions.kt
@@ -0,0 +1,18 @@
+package ru.practicum.android.diploma.utils
+
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import ru.practicum.android.diploma.R
+
+//Snackbar как альтернатива Toast
+
+fun Fragment.showSnackbar(message: String) {
+    val snackbar = Snackbar.make(
+        requireView(),
+        message,
+        Snackbar.LENGTH_SHORT
+    )
+    val snackbarView = snackbar.view
+    snackbarView.setBackgroundResource(R.color.black_and_white)
+    snackbar.show()
+}
--- app/src/main/java/ru/practicum/android/diploma/utils/NumericConstants.kt
+++ app/src/main/java/ru/practicum/android/diploma/utils/NumericConstants.kt
@@ -3,6 +3,6 @@ package ru.practicum.android.diploma.utils
 object NumericConstants {
     // время
     const val HALF_SECOND = 500L // кулдаун для клика
-    const val ONE_SECOND = 1000L // одна секунда
-    const val TWO_SECONDS = 2000L // две секунды
+    const val ONE_SECOND = 1000L
+    const val TWO_SECONDS = 2000L
 }
