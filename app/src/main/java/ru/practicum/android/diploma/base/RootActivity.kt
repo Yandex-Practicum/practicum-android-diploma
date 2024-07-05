7581047b291d57b078be9b6be7902ca4f20fe9ff
NekhoroshkinaElena <muxaewaelena225@yandex.ru>
2024-07-05 19:36:48 +0300

    Добавила нижнюю панель навигации

--- app/src/main/java/ru/practicum/android/diploma/base/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/base/RootActivity.kt
@@ -2,7 +2,9 @@ package ru.practicum.android.diploma.base
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
@@ -19,6 +21,18 @@ class RootActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.fcvContainer) as NavHostFragment
         val navController = navHostFragment.navController
 
+        binding?.bnvBottomNav?.setupWithNavController(navController)
+        val hideList = listOf(
+            R.id.vacancyFragment,
+            R.id.filterFragment,
+            R.id.locationFragment,
+            R.id.countryFragment,
+            R.id.regionFragment,
+            R.id.sectorFragment
+        )
+        navController.addOnDestinationChangedListener { _, navDestination, _ ->
+            binding?.bnvBottomNav?.isVisible = !hideList.contains(navDestination.id)
+        }
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
     }
