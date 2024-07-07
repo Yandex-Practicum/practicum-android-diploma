e8cf64a3ba741a2a415cfa7331f0d7bc154e57ff
Сергей Михалёв <snt_mail@bk.ru>
2024-07-07 18:58:08 +0500

    изменён нейминг результирующих кодов

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/RetrofitClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/RetrofitClient.kt
@@ -4,10 +4,10 @@ import android.content.Context
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.HttpException
-import ru.practicum.android.diploma.search.data.dto.REST_BAD_REQUEST
-import ru.practicum.android.diploma.search.data.dto.REST_SERVER_ERROR
-import ru.practicum.android.diploma.search.data.dto.REST_SUCCESS
+import ru.practicum.android.diploma.search.data.dto.RESULT_CODE_BAD_REQUEST
 import ru.practicum.android.diploma.search.data.dto.RESULT_CODE_NO_INTERNET
+import ru.practicum.android.diploma.search.data.dto.RESULT_CODE_SERVER_ERROR
+import ru.practicum.android.diploma.search.data.dto.RESULT_CODE_SUCCESS
 import ru.practicum.android.diploma.search.data.dto.Resp
 import ru.practicum.android.diploma.search.data.dto.SearchRequest
 import ru.practicum.android.diploma.utils.isInternetAvailable
@@ -31,22 +31,22 @@ class RetrofitClient(
                 when (dto) {
                     is VacancyRequest -> {
                         val response = jobApiService.getVacancy(dto.id)
-                        response.apply { resultCode = REST_SUCCESS }
+                        response.apply { resultCode = RESULT_CODE_SUCCESS }
                     }
 
                     is SearchRequest -> {
                         val response = jobApiService.searchVacancies(options = dto.options)
-                        response.apply { resultCode = REST_SUCCESS }
+                        response.apply { resultCode = RESULT_CODE_SUCCESS }
                     }
 
                     else -> {
-                        Resp().apply { resultCode = REST_BAD_REQUEST }
+                        Resp().apply { resultCode = RESULT_CODE_BAD_REQUEST }
                     }
                 }
 
             } catch (e: HttpException) {
                 println("RetrofitClient error code: ${e.code()} message: ${e.message}")
-                Resp().apply { resultCode = REST_SERVER_ERROR }
+                Resp().apply { resultCode = RESULT_CODE_SERVER_ERROR }
             }
         }
     }
