5a7fd95b710463d29f62fca71b131532cf0ee86c
Aleksey Kalugin <mralexjoin@gmail.com>
2024-07-07 18:44:18 +0300

    Константы перемещены в buildConfig

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -5,13 +5,11 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.favourites.data.db.MainDB
 import ru.practicum.android.diploma.search.data.network.JobApiService
 import ru.practicum.android.diploma.search.data.network.NetworkClient
 import ru.practicum.android.diploma.search.data.network.RetrofitClient
-
-const val BASE_URL = "https://api.hh.ru/"
-const val DB_NAME = "FindYourJob_db"
 
 val dataModule = module {
 
@@ -24,7 +22,7 @@ val dataModule = module {
 
     single<JobApiService> {
         Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(BuildConfig.HH_BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(JobApiService::class.java)
@@ -34,7 +32,7 @@ val dataModule = module {
         Room.databaseBuilder(
             context = androidContext(),
             klass = MainDB::class.java,
-            name = DB_NAME
+            name = BuildConfig.DATABASE_FILE_NAME
         ).build()
     }
 
