d3a143d36facd634acc1c24368156b78387d0020
Aleksey Kalugin <mralexjoin@gmail.com>
2024-07-10 21:05:54 +0300

    Подготовлен адаптер для пагинации и небольшой фикс отображения вакансии

--- app/src/main/java/ru/practicum/android/diploma/search/ui/adapter/VacancyViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/adapter/VacancyViewHolder.kt
@@ -1,17 +1,25 @@
 package ru.practicum.android.diploma.search.ui.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.VacancyItemBinding
 import ru.practicum.android.diploma.search.domain.models.Vacancy
 
-class VacancyViewHolder(private val binding: VacancyItemBinding) :
+class VacancyViewHolder(
+    parentView: ViewGroup,
+    val binding: VacancyItemBinding = VacancyItemBinding.inflate(
+        LayoutInflater.from(parentView.context), parentView, false
+    ),
+) :
     RecyclerView.ViewHolder(binding.root) {
 
     fun bind(vacancy: Vacancy) {
-        binding.tvTitle.text = vacancy.name
-        binding.tvArea.text = vacancy.area
+        binding.tvTitle.text = itemView.resources.getString(
+            R.string.vacancy_item_title_template, vacancy.name, vacancy.area
+        )
         binding.tvSalary.text = vacancy.salary
         binding.tvCompany.text = vacancy.company
         Glide.with(itemView)
