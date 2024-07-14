091f49622310074f1948bcdcb45b3402ab9ee298
NekhoroshkinaElena <muxaewaelena225@yandex.ru>
2024-07-14 12:19:43 +0300

    fix: добавлен денежный символ валюты

--- app/src/main/java/ru/practicum/android/diploma/utils/Formatter.kt
+++ app/src/main/java/ru/practicum/android/diploma/utils/Formatter.kt
@@ -9,20 +9,20 @@ fun formattingSalary(salaryFrom: Int?, salaryTo: Int?, currency: String, context
         salaryFrom != null && salaryTo == null -> context.getString(
             R.string.salary_from,
             formatNumber(salaryFrom),
-            currency
+            formatCurrency(currency)
         )
 
         salaryFrom == null && salaryTo != null -> context.getString(
             R.string.salary_to,
             formatNumber(salaryTo),
-            currency
+            formatCurrency(currency)
         )
 
         salaryFrom != null && salaryTo != null -> context.getString(
             R.string.salary_from_to,
             formatNumber(salaryFrom),
             formatNumber(salaryTo),
-            currency
+            formatCurrency(currency)
         )
 
         else -> context.getString(R.string.salary_not_specified)
@@ -31,4 +31,22 @@ fun formattingSalary(salaryFrom: Int?, salaryTo: Int?, currency: String, context
 
 private fun formatNumber(number: Int): String {
     return NumberFormat.getInstance().format(number)
+}
+
+private fun formatCurrency(currency: String): String {
+    return when (currency) {
+        "RUR", "RUB" -> " ₽"
+        "BYR" -> "Br"
+        "USD" -> "$"
+        "EUR" -> "€"
+        "KZT" -> "₸"
+        "UAH" -> "₴"
+        "AZN" -> "₼"
+        "UZS" -> "Soʻm"
+        "GEL" -> "₾"
+        "KGS" -> "с"
+        else -> {
+            currency
+        }
+    }
 }
