af5aed36f4585763c0325b1f8eaad00f707803af
G-dev-ui <koroliov2108@mail.ru>
2024-08-02 22:00:50 +0300

    Добавил утилиты для дебаунса и проверки интернета(Исправил отступы)

--- app/src/main/java/ru/practicum/android/diploma/util/NetworkUtils.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/NetworkUtils.kt
@@ -5,20 +5,17 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 
-    fun isInternetAvailable(context: Context): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-        var result = false
-        if (capabilities != null) {
-            result = when {
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                else -> false
-            }
+fun isInternetAvailable(context: Context): Boolean {
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+    var result = false
+    if (capabilities != null) {
+        result = when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
         }
-        return result
     }
-
-
-
+    return result
+}
