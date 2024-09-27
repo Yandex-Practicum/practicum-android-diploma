69c8b29f79e18f8b682edf7aab5d923e70eb2a83
Monomythconvergence <shoulddoisuppose@gmail.com>
2024-09-27 23:20:07 +0300

    task_13

--- app/src/main/java/ru/practicum/android/diploma/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/RootActivity.kt
@@ -2,6 +2,7 @@ package ru.practicum.android.diploma
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -13,17 +14,36 @@ class RootActivity : AppCompatActivity() {
     private val binding: ActivityRootBinding get() = _binding!!
 
     private val navController: NavController by lazy {
-        (supportFragmentManager.findFragmentById(R.id.root_fragments) as NavHostFragment).navController
+        (supportFragmentManager.findFragmentById(R.id.rootFragments) as NavHostFragment).navController
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navController.addOnDestinationChangedListener { _, destination, _ -> //скрываем/показываем нижнюю панель
+            when (destination.id) {
+                R.id.search_navigation, R.id.team_navigation, R.id.favorites_navigation,R.id.root_navigation,
+                ru.practicum.android.diploma.search.R.id.searchFragment,        // Временные заглушки до оформления
+                ru.practicum.android.diploma.favorites.R.id.favoritesFragment,  // NavGraph'а
+                ru.practicum.android.diploma.team.R.id.teamFragment,            // По установке навигации - удалить
+                -> {
+                    binding.bottomNavigationMenu.isVisible = true
+                    binding.navigationPanelDivider.isVisible = true
+                }
+                else -> {
+                    binding.bottomNavigationMenu.isVisible = false
+                    binding.navigationPanelDivider.isVisible = false
+                }
+            }
+        }
+
         binding.bottomNavigationMenu.setupWithNavController(navController)
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
+
     }
 
     private fun networkRequestExample(accessToken: String) {
