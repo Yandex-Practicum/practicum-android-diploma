d85bdbdcb5a0ea06f8f90a6be96ad199d603e5dc
MSagGik <smw.pantheon@bk.ru>
2024-09-27 15:05:55 +0300

    Make fix bug check "Detekt"

--- common_utils/src/main/java/ru/practicum/android/diploma/commonutils/NetworkUtils.kt
+++ common_utils/src/main/java/ru/practicum/android/diploma/commonutils/NetworkUtils.kt
@@ -5,16 +5,12 @@ import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 
 class NetworkUtils {
-    private fun isConnected(context: Context): Boolean {
+    public fun isConnected(context: Context): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-        if (capabilities != null) {
-            when {
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
-                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
-            }
-        }
-        return false
+        return (capabilities != null
+            && (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+            || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+            || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)))
     }
 }
