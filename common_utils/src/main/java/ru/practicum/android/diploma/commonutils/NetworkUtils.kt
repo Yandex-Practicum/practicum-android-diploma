83fccd1dcf6f59064bc26e1de023f22acc6e2e41
MSagGik <smw.pantheon@bk.ru>
2024-09-28 21:23:09 +0300

    Make refactor isConnected()

--- common_utils/src/main/java/ru/practicum/android/diploma/commonutils/NetworkUtils.kt
+++ common_utils/src/main/java/ru/practicum/android/diploma/commonutils/NetworkUtils.kt
@@ -4,13 +4,11 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 
-class NetworkUtils {
-    fun isConnected(context: Context): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-        return (capabilities != null
-            && (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
-                || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
-                || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)))
-    }
+fun Context.isConnected(): Boolean {
+    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+    return (capabilities != null
+        && (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+        || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+        || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)))
 }
