435e629eb509f89c5f7c2756f47c6a2c725a771b
Захаров Павел <pzakharov83@gmail.com>
2024-09-28 20:37:09 +0300

    replace id with alias for ksp and parcelize

--- data_network/build.gradle.kts
+++ data_network/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("kotlin-parcelize")
-    id("com.google.devtools.ksp")
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.ksp)
 }
 
 android {
