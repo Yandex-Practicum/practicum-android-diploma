1cfdfcf5f92e7a5e02576cbce58b254d58eecae1
Захаров Павел <pzakharov83@gmail.com>
2024-09-29 13:04:29 +0300

    add vacancy detail

--- search/src/main/java/ru/practicum/android/diploma/search/domain/usecase/VacanciesInteractor.kt
+++ search/src/main/java/ru/practicum/android/diploma/search/domain/usecase/VacanciesInteractor.kt
@@ -1,10 +1,12 @@
 package ru.practicum.android.diploma.search.domain.usecase
 
 import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.search.domain.Vacancy
+import ru.practicum.android.diploma.search.domain.VacancyDetail
 
 interface VacanciesInteractor {
-    fun searchVacancies(options: Map<String, String>): Flow<String>
-    fun listVacancy(id: String): Flow<String>
+    fun searchVacancies(options: Map<String, String>): Flow<List<Vacancy>>
+    fun listVacancy(id: String): Flow<VacancyDetail>
     fun listAreas(): Flow<String>
     fun listIndustries(): Flow<String>
 }
--- search/src/main/java/ru/practicum/android/diploma/search/domain/usecase/impl/VacanciesInteractorImpl.kt
+++ search/src/main/java/ru/practicum/android/diploma/search/domain/usecase/impl/VacanciesInteractorImpl.kt
@@ -2,13 +2,15 @@ package ru.practicum.android.diploma.search.domain.usecase.impl
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import ru.practicum.android.diploma.search.domain.Vacancy
+import ru.practicum.android.diploma.search.domain.VacancyDetail
 import ru.practicum.android.diploma.search.domain.repository.VacanciesRepository
 import ru.practicum.android.diploma.search.domain.usecase.VacanciesInteractor
 
 internal class VacanciesInteractorImpl(private val repository: VacanciesRepository) : VacanciesInteractor {
-    override fun searchVacancies(options: Map<String, String>): Flow<String> = flow {}
+    override fun searchVacancies(options: Map<String, String>): Flow<List<Vacancy>> = flow {}
 
-    override fun listVacancy(id: String): Flow<String> = flow {}
+    override fun listVacancy(id: String): Flow<VacancyDetail> = flow {}
 
     override fun listAreas(): Flow<String> = flow {}
 
