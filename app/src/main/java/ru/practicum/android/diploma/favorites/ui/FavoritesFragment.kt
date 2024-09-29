89a95949bbdde8d70150a85d7bcb115f3093a279
peter <peter.hero2000@yandex.ru>
2024-09-29 21:00:48 +0300

    Сверстал экран команды и добавил имена в strings.xml. Так же добавил в стили текстов цвета из тем тк на макетах они почти всегда одинаковые. +поменял лейауты в FavoritesFragment и SearchFragment на заглушки

--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/FavoritesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/FavoritesFragment.kt
@@ -14,6 +14,6 @@ class FavoritesFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.activity_root, container, false)
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
     }
 }
