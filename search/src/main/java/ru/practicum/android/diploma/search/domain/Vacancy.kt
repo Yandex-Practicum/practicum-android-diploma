59997d494aef1895018af5ec91a64278f1c141cd
Захаров Павел <pzakharov83@gmail.com>
2024-09-29 19:46:29 +0300

    fix extrnal deps

--- search/src/main/java/ru/practicum/android/diploma/search/domain/Vacancy.kt
+++ search/src/main/java/ru/practicum/android/diploma/search/domain/Vacancy.kt
@@ -1,11 +1,9 @@
 package ru.practicum.android.diploma.search.domain
 
-import android.icu.util.Currency
-
 data class Vacancy(
     val title: String,
     val companyName: String,
     val salary: Int?,
     val salaryCurrency: String?,
-    val companyLogo: String?
+    val companyLogo: String?,
 )
