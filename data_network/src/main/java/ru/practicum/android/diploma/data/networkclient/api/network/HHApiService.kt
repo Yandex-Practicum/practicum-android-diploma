2d537adced1d66b36ff508ac643caf8bdc3cc33e
MSagGik <smw.pantheon@bk.ru>
2024-09-29 15:22:39 +0300

    Make a bug fix for the "detekt" check

--- data_network/src/main/java/ru/practicum/android/diploma/data/networkclient/api/network/HHApiService.kt
+++ data_network/src/main/java/ru/practicum/android/diploma/data/networkclient/api/network/HHApiService.kt
@@ -10,33 +10,40 @@ import ru.practicum.android.diploma.data.networkclient.api.dto.HHIndustriesRespo
 import ru.practicum.android.diploma.data.networkclient.api.dto.HHRegionsResponse
 import ru.practicum.android.diploma.data.networkclient.api.dto.HHVacanciesResponse
 import ru.practicum.android.diploma.data.networkclient.api.dto.HHVacancyDetailResponse
+import ru.practicum.android.diploma.data.networkclient.api.network.HHApiService.ApiConstants.HH_HEADER_ONE
+import ru.practicum.android.diploma.data.networkclient.api.network.HHApiService.ApiConstants.HH_TOKEN
 
 internal interface HHApiService {
-    @Headers("HH-User-Agent: CareerRepository/1.0 (zpe25@yandex.ru)")
+    @Headers(HH_HEADER_ONE)
     @GET("/vacancies")
     suspend fun searchVacancies(
         @QueryMap options: Map<String, String>,
-        @Header("Authorization") token: String = "Bearer ${BuildConfig.HH_ACCESS_TOKEN}"
+        @Header("Authorization") token: String = HH_TOKEN
     ): HHVacanciesResponse
 
-    @Headers("HH-User-Agent: CareerRepository/1.0 (zpe25@yandex.ru)")
+    @Headers(HH_HEADER_ONE)
     @GET("/industries")
     suspend fun searchIndustries(
         @QueryMap options: Map<String, String>?,
-        @Header("Authorization") token: String = "Bearer ${BuildConfig.HH_ACCESS_TOKEN}"
+        @Header("Authorization") token: String = HH_TOKEN
     ): HHIndustriesResponse
 
-    @Headers("HH-User-Agent: CareerRepository/1.0 (zpe25@yandex.ru)")
+    @Headers(HH_HEADER_ONE)
     @GET("/areas")
     suspend fun searchRegions(
         @QueryMap options: Map<String, String>?,
-        @Header("Authorization") token: String = "Bearer ${BuildConfig.HH_ACCESS_TOKEN}"
+        @Header("Authorization") token: String = HH_TOKEN
     ): HHRegionsResponse
 
-    @Headers("HH-User-Agent: CareerRepository/1.0 (zpe25@yandex.ru)")
+    @Headers(HH_HEADER_ONE)
     @GET("/vacancies/{vacancy_id}")
     suspend fun getVacancy(
         @Path("vacancy_id") vacancyId: String,
-        @Header("Authorization") token: String = "Bearer ${BuildConfig.HH_ACCESS_TOKEN}"
+        @Header("Authorization") token: String = HH_TOKEN
     ): HHVacancyDetailResponse
+
+    private object ApiConstants {
+        const val HH_HEADER_ONE = "HH-User-Agent: CareerRepository/1.0 (zpe25@yandex.ru))"
+        const val HH_TOKEN = "Bearer ${BuildConfig.HH_ACCESS_TOKEN}"
+    }
 }
