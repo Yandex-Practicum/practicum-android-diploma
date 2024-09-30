d66f53a5ca0fc13630d889fa05396e285bb269ed
ber9z <berez.a@bk.ru>
2024-09-30 14:13:56 +0300

    добавление полей в VacancyDetailsEntity

--- app/src/main/java/ru/practicum/android/diploma/database/entities/VacancyDetailsEntity.kt
+++ app/src/main/java/ru/practicum/android/diploma/database/entities/VacancyDetailsEntity.kt
@@ -6,4 +6,13 @@ import ru.practicum.android.diploma.database.dao.VacancyDetailsDao
 @Entity(tableName = VacancyDetailsDao.TABLE_NAME)
 data class VacancyDetailsEntity(
     val id: String,
+    val name: String,
+    val salary: String?,
+    val area: String,
+    val address: String?,
+    val experience: String?,
+    val schedule: String?,
+    val employment: String?,
+    val description: String,
+    val keySkill: String,
 )
