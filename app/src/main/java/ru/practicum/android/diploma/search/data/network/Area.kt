e06c02550a76355202472200ca5e2c19318bf06c
Raigou <paladi.post@gmail.com>
2024-09-30 20:05:39 +0300

    DataClass Area

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/Area.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/Area.kt
@@ -2,7 +2,7 @@ package ru.practicum.android.diploma.search.data.network
 
 data class Area(
     val id: String,
-    val parent_id: String?,
+    val parentId: String?,
     val name: String,
     val areas: List<Area> // Список дочерних регионов
 )
