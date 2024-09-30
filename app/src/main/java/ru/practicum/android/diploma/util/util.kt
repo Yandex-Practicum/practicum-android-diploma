7386fb7f2f3ee73b4476d747506c11eaa9e98864
peter <peter.hero2000@yandex.ru>
2024-10-01 00:39:23 +0300

    Добавил функцию debounce в папку util Создает функцию, которая ограничивает вызов [action] не чаще, чем раз в [delayMs] миллисекунд.  * delayMs Задержка в миллисекундах перед вызовом [action]. scope Корутинный Scope, в котором будет выполняться задержка. action Функция, которая будет вызвана после задержки. return Функция, принимающая параметр [T] и вызывающая [action] с задержкой.

---
+++ app/src/main/java/ru/practicum/android/diploma/util/util.kt
@@ -0,0 +1,26 @@
+package ru.practicum.android.diploma.util
+
+import kotlinx.coroutines.*
+
+/**
+ * Функция, которая ограничивает вызов [action] не чаще, чем раз в [delayMs] миллисекунд.
+ * Пример использования:
+ * private val debouncedSearch = debounce<String>(500L, scope) { query ->
+ *         performSearch(query)
+ *}
+ */
+
+fun <T> debounce(
+    delayMs: Long,
+    scope: CoroutineScope,
+    action: (T) -> Unit
+): (T) -> Unit {
+    var debounceJob: Job? = null
+    return { param: T ->
+        debounceJob?.cancel()
+        debounceJob = scope.launch {
+            delay(delayMs)
+            action(param)
+        }
+    }
+}
