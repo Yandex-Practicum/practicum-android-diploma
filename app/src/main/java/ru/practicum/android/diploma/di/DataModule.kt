e3f15f664aa0b52f0333393cf78a4fe6eb2bc83f
ber9z <berez.a@bk.ru>
2024-09-30 13:08:26 +0300

    добавление бд в DI dataModule

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,2 +1,15 @@
 package ru.practicum.android.diploma.di
 
+import androidx.room.Room
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import ru.practicum.android.diploma.database.AppDatabase
+
+val dataModule = module {
+
+    single {
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database.db")
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+}
