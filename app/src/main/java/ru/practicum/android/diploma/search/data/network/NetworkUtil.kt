933590dec3c4f7481e7ea048b3fa491df60b3598
Raigou <paladi.post@gmail.com>
2024-10-01 00:48:34 +0300

    NetworkUtils

---
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/NetworkUtil.kt
@@ -0,0 +1,16 @@
+package ru.practicum.android.diploma.search.data.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+class NetworkUtils(private val context: Context) {
+
+    fun isNetworkAvailable(): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+        return networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
+    }
+}
