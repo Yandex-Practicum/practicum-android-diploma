ae5abf7fc1544ced63a2ce738edeca6510eeed5f
Raigou <paladi.post@gmail.com>
2024-10-01 21:56:48 +0300

    add catch error

--- app/src/main/java/ru/practicum/android/diploma/search/data/impl/SearchRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/impl/SearchRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package ru.practicum.android.diploma.search.data.impl
 
 import android.content.Context
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -11,12 +14,14 @@ import ru.practicum.android.diploma.search.domain.api.SearchRepository
 import ru.practicum.android.diploma.search.domain.models.Resource
 import ru.practicum.android.diploma.search.domain.models.Vacancy
 import ru.practicum.android.diploma.search.domain.models.VacancySearchParams
+import java.io.IOException
 
 class SearchRepositoryImpl(
     private val api: HeadHunterAPI,
     private val context: Context
 ) : SearchRepository {
 
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun searchVacancies(params: VacancySearchParams): Flow<Resource<List<Vacancy>>> = flow {
         if (NetworkUtils.isNetworkAvailable(context)) {
             try {
@@ -25,26 +30,30 @@ class SearchRepositoryImpl(
                 val options = params.toMap()
 
                 val response = api.searchVacancies(
-                    authToken = null,  // Токен авторизации
+                    authToken = null, // Токен авторизации
                     userAgent = "userAgent",
                     options = options
                 )
 
                 if (response.vacancies.isNotEmpty()) {
-                    emit(Resource.Success(
-                        data = response.vacancies,
-                        found = response.found,
-                        page = response.page,
-                        pages = response.pages
-                    ))
+                    emit(
+                        Resource.Success(
+                            data = response.vacancies,
+                            found = response.found,
+                            page = response.page,
+                            pages = response.pages
+                        )
+                    )
                 } else {
                     emit(Resource.Error("No vacancies found"))
                 }
+            } catch (e: IOException) {
+                emit(Resource.Error("Network error: ${e.localizedMessage}"))
+            } catch (e: HttpException) {
+                emit(Resource.Error("HTTP error: ${e.localizedMessage}"))
             } catch (e: Exception) {
                 emit(Resource.Error(e.localizedMessage ?: "Unknown error"))
             }
-        } else {
-            emit(Resource.Error("No Internet Connection"))
         }
     }.flowOn(Dispatchers.IO)
 }
