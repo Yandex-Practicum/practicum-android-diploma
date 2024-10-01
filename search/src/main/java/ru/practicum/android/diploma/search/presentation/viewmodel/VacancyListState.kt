9b69303a4f9e5a29fc42e9311b1418ff9416cd66
Захаров Павел <pzakharov83@gmail.com>
2024-10-01 15:45:41 +0300

    add presentation

--- search/src/main/java/ru/practicum/android/diploma/search/presentation/viewmodel/VacancyListState.kt
+++ search/src/main/java/ru/practicum/android/diploma/search/presentation/viewmodel/VacancyListState.kt
@@ -1,12 +1,12 @@
 package ru.practicum.android.diploma.search.presentation.viewmodel
 
-import ru.practicum.android.diploma.search.domain.models.VacancyDetail
+import ru.practicum.android.diploma.search.domain.models.Vacancy
 
 sealed interface VacancyListState {
 
     object Loading : VacancyListState
 
-    data class Content(val vacancyDetail: VacancyDetail) : VacancyListState
+    data class Content(val vacancies: List<Vacancy>) : VacancyListState
 
     data class Error(val reason: String) : VacancyListState
 
--- search/src/main/java/ru/practicum/android/diploma/search/presentation/viewmodel/VacancyListViewModel.kt
+++ search/src/main/java/ru/practicum/android/diploma/search/presentation/viewmodel/VacancyListViewModel.kt
@@ -2,6 +2,10 @@ package ru.practicum.android.diploma.search.presentation.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.search.domain.models.Vacancy
 import ru.practicum.android.diploma.search.domain.usecase.VacanciesInteractor
 
@@ -14,6 +18,35 @@ class VacancyListViewModel(
 
     val adapter = VacancyListAdapter {
         TODO("not implemented4")
+    }
+
+    private val _vacanciesStateLiveData = MutableLiveData<VacancyListState>()
+    fun observeVacanciesState(): LiveData<VacancyListState> = _vacanciesStateLiveData
+
+    fun searchVacancies(vacancyKeywords: String?) {
+        if (vacancyKeywords.isNullOrEmpty()) return
+        renderState(VacancyListState.Loading)
+        viewModelScope.launch {
+            vacancyInteractor.searchVacancies(mapOf("text" to vacancyKeywords)).collect { pair ->
+                processResult(pair.first, pair.second)
+            }
+        }
+    }
+
+    fun processResult(foundVacancies: List<Vacancy>?, errorMessage: String?) {
+        if (foundVacancies != null) {
+            vacancies.clear()
+            vacancies.addAll(foundVacancies)
+        }
+        when {
+            errorMessage != null -> renderState(VacancyListState.Error(errorMessage))
+            vacancies.isEmpty() -> renderState(VacancyListState.Empty)
+            else -> renderState(VacancyListState.Content(vacancies))
+        }
+    }
+
+    private fun renderState(state: VacancyListState) {
+        _vacanciesStateLiveData.postValue(state)
     }
 
     companion object {
