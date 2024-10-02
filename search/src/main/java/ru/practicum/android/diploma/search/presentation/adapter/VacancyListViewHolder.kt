4644efa7bc20e55c56411c844f7d06e11d4b6a8d
Захаров Павел <pzakharov83@gmail.com>
2024-10-02 21:41:27 +0300

    fix once again

--- search/src/main/java/ru/practicum/android/diploma/search/presentation/adapter/VacancyListViewHolder.kt
+++ search/src/main/java/ru/practicum/android/diploma/search/presentation/adapter/VacancyListViewHolder.kt
@@ -34,7 +34,6 @@ class VacancyListViewHolder(parent: View) : RecyclerView.ViewHolder(parent) {
                     model.salaryCurrency
                 )
             }
-
             model.salaryMin != null -> {
                 itemView.context.getString(
                     ru.practicum.android.diploma.common_ui.R.string.salary_from,
@@ -42,7 +41,6 @@ class VacancyListViewHolder(parent: View) : RecyclerView.ViewHolder(parent) {
                     model.salaryCurrency
                 )
             }
-
             model.salaryMax != null -> {
                 itemView.context.getString(
                     ru.practicum.android.diploma.common_ui.R.string.salary_to,
@@ -50,15 +48,12 @@ class VacancyListViewHolder(parent: View) : RecyclerView.ViewHolder(parent) {
                     model.salaryCurrency
                 )
             }
-
-            else -> {
-                itemView.context.getString(ru.practicum.android.diploma.common_ui.R.string.no_salary)
-            }
+            else -> { itemView.context.getString(ru.practicum.android.diploma.common_ui.R.string.no_salary) }
         }
         Glide.with(itemView).load(model.companyLogo).placeholder(R.drawable.placeholder_logo_item_favorite).transform(
-                CenterCrop(), RoundedCorners(
-                    Utils.doToPx(RADIUS_ROUND_VIEW, itemView.context.applicationContext)
-                )
-            ).transform().into(vacancyImage)
+            CenterCrop(), RoundedCorners(
+                Utils.doToPx(RADIUS_ROUND_VIEW, itemView.context.applicationContext)
+            )
+        ).transform().into(vacancyImage)
     }
 }
