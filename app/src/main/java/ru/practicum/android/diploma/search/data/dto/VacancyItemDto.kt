72788f17940b1a50a33377f562e8ca5433ea9847
ber9z <berez.a@bk.ru>
2024-10-03 19:20:17 +0300

    добавление конвертора

--- app/src/main/java/ru/practicum/android/diploma/search/data/dto/VacancyItemDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/dto/VacancyItemDto.kt
@@ -6,9 +6,9 @@ import ru.practicum.android.diploma.vacancy.data.dto.EmployerDto
 import ru.practicum.android.diploma.vacancy.data.dto.SalaryDto
 
 data class VacancyItemDto(
-    val address: AddressDto,
+    val address: AddressDto?,
     val area: AreaDto,
     val employer: EmployerDto,
     val name: String,
-    val salary: SalaryDto,
+    val salary: SalaryDto?,
 )
