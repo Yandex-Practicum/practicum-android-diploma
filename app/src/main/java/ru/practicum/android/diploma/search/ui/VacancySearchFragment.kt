4dd5adb0aecff6bc387e5372a11245153bca873e
Sergey <Cer.97@yandex.ru>
2024-10-03 16:26:02 +0300

    Добавил все плейсхолдеры экрана поиска и всплывающее синее окно с результатами происка

--- app/src/main/java/ru/practicum/android/diploma/search/ui/VacancySearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/VacancySearchFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.databinding.VacancySearchFragmentBinding
 
@@ -17,6 +18,14 @@ class VacancySearchFragment : Fragment() {
     ): View {
         _binding = VacancySearchFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.defaultSearchPlaceholder.isVisible = false
+        binding.notConnectedPlaceholder.isVisible = false
+        binding.notFoundPlaceholder.isVisible = false
+        binding.serverErrorPlaceholder.isVisible = true
     }
 
     override fun onDestroyView() {
