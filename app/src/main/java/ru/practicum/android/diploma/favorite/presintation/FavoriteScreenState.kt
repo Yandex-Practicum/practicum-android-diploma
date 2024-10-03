dcaddec822a8c55b5f8f4742d5abe9bb3cdd7b0f
SafarySun <arakalandadze@gmail.com>
2024-10-03 18:19:43 +0600

    Добавлен класс для отоброжения состоянийя экрана Избранных Вакансий

--- app/src/main/java/ru/practicum/android/diploma/favorite/presintation/FavoriteScreenState.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorite/presintation/FavoriteScreenState.kt
@@ -3,8 +3,7 @@ package ru.practicum.android.diploma.favorite.presintation
 import ru.practicum.android.diploma.vacancy.domain.entity.Vacancy
 
 sealed interface FavoriteScreenState {
-    data class ContentState(val vacancies: List<Vacancy>):FavoriteScreenState
+    data class ContentState(val vacancies: List<Vacancy>) : FavoriteScreenState
     object Empty : FavoriteScreenState
     object Error : FavoriteScreenState
-
 }
