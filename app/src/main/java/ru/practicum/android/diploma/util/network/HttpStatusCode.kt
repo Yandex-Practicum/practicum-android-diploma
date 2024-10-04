68e8257c54002cdda934c4f9ef802bc718bdf30a
ber9z <berez.a@bk.ru>
2024-10-04 18:27:54 +0300

    detekt fix 2

--- app/src/main/java/ru/practicum/android/diploma/util/network/HttpStatusCode.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/network/HttpStatusCode.kt
@@ -1,11 +1,8 @@
 package ru.practicum.android.diploma.util.network
 
-class HttpStatusCode {
-
-    companion object {
+object HttpStatusCode {
         const val OK = 200
         const val BAD_REQUEST = 400
         const val FORBIDDEN = 403
         const val NOT_FOUND = 404
-    }
 }
