fb5bab504091c30fd3efeded57f5f67b7c86e117
SafarySun <arakalandadze@gmail.com>
2024-10-05 02:20:13 +0600

    заменил интерфейс на класс

--- app/src/main/java/ru/practicum/android/diploma/favorite/presintation/FavoriteScreenState.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorite/presintation/FavoriteScreenState.kt
@@ -2,8 +2,8 @@ package ru.practicum.android.diploma.favorite.presintation
 
 import ru.practicum.android.diploma.vacancy.domain.entity.Vacancy
 
-sealed interface FavoriteScreenState {
-    data class ContentState(val vacancies: List<Vacancy>) : FavoriteScreenState
-    object Empty : FavoriteScreenState
-    object Error : FavoriteScreenState
+sealed class FavoriteScreenState {
+    data class ContentState(val vacancies: List<Vacancy>) : FavoriteScreenState()
+    object EmptyState : FavoriteScreenState()
+    object ErrorState : FavoriteScreenState()
 }
