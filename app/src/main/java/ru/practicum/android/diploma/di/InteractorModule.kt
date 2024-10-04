7073bf2d154a635cead5c52f3b7e17eb76b2b41f
ber9z <berez.a@bk.ru>
2024-10-04 17:37:52 +0300

    добавлены DI

--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -1,7 +1,12 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.search.domain.api.SearchVacancyInteractor
+import ru.practicum.android.diploma.search.domain.impl.SearchVacancyInteractorImpl
 
 val interactorModule = module {
 
+    single<SearchVacancyInteractor> {
+        SearchVacancyInteractorImpl(get())
+    }
 }
