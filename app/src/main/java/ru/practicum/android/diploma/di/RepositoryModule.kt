b55d09ec45a338023a426477d2cdd6a881ac73ab
SafarySun <arakalandadze@gmail.com>
2024-10-06 20:26:07 +0600

    Реализация методов Избранных вакансий Добавлен конвертер Немного изменил дао Функции Сделаны репозитории и Интеракторы

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -1,6 +1,8 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.favorite.data.FavoriteVacancyRepositoryImpl
+import ru.practicum.android.diploma.favorite.domain.api.FavoriteVacancyRepository
 import ru.practicum.android.diploma.search.data.impl.SearchVacancyRepositoryImpl
 import ru.practicum.android.diploma.search.domain.api.SearchVacancyRepository
 
@@ -8,5 +10,9 @@ val repositoryModule = module {
 
     single<SearchVacancyRepository> {
         SearchVacancyRepositoryImpl(get(), get())
+    }
+
+    single<FavoriteVacancyRepository> {
+        FavoriteVacancyRepositoryImpl(get(), get())
     }
 }
