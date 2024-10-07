0279143046c541b622e233c16137457d95788700
Захаров Павел <pzakharov83@gmail.com>
2024-10-07 22:30:21 +0300

    add indetation

--- common_utils/src/main/java/ru/practicum/android/diploma/commonutils/network/NetworkUtils.kt
+++ common_utils/src/main/java/ru/practicum/android/diploma/commonutils/network/NetworkUtils.kt
@@ -13,26 +13,20 @@ fun Context.isConnected(): Boolean {
     val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
     return (capabilities != null
         && (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
-            || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
-            || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)))
+        || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+        || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)))
 }
 
 fun <T, S> Context.executeNetworkRequest(
     request: suspend () -> T,
-    successHandler: (T) -> Resource<S>
+    successHandler: (T) -> Resource<S>,
 ): Flow<Resource<S>> =
     flow {
         val response: T = request.invoke()
         when ((response as Response).resultCode) {
-            HttpStatus.NO_INTERNET -> {
-                emit(
-                    Resource.Error(getString(R.string.check_network_connection))
-                )
-            }
+            HttpStatus.NO_INTERNET -> { emit(Resource.Error(getString(R.string.check_network_connection))) }
             HttpStatus.OK -> {
-                with(response as T) {
-                    emit(successHandler(response))
-                }
+                with(response as T) { emit(successHandler(response)) }
             }
             HttpStatus.CLIENT_ERROR -> {
                 emit(
@@ -43,19 +37,15 @@ fun <T, S> Context.executeNetworkRequest(
                 )
             }
             HttpStatus.CLIENT_ERROR_404 -> {
-                emit(
-                    Resource.Error(getString(
+                emit(Resource.Error(getString(
                             R.string.request_was_not_accepted_404
-                        ))
-                )
+                        )))
             }
             HttpStatus.SERVER_ERROR -> {
-                emit(
-                    Resource.Error(getString(
+                emit(Resource.Error(getString(
                             R.string.unexpected_error,
                             response.resultCode
-                        ))
-                )
+                        )))
             }
         }
     }
