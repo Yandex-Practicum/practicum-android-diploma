9024e1dfdde51ee4c1152885d3cdfd8251c64975
ber9z <berez.a@bk.ru>
2024-10-08 13:28:21 +0300

    добавлен viewModel в DI

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -3,9 +3,14 @@ package ru.practicum.android.diploma.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.search.presentation.VacancySearchViewModel
+import ru.practicum.android.diploma.vacancy.presentation.VacancyViewModel
 
 val viewModelModule = module {
     viewModel {
         VacancySearchViewModel(get())
+    }
+
+    viewModel { (vacancyId: String) ->
+        VacancyViewModel(vacancyId, get())
     }
 }
