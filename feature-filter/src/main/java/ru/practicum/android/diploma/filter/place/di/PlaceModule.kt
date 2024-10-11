b17f726d9bd2a2b383ace8ffeccf970902c24aea
MSagGik <smw.pantheon@bk.ru>
2024-10-11 17:20:37 +0300

    Make a bug fix for the "detekt" check

--- feature-filter/src/main/java/ru/practicum/android/diploma/filter/place/di/PlaceModule.kt
+++ feature-filter/src/main/java/ru/practicum/android/diploma/filter/place/di/PlaceModule.kt
@@ -2,11 +2,11 @@ package ru.practicum.android.diploma.filter.place.di
 
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import ru.practicum.android.diploma.filter.place.data.mappers.AreaMapper
 import ru.practicum.android.diploma.filter.place.data.repositoryimpl.network.RegionRepositoryImpl
 import ru.practicum.android.diploma.filter.place.domain.repository.RegionRepository
 import ru.practicum.android.diploma.filter.place.domain.usecase.RegionInteractor
 import ru.practicum.android.diploma.filter.place.domain.usecase.impl.RegionInteractorImpl
-import ru.practicum.android.diploma.filter.place.data.mappers.AreaMapper
 
 val placeModule = module {
     single<RegionInteractor> {
