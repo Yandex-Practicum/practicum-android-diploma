0b050d8e07124898bfc2f62e9d9fca52193b9044
Захаров Павел <pzakharov83@gmail.com>
2024-10-11 15:49:11 +0300

    add view adapter

--- feature-filter/src/main/java/ru/practicum/android/diploma/filter/domain/usecase/RegionInteractor.kt
+++ feature-filter/src/main/java/ru/practicum/android/diploma/filter/domain/usecase/RegionInteractor.kt
@@ -1,9 +1,7 @@
 package ru.practicum.android.diploma.filter.domain.usecase
 
 import kotlinx.coroutines.flow.Flow
-import ru.practicum.android.diploma.commonutils.Resource
 import ru.practicum.android.diploma.filter.domain.model.AreaInReference
-
 
 interface RegionInteractor {
     fun listAreas(): Flow<Pair<List<AreaInReference>?, String?>>
