2efeee567f6a0eb40563a8396b03ad66085cfec6
julia.vakulina <yuliavak@mail.ru>
2024-10-12 20:27:45 +0700

    Filter Screen layout and action

--- feature-filter/src/main/java/ru/practicum/android/diploma/filter/filter/presentation/ui/FilterFragment.kt
+++ feature-filter/src/main/java/ru/practicum/android/diploma/filter/filter/presentation/ui/FilterFragment.kt
@@ -1,11 +1,15 @@
 package ru.practicum.android.diploma.filter.filter.presentation.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import ru.practicum.android.diploma.commonutils.Utils.closeKeyBoard
 import ru.practicum.android.diploma.filter.R
 import ru.practicum.android.diploma.filter.databinding.FragmentFilterBinding
 
@@ -22,6 +26,7 @@ internal class FilterFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("ResourceAsColor")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.workPlace.setOnClickListener {
@@ -33,8 +38,29 @@ internal class FilterFragment : Fragment() {
         binding.buttonLeftFilter.setOnClickListener {
             findNavController().navigateUp()
         }
-    }
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.clearIconFilter.visibility = clearButtonVisibility(s)
+            }
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        binding.editTextFilter.addTextChangedListener(simpleTextWatcher)
+        binding.clearIconFilter.setOnClickListener {
+            binding.editTextFilter.setText("")
+            requireContext().closeKeyBoard(binding.editTextFilter)
+        }
 
+    }
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
