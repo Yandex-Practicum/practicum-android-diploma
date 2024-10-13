92e1774c4a6f0799ef3e5811ba2b213b89b64b01
Захаров Павел <pzakharov83@gmail.com>
2024-10-13 20:04:07 +0300

    add country return

--- feature-filter/src/main/java/ru/practicum/android/diploma/filter/place/presentation/ui/PlaceFragment.kt
+++ feature-filter/src/main/java/ru/practicum/android/diploma/filter/place/presentation/ui/PlaceFragment.kt
@@ -1,9 +1,11 @@
 package ru.practicum.android.diploma.filter.place.presentation.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.filter.R
@@ -15,8 +17,8 @@ internal class PlaceFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        savedInstanceState: Bundle?,
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentPlaceBinding.inflate(inflater, container, false)
         return binding.root
@@ -24,6 +26,10 @@ internal class PlaceFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, arguments?.getString(ARGS_COUNTRY_NAME) ?: "")
+        arguments?.getString(ARGS_COUNTRY_NAME)?.let {
+            binding.tvCountry.text = it
+        }
         binding.country.setOnClickListener {
             findNavController().navigate(R.id.action_placeFragment_to_countryFragment)
         }
@@ -38,5 +44,15 @@ internal class PlaceFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    companion object {
+        private const val TAG = "PlaceFragment"
+        private const val ARGS_COUNTRY_ID = "country_id"
+        private const val ARGS_COUNTRY_NAME = "country_name"
+
+        fun createArgs(countryId: String, countryName: String): Bundle =
+            bundleOf(ARGS_COUNTRY_ID to countryId, ARGS_COUNTRY_NAME to countryName)
+
     }
 }
