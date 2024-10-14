ff75b3ef8d583aa17b0c1a1c6f71b6e740b8e49f
Monomythconvergence <shoulddoisuppose@gmail.com>
2024-10-14 22:01:06 +0300

    69

--- feature-filter/src/main/java/ru/practicum/android/diploma/filter/filter/presentation/viewmodel/FilterViewModel.kt
+++ feature-filter/src/main/java/ru/practicum/android/diploma/filter/filter/presentation/viewmodel/FilterViewModel.kt
@@ -1,12 +1,28 @@
 package ru.practicum.android.diploma.filter.filter.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.practicum.android.diploma.filter.filter.domain.usecase.impl.FilterSPInteractor
 
+const val ARGS_INDUSTRY_ID = "industry_id"
+const val ARGS_INDUSTRY_NAME = "industry_name"
 class FilterViewModel(
     private val filterSPInteractor: FilterSPInteractor
 ) : ViewModel() {
+    private var _filterOptionsListLiveData: MutableLiveData<Map<String, String>> =
+        MutableLiveData<Map<String, String>>()
+    val filterOptionsListLiveData: LiveData<Map<String, String>> = _filterOptionsListLiveData
+
     init {
-        val sharedPrefs = filterSPInteractor.getAll()
+        loadFilterSettings()
+    }
+
+    fun loadFilterSettings() {
+        _filterOptionsListLiveData.postValue(filterSPInteractor.getAll())
+    }
+
+    fun putValue(key: String, value: String) {
+        filterSPInteractor.putValue(key, value)
     }
 }
