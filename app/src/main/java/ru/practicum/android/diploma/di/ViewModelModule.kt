e62d505bceed1ab9da45ba017543da2ba71aef43
ber9z <berez.a@bk.ru>
2024-10-15 12:15:05 +0300

    добавление VM в DI

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.favorite.presintation.FavoriteVacancyViewModel
+import ru.practicum.android.diploma.filters.presentation.IndustriesFilterViewModel
 import ru.practicum.android.diploma.search.presentation.VacancySearchViewModel
 import ru.practicum.android.diploma.vacancy.presentation.VacancyDetailsViewModel
 
@@ -17,5 +18,9 @@ val viewModelModule = module {
 
     viewModel { (vacancyId: String) ->
         VacancyDetailsViewModel(vacancyId, get(), get())
+    }
+
+    viewModel {
+        IndustriesFilterViewModel(get())
     }
 }
