e5e5b933ddccde4eed4a785d4e47a7e0af2a25e2
ber9z <berez.a@bk.ru>
2024-10-15 13:55:25 +0300

    адаптер и вьюхолдер

---
+++ app/src/main/java/ru/practicum/android/diploma/filters/ui/IndustriesRecyclerViewAdapter.kt
@@ -0,0 +1,36 @@
+package ru.practicum.android.diploma.filters.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.practicum.android.diploma.databinding.IndustryCardBinding
+import ru.practicum.android.diploma.filters.domain.models.Industry
+
+class IndustriesRecyclerViewAdapter(
+    private val clickListener: IndustryClickListener
+) : RecyclerView.Adapter<IndustriesViewHolder>() {
+
+    private val list = mutableListOf<Industry>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IndustriesViewHolder {
+        val view = LayoutInflater.from(parent.context)
+        return IndustriesViewHolder(IndustryCardBinding.inflate(view, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: IndustriesViewHolder, position: Int) {
+        val itemView = list[position]
+        holder.bind(itemView)
+
+        holder.itemView.setOnClickListener {
+            clickListener.onClick(itemView)
+        }
+    }
+
+    fun interface IndustryClickListener {
+        fun onClick(industry: Industry)
+    }
+}
