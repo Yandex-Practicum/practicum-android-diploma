84ab4c68fe883f2bfd0f98dd69f7541d858363a0
Vozzhd <Vozzhd@yandex.ru>
2024-10-15 21:28:37 +0300

    Исправления после ревью -добавлен интерцептор -добавлен код для работы с HTTP Toolkit

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -2,15 +2,18 @@ package ru.practicum.android.diploma.di
 
 import android.content.Context
 import androidx.room.Room
+import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.database.AppDatabase
 import ru.practicum.android.diploma.database.converters.VacancyDbConverter
 import ru.practicum.android.diploma.filters.data.dto.converter.ConverterForAreas
 import ru.practicum.android.diploma.search.data.converters.SalaryCurrencySignFormater
 import ru.practicum.android.diploma.search.data.converters.SearchVacancyNetworkConverter
+import ru.practicum.android.diploma.util.network.ApiKeyInterceptor
 import ru.practicum.android.diploma.util.network.HHApiService
 import ru.practicum.android.diploma.util.network.NetworkClient
 import ru.practicum.android.diploma.util.network.RetrofitNetworkClient
@@ -28,10 +31,20 @@ val dataModule = module {
         androidContext().getSharedPreferences("setting_preferences", Context.MODE_PRIVATE)
     }
 
+    single<ApiKeyInterceptor> {
+        ApiKeyInterceptor(BuildConfig.HH_ACCESS_TOKEN)
+    }
+
     single<HHApiService> {
+
         Retrofit.Builder()
             .baseUrl("https://api.hh.ru/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(get())
+                    .build()
+            )
             .build()
             .create(HHApiService::class.java)
     }
