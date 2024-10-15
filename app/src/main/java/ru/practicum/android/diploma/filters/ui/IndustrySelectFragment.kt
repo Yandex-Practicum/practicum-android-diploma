43ec63d35ec2c55bee117f1a8701ea8cabdc887e
ber9z <berez.a@bk.ru>
2024-10-15 14:48:05 +0300

    Переименовал фрагмент выбора отрасли

--- app/src/main/java/ru/practicum/android/diploma/filters/ui/IndustrySelectFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/ui/IndustrySelectFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.databinding.BranchSelectFragmentBinding
 
-class BranchSelectFragment : Fragment() {
+class IndustrySelectFragment : Fragment() {
     private var _binding: BranchSelectFragmentBinding? = null
     private val binding get() = _binding!!
 
