01b1dc167cc488712064da3e9adfcc72928097bb
Velichko Sergey <Cer.97@yandex.ru>
2024-10-16 21:06:21 +0300

    Скелет для отображения отрасли + обсервер закоментированный, тк нет полей для него

--- app/src/main/java/ru/practicum/android/diploma/filters/areas/ui/AreaSelectFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/areas/ui/AreaSelectFragment.kt
@@ -6,12 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.AreaSelectFragmentBinding
+import ru.practicum.android.diploma.filters.areas.presentation.AreaSelectViewModel
 
 class AreaSelectFragment : Fragment() {
     private var _binding: AreaSelectFragmentBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: AreaSelectViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,7 +27,7 @@ class AreaSelectFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        observeInit()
         binding.toolBar.setNavigationOnClickListener {
             findNavController().popBackStack()
         }
@@ -36,6 +39,45 @@ class AreaSelectFragment : Fragment() {
         binding.regionSelect.setOnClickListener {
             findNavController().navigate(R.id.action_workingRegionFragment_to_citySelectFragment)
         }
+
+        binding.areaInputLayout.setEndIconOnClickListener {
+            viewModel.clearAreaFilter()
+        }
+        binding.regionInputLayout.setEndIconOnClickListener {
+            viewModel.clearRegionFilter()
+        }
+    }
+
+    private fun observeInit() {
+        /*    viewModel.getCountryAndRegionStateMap.observe(viewLifecycleOwner) { fields ->
+                if (fields.country.isNotEmpty()) {
+                    binding.countrySelect.apply {
+                        setText(fields.country)
+                        isActivated = true
+                    }
+                    binding.countryInputLayout.setEndIconDrawable(R.drawable.ic_close_24px)
+                } else {
+                    binding.countrySelect.apply {
+                        setText("")
+                        isActivated = false
+                    }
+                    binding.countryInputLayout.setEndIconDrawable(R.drawable.ic_arrow_forward_24px)
+                }
+
+                if (fields.region.isNotEmpty()) {
+                    binding.regionSelect.apply {
+                        setText(fields.region)
+                        isActivated = true
+                    }
+                    binding.regionInputLayout.setEndIconDrawable(R.drawable.ic_close_24px)
+                } else {
+                    binding.regionSelect.apply {
+                        setText("")
+                        isActivated = false
+                    }
+                    binding.regionSelect.setEndIconDrawable(R.drawable.ic_arrow_forward_24px)
+                }
+            }*/
     }
 
     override fun onDestroyView() {
