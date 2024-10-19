e3899c40bf9b028a2e789bc56aae241790c618b6
julia.vakulina <yuliavak@mail.ru>
2024-10-19 20:25:54 +0700

    SingleLiveEvent in navigation

--- common-navigate/src/main/java/ru/practicum/android/diploma/navigate/observable/SingleLiveEvent.kt
+++ common-navigate/src/main/java/ru/practicum/android/diploma/navigate/observable/SingleLiveEvent.kt
@@ -1,5 +1,6 @@
 package ru.practicum.android.diploma.navigate.observable
 
+import androidx.annotation.MainThread
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
@@ -8,6 +9,7 @@ import java.util.concurrent.atomic.AtomicBoolean
 class SingleLiveEvent<T> : MutableLiveData<T>() {
     private val pending = AtomicBoolean(false)
 
+    @MainThread
     override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
         super.observe(owner) { t ->
             if (pending.compareAndSet(true, false)) {
@@ -16,6 +18,7 @@ class SingleLiveEvent<T> : MutableLiveData<T>() {
         }
     }
 
+    @MainThread
     override fun setValue(t: T?) {
         pending.set(true)
         super.setValue(t)
