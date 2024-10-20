7fbfbfb197ac11f247a594d7e4cf29b67bd4c2fe
Захаров Павел <pzakharov83@gmail.com>
2024-10-20 15:55:03 +0300

    cleanup

--- feature-search/src/main/java/ru/practicum/android/diploma/search/presentation/viewmodel/VacancyListViewModel.kt
+++ feature-search/src/main/java/ru/practicum/android/diploma/search/presentation/viewmodel/VacancyListViewModel.kt
@@ -45,14 +45,12 @@ internal class VacancyListViewModel(
         _screenStateLiveData.value = SearchScreenState.IDLE
         _vacancyListStateLiveData.value = VacancyListState.Empty
         _currentResultsCountLiveData.value = 0
-
     }
 
     private fun initQueryFilter(filterSearch: FilterSearch) {
         filterSearch.branchOfProfession?.id?.let { queryFilter.put(INDUSTRY_ID, it) }
         filterSearch.expectedSalary?.let { queryFilter.put(SALARY, it) }
         filterSearch.doNotShowWithoutSalary.let { queryFilter.put(ONLY_WITH_SALARY, it.toString()) }
-        // !!!
         filterSearch.placeSearch?.let { place ->
             place.idRegion?.let { queryFilter.put(AREA_ID, it) }
         }
