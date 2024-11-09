28112c3037110d4025f77e1cb9b9de5d8d961bef
1lkham <offgenius@gmail.com>
2024-11-10 00:39:38 +0300

    added fonts

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/favorite_job/FavoriteJobViewModel.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.ui.favorite_job
+
+import androidx.lifecycle.ViewModel
+
+class FavoriteJobViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/search_job/SearchJobViewModel.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.ui.search_job
+
+import androidx.lifecycle.ViewModel
+
+class SearchJobViewModel: ViewModel() {
+    // TODO: Implement the ViewModel
+}
