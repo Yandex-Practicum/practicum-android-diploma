c53d18a963f160bb07555ce34233314cbf9e9c35
Vyacheslav Litvinov <rovyacheslav@gmail.com>
2024-12-11 22:52:25 +0200

    поправил пробелы после detekt

--- app/src/main/java/ru/practicum/android/diploma/data/dto/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/network/RetrofitNetworkClient.kt
@@ -4,8 +4,8 @@ import ru.practicum.android.diploma.data.dto.Response
 import ru.practicum.android.diploma.domain.NetworkClient
 
 class RetrofitNetworkClient(
-    private val hhService : HhApi,
-): NetworkClient {
+    private val hhService: HhApi,
+) : NetworkClient {
 
     override suspend fun doRequest(dto: Any): Response {
         TODO("Not yet implemented")
