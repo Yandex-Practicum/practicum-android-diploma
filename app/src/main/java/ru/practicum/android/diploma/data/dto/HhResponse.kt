33776638e281b4b16688039d38b115cb1e004455
MalinovIgor <malinov.igor@gmail.com>
2024-12-13 11:37:58 +0300

    to revise HhResponse

--- app/src/main/java/ru/practicum/android/diploma/data/dto/HhResponse.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/HhResponse.kt
@@ -1,5 +1,16 @@
 package ru.practicum.android.diploma.data.dto
 
+import ru.practicum.android.diploma.data.dto.model.ItemDto
 import ru.practicum.android.diploma.data.dto.model.VacancyDto
 
-class HhResponse(val results: List<VacancyDto>, resultCode: Int) : Response(resultCode)
+class HhResponse(
+    val arguments: List<HhResponse>,
+    val clusters: Any?,
+    val fixes: Any?,
+    val found: Int,
+    val items: List<ItemDto>,
+    val page: Int,
+    val pages: Int,
+    val per_page: Int,
+    val suggests: Any?,
+    resultCode: Int) : Response(resultCode)
