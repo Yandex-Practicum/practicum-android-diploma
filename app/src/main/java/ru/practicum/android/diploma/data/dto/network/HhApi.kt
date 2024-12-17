b314562c5dd696e93756ca7b9ae36ffdfc1e884f
1-Loneliness-1 <costya.kisurin@yandex.ru>
2024-12-17 14:32:39 +0300

    fix(data_retrofit): изменен запрос в сеть

--- app/src/main/java/ru/practicum/android/diploma/data/dto/network/HhApi.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/network/HhApi.kt
@@ -12,5 +12,14 @@ interface HhApi {
     suspend fun getVacancyById(@Path("vacancy_id") vacancyId: String): VacancyDto
 
     @GET("/vacancies")
-    suspend fun getVacancies(@Query("query") searchQuery: String): VacancySearchResponse
+    suspend fun getVacancies(
+        @Query("text") searchQuery: String, // Название вакансии
+        @Query("area") nameOfCityForFilter: String? = null, // Тут нужно передать id региона из справочника hh.ru
+        @Query("industry") nameOfIndustryForFilter: String? = null, // Тут id отрасли (можно задать несколько, но нам вроде не нужно)
+        @Query("only_with_salary") onlyWithSalary: Boolean = false, // Тут ставим true для вакансий только с зарплатой
+        @Query("currency") currencyOfSalary: String? = null, // Тут задается валюта (они тоже в справочнике)
+        @Query("salary") expectedSalary: String? = null, // Требуемая зарплата (чисто число)
+        @Query("per_page") numberOfVacanciesOnPage: String = "20", // Количество вакансий на страницу(у нас 20)
+        @Query("page") numberOfPage: String // Номер нужной страницы списка вакасий
+    ): VacancySearchResponse
 }
