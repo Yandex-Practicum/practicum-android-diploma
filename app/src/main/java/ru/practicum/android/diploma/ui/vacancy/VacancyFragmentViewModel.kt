ccb20f8634801fe6e90fecb37a033f4338f1313f
MalinovIgor <malinov.igor@gmail.com>
2024-12-19 20:01:27 +0300

    to revise import order12

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyFragmentViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyFragmentViewModel.kt
@@ -1,16 +1,17 @@
 package ru.practicum.android.diploma.ui.vacancy
 
-
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
 import ru.practicum.android.diploma.data.dto.model.favorites.ShareData
 import ru.practicum.android.diploma.domain.api.VacancyInteractor
 import ru.practicum.android.diploma.domain.models.Vacancy
 import ru.practicum.android.diploma.util.SingleLiveEvent
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class VacancyFragmentViewModel(private val vacancyInteractor: VacancyInteractor) : ViewModel() {
 
