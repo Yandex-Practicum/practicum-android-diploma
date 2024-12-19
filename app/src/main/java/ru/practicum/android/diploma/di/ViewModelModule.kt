f8afd43bb3f75bd07599a43ae0eff8e2c1e49908
Format010 <foreneng@yandex.ru>
2024-12-19 18:18:25 +0300

    №63 Добавил обработку ошибок и появление соответствующих плейсхолдеров - детекты2

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -3,10 +3,11 @@ package ru.practicum.android.diploma.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
-import ru.practicum.android.diploma.ui.vacancy.VacancyViewModel
 import ru.practicum.android.diploma.ui.search.viewmodel.SearchViewModel
+import ru.practicum.android.diploma.ui.vacancy.VacancyViewModel
 
 val viewModelModule = module {
+
     viewModelOf(::SearchViewModel)
 
     viewModel {
