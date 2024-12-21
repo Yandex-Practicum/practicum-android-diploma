be1ecd10bb5f9bd318f0f8203ad405816fe16f43
MalinovIgor <malinov.igor@gmail.com>
2024-12-21 14:14:45 +0300

    to add favoritesVM

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/favorites/FavoritesViewModel.kt
@@ -0,0 +1,12 @@
+package ru.practicum.android.diploma.ui.favorites
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import ru.practicum.android.diploma.domain.vacancy.VacancyInteractor
+
+class FavoritesViewModel(
+    application: Application,
+    private val interactor: VacancyInteractor
+) : AndroidViewModel(application) {
+
+}
