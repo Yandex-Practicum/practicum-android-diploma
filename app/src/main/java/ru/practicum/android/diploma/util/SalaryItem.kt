07db9f81a7b490bb9121505b742b3726beb4d1aa
Алексей П. <147827146+Format010@users.noreply.github.com>
2024-12-27 14:14:32 +0300

    Правка детектов для сдачи 6

--- app/src/main/java/ru/practicum/android/diploma/util/SalaryItem.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/SalaryItem.kt
@@ -6,7 +6,7 @@ import ru.practicum.android.diploma.domain.models.SalaryRange
 class SalaryItem {
     fun salaryDetermine(item: SalaryDto?): SalaryRange {
         return when {
-            (item == null || item.to == null && item.from == null) -> SalaryRange.NotSpecified
+            item == null || item.to == null && item.from == null -> SalaryRange.NotSpecified
             item.from != null && item.to != null && item.from == item.to -> SalaryRange.SingleValue(item.from)
             item.from != null && item.to == null -> SalaryRange.FromValue(item.from)
             item.from == null && item.to != null -> SalaryRange.ToValue(item.to)
