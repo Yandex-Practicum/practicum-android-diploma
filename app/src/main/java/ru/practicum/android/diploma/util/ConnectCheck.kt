5fb71baacb5c79ecef0c7056bd106810f7bdc16a
gen <gen@home530>
2025-01-20 02:36:15 +0300

    code style fix3

--- app/src/main/java/ru/practicum/android/diploma/util/ConnectCheck.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/ConnectCheck.kt
@@ -8,12 +8,10 @@ fun isConnected(context: Context): Boolean {
     var isConnected = false
     val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-    if ((capabilities != null) &&
-        (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+    if (capabilities != null) {
+        isConnected = (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
             capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
             capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET))
-    ) {
-        isConnected = true
     }
     return isConnected
 }
