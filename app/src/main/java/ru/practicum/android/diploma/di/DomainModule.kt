cb6504a362460c19794f9b6f8b0b7fe75ecbbad3
Faina Shi <shigapovafaina@gmail.com>
2025-01-20 04:30:30 +0300

    Epic0-7: Заглушки в модули di для успешного запуска приложения

--- app/src/main/java/ru/practicum/android/diploma/di/DomainModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DomainModule.kt
@@ -1,7 +1,9 @@
 package ru.practicum.android.diploma.di
 
+import com.google.gson.Gson
 import org.koin.dsl.module
 
 val domainModule = module {
-    TODO()
+    // заглушка (чтобы проект собрался), потом убрать
+    factory { Gson() }
 }
