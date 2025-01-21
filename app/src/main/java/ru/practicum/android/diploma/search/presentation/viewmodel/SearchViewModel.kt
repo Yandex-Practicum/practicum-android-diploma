3a64a813e6c46a6d21430c41f1ace4635eb6b75a
Maxim <satinmaksim@yandex.ru>
2025-01-22 00:10:31 +0300

    Loading PH in progress

--- app/src/main/java/ru/practicum/android/diploma/search/presentation/viewmodel/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/presentation/viewmodel/SearchViewModel.kt
@@ -1,8 +1,18 @@
 package ru.practicum.android.diploma.search.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.search.domain.interactor.SearchInteractor
+import ru.practicum.android.diploma.search.domain.model.SearchQueryParams
+import ru.practicum.android.diploma.search.domain.model.SearchViewState
 
 class SearchViewModel(
     private val searchInteractor: SearchInteractor
-) : ViewModel()
+) : ViewModel() {
+
+    private val searchScreenLiveData = MutableLiveData<SearchViewState>()
+    fun getSearchLiveData(): LiveData<SearchViewState> = searchScreenLiveData
+}
