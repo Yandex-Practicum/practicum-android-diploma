58cae6072f005620b9b1f644b9b47c7f9a6b5b76
Kvadrokom <lortrem@mail.ru>
2025-01-21 21:06:52 +0300

    code fix

--- app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyResponse.kt
+++
@@ -1,3 +0,0 @@
-package ru.practicum.android.diploma.data.dto
-
-data class VacancyResponse(val description: String) : Response()
--- app/src/main/java/ru/practicum/android/diploma/data/network/VacancyApi.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/VacancyApi.kt
@@ -5,12 +5,12 @@ import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.QueryMap
 import ru.practicum.android.diploma.data.dto.VacanciesResponse
-import ru.practicum.android.diploma.data.dto.VacancyResponse
+import ru.practicum.android.diploma.data.dto.VacancyDescriptionResponse
 
 interface VacancyApi {
     @Headers("HH-User-Agent: Application Practicum_17_android_team")
     @GET("/vacancies/{vacancy_id}?locale=RU")
-    suspend fun getVacancy(@Path("vacancy_id") id: String): VacancyResponse
+    suspend fun getVacancy(@Path("vacancy_id") id: String): VacancyDescriptionResponse
 
     @Headers("HH-User-Agent: Application Practicum_17_android_team")
     @GET("/vacancies")
