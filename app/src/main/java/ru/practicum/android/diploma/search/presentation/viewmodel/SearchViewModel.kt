efd6c8964909ebde598146eeb4b6fc74a1262c5e
Maxim <satinmaksim@yandex.ru>
2025-01-23 02:05:39 +0300

    pagination and items display added. code structure next

--- app/src/main/java/ru/practicum/android/diploma/search/presentation/viewmodel/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/presentation/viewmodel/SearchViewModel.kt
@@ -7,29 +7,62 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.search.domain.interactor.SearchInteractor
 import ru.practicum.android.diploma.search.domain.model.SearchViewState
+import ru.practicum.android.diploma.search.domain.model.VacancyItems
+import ru.practicum.android.diploma.search.domain.model.VacancyList
 
 class SearchViewModel(
     private val searchInteractor: SearchInteractor
 ) : ViewModel() {
 
     private var currentPage: Int = 0
+    private var maxPages: Int? = 0
+    private var searchQuery: String = ""
+    private var vacancyList = mutableListOf<VacancyItems>()
+    private var isNextPageLoading: Boolean = false
 
     private val stateLiveData = MutableLiveData<SearchViewState>()
     fun observeState(): LiveData<SearchViewState> = stateLiveData
 
     fun searchVacancy(searchQuery: String) {
+        isNextPageLoading = true
         if (searchQuery.isNotEmpty()) {
+            isNextPageLoading = true
+            this.searchQuery = searchQuery
             renderState(SearchViewState.Loading)
             viewModelScope.launch {
                 searchInteractor
                     .searchVacancy(searchQuery, currentPage)
                     .collect { viewState ->
-                        renderState(viewState) }
+                        renderState(viewState)
+                    }
             }
+        }
+        isNextPageLoading = false
+    }
+
+    fun onLastItemReached(){
+        if(currentPage != maxPages && maxPages != 0){
+            currentPage += 1
+               searchVacancy(searchQuery)
         }
     }
 
-    private fun renderState(state: SearchViewState) {
-        stateLiveData.postValue(state)
+    fun showVacancyDetails(vacancyItems: VacancyItems){
+
     }
+
+    private fun renderState(state: SearchViewState) {
+        if (state is SearchViewState.Success) {
+            this.maxPages = state.vacancyList.pages
+            this.vacancyList += state.vacancyList.items
+            val vacancyList = state.vacancyList.copy(items = this.vacancyList)
+            stateLiveData.postValue(
+                SearchViewState.Success(vacancyList)
+            )
+        } else {
+            stateLiveData.postValue(state)
+        }
+    }
+
+
 }
--- app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/SearchFragment.kt
@@ -1,20 +1,47 @@
 package ru.practicum.android.diploma.search.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
+import ru.practicum.android.diploma.search.domain.model.Salary
+import ru.practicum.android.diploma.search.domain.model.SearchViewState
+import ru.practicum.android.diploma.search.domain.model.VacancyItems
 import ru.practicum.android.diploma.search.presentation.viewmodel.SearchViewModel
+import ru.practicum.android.diploma.search.ui.decorations.LayoutItemDecoration
 
 class SearchFragment : Fragment() {
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
     private val viewModel by viewModel<SearchViewModel>()
+    private var adapter: SearchAdapter? = null
+
+
+    val vacancyItem = VacancyItems(
+        "1",
+        "Программист",
+        "Москва",
+        "Sber",
+        null,
+        Salary(
+            40000,
+            100000,
+            "RUR"
+        )
+    )
+
+    private var vacancyList = arrayListOf(vacancyItem, vacancyItem,vacancyItem,vacancyItem,vacancyItem,vacancyItem,vacancyItem,vacancyItem,vacancyItem,
+        vacancyItem, vacancyItem,vacancyItem)
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,10 +62,44 @@ class SearchFragment : Fragment() {
         binding.filterButton.setOnClickListener {
             findNavController().navigate(R.id.action_searchFragment_to_filterSettingsFragment)
         }
-        viewModel.observeState().observe(viewLifecycleOwner) {
-//           render(it)
-        }
         viewModel.searchVacancy("Программист")
+        binding.initScreenPH.isVisible = false
+
+        binding.searchVacanciesRV.isVisible = true
+
+        binding.scrollView.isVisible = true
+        binding.progressBar.isVisible = true
+        adapter = SearchAdapter(viewModel::showVacancyDetails)
+        binding.searchVacanciesRV.adapter = adapter
+        binding.searchVacanciesRV.addItemDecoration(LayoutItemDecoration(86))
+        binding.searchVacanciesRV.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                Log.d("OnScroll", "scroll")
+                if (dy > 0) {
+                    val pos =
+                        (binding.searchVacanciesRV.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
+                    val itemsCount = adapter!!.itemCount
+                    if (pos >= itemsCount - 1) {
+                        viewModel.onLastItemReached()
+//                        Log.d("VacancyItems", "${vacancyList.size}")
+                    }
+                }
+            }
+        })
+
+        viewModel.observeState().observe(viewLifecycleOwner) { state ->
+
+            if (state is SearchViewState.Success) {
+                Log.d("Details", "${state}")
+                adapter?.updateItems(state.vacancyList.items)
+//           render(it)
+            }
+
+
+
+        }
+    }
     }
 /*    private fun render(state: SearchViewState) {
         when (state) {
@@ -51,4 +112,4 @@ class SearchFragment : Fragment() {
         }
     }
 */
-}
+
