571248069b1a4bcbe809acfa4a8cca1d8312fdac
PolarSnowflake <a.mayorshina@yandex.ru>
2025-01-22 18:27:52 +0300

    Fix 1.1 : fix detekt

--- app/src/main/java/ru/practicum/android/diploma/ui/search/fragment/VacancyAdapter.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/fragment/VacancyAdapter.kt
@@ -2,5 +2,5 @@ package ru.practicum.android.diploma.ui.search.fragment
 
 @Suppress("EmptyClassBlock")
 class VacancyAdapter {
-    // TODO: Add logic here
+    // Add logic here
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/search/fragment/VacancyViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/fragment/VacancyViewHolder.kt
@@ -2,5 +2,5 @@ package ru.practicum.android.diploma.ui.search.fragment
 
 @Suppress("EmptyClassBlock")
 class VacancyViewHolder {
-    // TODO: Add logic here
+    // Add logic here
 }
