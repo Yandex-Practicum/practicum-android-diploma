080de6c49e944b625c42e23e6cfa336d9021f2d5
Faina Shi <shigapovafaina@gmail.com>
2025-01-24 22:35:19 +0300

    Epic1-26: View holder и view adapter для элементов списка вакансий

--- app/src/main/java/ru/practicum/android/diploma/ui/search/fragment/VacancyViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/fragment/VacancyViewHolder.kt
@@ -1,6 +1,33 @@
 package ru.practicum.android.diploma.ui.search.fragment
 
-@Suppress("EmptyClassBlock")
-class VacancyViewHolder {
-    // Add logic here
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ItemVacancyBinding
+import ru.practicum.android.diploma.domain.models.Vacancy
+
+class VacancyViewHolder(
+    private val binding: ItemVacancyBinding,
+    private val onClick: (vacancy: Vacancy) -> Unit,
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(model: Vacancy) {
+        binding.textViewVacancyTitle.text = model.name
+        binding.textViewVacancyEmployer.text = model.name
+        binding.textViewVacancySalary.text = model.salary?.from.toString()
+        val image: ImageView = binding.imageViewVacancyLogo
+
+        val radius = itemView.resources.getDimensionPixelSize(R.dimen.dp_12)
+
+        Glide.with(itemView)
+            .load(model.employer?.logoUrls?.size240)
+            .placeholder(R.drawable.ic_droid_48)
+            .error(R.drawable.ic_droid_48)
+            .centerCrop()
+            .transform(RoundedCorners(radius))
+            .into(image)
+
+        binding.root.setOnClickListener { _ -> onClick(model) }
+    }
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/search/viewmodel/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/viewmodel/SearchViewModel.kt
@@ -1,9 +1,13 @@
 package ru.practicum.android.diploma.ui.search.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.domain.common.SearchResult
+import ru.practicum.android.diploma.domain.models.Vacancy
 import ru.practicum.android.diploma.domain.search.api.VacanciesInteractor
 
 class SearchViewModel(
@@ -15,38 +19,38 @@ class SearchViewModel(
 
     fun searchResultLiveData(): LiveData<SearchResult> = searchResultData
 
-//    init {
-//        searchVacancies()
-//    }
-//
-//    private fun searchVacancies() {
-//        var searchResult: SearchResult = SearchResult.Loading
-//        searchResultData.postValue(searchResult)
-//
-//        viewModelScope.launch {
-//            vacanciesInteractor
-//                .execute()
-//                .collect { result ->
-//                    searchResult = if (result != null) {
-//                        if (result.found == 0) {
-//                            SearchResult.NotFound
-//                        } else {
-//                            SearchResult.SearchVacanciesContent(
-//                                result.found,
-//                                result.items
-//                            )
-//
-//                        }
-//                    } else {
-//                        SearchResult.Error
-//                    }
-//                    /*
-//                   еще потом докинуть условие в каком случае нет интернета
-//                   то есть в каком случае возращать SearchResult.NoConnection (задача 27)
-//                    */
-//                    searchResultData.postValue(searchResult)
-//                }
-//        }
-//    }
+    fun searchVacancies() {
+        var searchResult: SearchResult = SearchResult.Loading
+        searchResultData.postValue(searchResult)
+
+        viewModelScope.launch {
+            vacanciesInteractor
+                .execute()
+                .collect { result ->
+                    searchResult = if (result != null) {
+                        if (result.found == 0) {
+                            SearchResult.NotFound
+                        } else {
+                            SearchResult.SearchVacanciesContent(
+                                result.found,
+                                result.items
+                            )
+
+                        }
+                    } else {
+                        SearchResult.Error
+                    }
+                    /*
+                   еще потом докинуть условие в каком случае нет интернета
+                   то есть в каком случае возращать SearchResult.NoConnection (задача 27)
+                    */
+                    searchResultData.postValue(searchResult)
+                }
+        }
+    }
+
+    fun onVacancyClicked(vacancy: Vacancy) {
+        Log.i("isClicked", "Пользователь нажал на вакансию")
+    }
 
 }
