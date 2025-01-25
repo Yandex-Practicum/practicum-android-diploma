678462b02b2a1c50ea0b73d5d0cbb75e07734510
Svetlana <s.a.novakovskaya@yandex.ru>
2025-01-26 01:01:12 +0500

    Converter, FavotiteDao, FavoriteRepositoryImpl

--- app/src/main/java/ru/practicum/android/diploma/favorites/data/dao/FavoriteDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/data/dao/FavoriteDao.kt
@@ -1,10 +1,12 @@
 package ru.practicum.android.diploma.favorites.data.dao
 
+import android.database.sqlite.SQLiteException
 import androidx.room.Query
 import ru.practicum.android.diploma.common.data.db.entity.VacancyEntity
 
 interface FavoriteDao {
     // метод @Query для получения списка со всеми треками, добавленными в избранное;
     @Query("SELECT * FROM vacancy_entity ORDER BY timeOfLikeSec DESC")
+    @Throws(SQLiteException::class)
     fun getVacancyListByTime(): List<VacancyEntity>
 }
--- app/src/main/java/ru/practicum/android/diploma/favorites/data/repository/FavoritesRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/data/repository/FavoritesRepositoryImpl.kt
@@ -2,11 +2,13 @@ package ru.practicum.android.diploma.favorites.data.repository
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.database.sqlite.SQLiteException
 import com.google.gson.Gson
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.internal.NopCollector.emit
+import kotlinx.coroutines.flow.flow
 import ru.practicum.android.diploma.AppDatabase
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.common.data.db.entity.VacancyEntity
 import ru.practicum.android.diploma.common.domain.models.Resource
 import ru.practicum.android.diploma.common.util.Converter
 import ru.practicum.android.diploma.favorites.domain.repository.FavoriteRepository
@@ -19,13 +21,16 @@ class FavoritesRepositoryImpl(
     private val converter: Converter,
     private val context: Context
 ) : FavoriteRepository {
-    override suspend fun getVacancyList(): Flow<Resource<List<VacancyItems>>> {
-        val list = appDatabase.favoriteDao().getVacancyListByTime()
+    override suspend fun getVacancyList(): Flow<Resource<List<VacancyItems>>>  = flow {
+        try {
+            val vacancyList = appDatabase.favoriteDao().getVacancyListByTime()
+            emit(Resource.Success(convert(vacancyList)))
+        } catch (e: SQLiteException) {
+            emit(Resource.Error(R.string.list_is_not_recieved.toString())) // не удалось получить список вакансий
+        }
+   }
 
-        if (list.isEmpty())
-            emit(Resource.Error(R.string.your_mediateka_is_empty.toString()))
-        else
-            emit(Resource.Success(convertListToList(list)))
+    private fun convert(vacancyList: List<VacancyEntity>) : List<VacancyItems> {
+        return vacancyList.map { vacancy -> converter.map(vacancy)}
     }
-
 }
