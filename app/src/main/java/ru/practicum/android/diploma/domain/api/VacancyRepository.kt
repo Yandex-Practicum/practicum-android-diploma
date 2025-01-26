7d080812f0320819e8aeb16da17e4f095a97f365
Kvadrokom <lortrem@mail.ru>
2025-01-26 16:09:11 +0300

    code style fix

--- app/src/main/java/ru/practicum/android/diploma/domain/api/VacancyRepository.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/VacancyRepository.kt
@@ -1,9 +1,9 @@
 package ru.practicum.android.diploma.domain.api
 
+import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.domain.models.Page
 import ru.practicum.android.diploma.domain.models.Resource
 import ru.practicum.android.diploma.domain.models.Vacancy
-import kotlinx.coroutines.flow.Flow
 
 interface VacancyRepository {
     fun getVacancies(options: Map<String, String>): Flow<Resource<Page>>
