af6ca5bb70b32e3588221f57820b8cf702b30e9a
Keithcutt <mayerhofnew@gmail.com>
2025-01-27 20:50:58 +0300

    Bugfix

--- app/src/main/java/ru/practicum/android/diploma/util/ResponseCode.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/ResponseCode.kt
@@ -3,15 +3,19 @@ package ru.practicum.android.diploma.util
 private const val SUCCESS_CODE: Int = 200
 private const val SERVER_ERROR_CODE: Int = 500
 private const val NO_INTERNET_CODE: Int = -1
+private const val DATABASE_ERROR_CODE = 999
+
 enum class ResponseCode(val code: Int, val description: String) {
 
     SUCCESS(SUCCESS_CODE, "Запрос выполнен"),
     SERVER_ERROR(SERVER_ERROR_CODE, "Проблемы с сервером"),
-    NO_INTERNET(NO_INTERNET_CODE, "Отсутствие интернета");
+    NO_INTERNET(NO_INTERNET_CODE, "Отсутствие интернета"),
+    DATABASE_ERROR(DATABASE_ERROR_CODE, "Ошибка базы данных");
 
     companion object {
         fun fromCode(code: Int): ResponseCode {
-            return entries.find { it.code == code } ?: throw IllegalArgumentException("Unknown code: $code")
+            return entries.find { it.code == code }
+                ?: throw IllegalArgumentException("Unknown code: $code")
         }
     }
 }
