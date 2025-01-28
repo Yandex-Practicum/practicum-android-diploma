b39895f99a4247856b5828acac5e6b3dca89fb8c
tsyplenkovdo <danil.ciplenkov@yandex.ru>
2025-01-28 11:17:09 +0300

    [EPIC-1]-#23 Добавил одидание в запросе

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -3,18 +3,20 @@ package ru.practicum.android.diploma.di
 import android.content.Context
 import androidx.room.Room
 import com.google.gson.Gson
+import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.practicum.android.diploma.AppDatabase
-import ru.practicum.android.diploma.common.data.Mapper
-import ru.practicum.android.diploma.common.data.network.HeadHunterApi
-import ru.practicum.android.diploma.common.data.network.RetrofitNetworkClient
 import ru.practicum.android.diploma.common.constants.APP_DATA_BASE
 import ru.practicum.android.diploma.common.constants.APP_SHARED_PREFS
 import ru.practicum.android.diploma.common.constants.HH_BASE_URL
+import ru.practicum.android.diploma.common.constants.TIMEOUT
+import ru.practicum.android.diploma.common.data.Mapper
+import ru.practicum.android.diploma.common.data.network.HeadHunterApi
+import ru.practicum.android.diploma.common.data.network.RetrofitNetworkClient
 import ru.practicum.android.diploma.common.util.ConnectivityManager
 import ru.practicum.android.diploma.favorites.data.repository.FavoritesRepositoryImpl
 import ru.practicum.android.diploma.favorites.domain.repository.FavoriteRepository
@@ -24,6 +26,7 @@ import ru.practicum.android.diploma.search.data.repository.SearchRepositoryImpl
 import ru.practicum.android.diploma.search.domain.repository.SearchRepository
 import ru.practicum.android.diploma.vacancy.data.repository.VacancyRepositoryImpl
 import ru.practicum.android.diploma.vacancy.domain.repository.VacancyRepository
+import java.util.concurrent.TimeUnit
 
 val dataModule = module {
 
@@ -42,9 +45,18 @@ val dataModule = module {
         ).build()
     }
 
+    single {
+        OkHttpClient.Builder()
+            .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .build()
+    }
+
     single<HeadHunterApi> {
         Retrofit.Builder()
             .baseUrl(HH_BASE_URL)
+            .client(get())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(HeadHunterApi::class.java)
