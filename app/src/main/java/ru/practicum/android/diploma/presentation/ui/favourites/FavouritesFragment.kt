0d303661e1f455f41b93f0c8c5a8aab8406961cb
Toxa-K <timkov1506@gmail.com>
2025-01-29 18:57:18 +0300

    Реализация передачи id вакансии в фрагмент Вакансия

--- app/src/main/java/ru/practicum/android/diploma/presentation/ui/favourites/FavouritesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/ui/favourites/FavouritesFragment.kt
@@ -4,24 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFavouritesBinding
 import ru.practicum.android.diploma.domain.models.Vacancy
 import ru.practicum.android.diploma.presentation.adapter.VacancyAdapter
+import ru.practicum.android.diploma.util.debounce
 
 class FavouritesFragment : Fragment() {
 
     private val viewModel: FavouritesViewModel by viewModel()
     private var _binding: FragmentFavouritesBinding? = null
     private val binding get() = _binding!!
-    private val adapter = VacancyAdapter().apply {
-        onItemClick = { vacancy ->
-            // Реализация логики при нажатии, открытие подробной информации о вакансии
-        }
-    }
+    private var onClickVacancy: (Vacancy) -> Unit = {}
+    private var adapter = VacancyAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,6 +36,17 @@ class FavouritesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        onClickVacancy = debounce<Vacancy>(
+            CLICK_DEBOUNCE_DAYLY,
+            viewLifecycleOwner.lifecycleScope,
+            false
+        ) { vacancy ->
+            val bundle = bundleOf(KEY_VACANCY to vacancy.id)
+            findNavController().navigate(R.id.action_favouritesFragment_to_vacancyFragment, bundle)
+        }
+
+        adapter.onItemClick = { vacancy -> onClickVacancy(vacancy) }
 
         setupRecyclerView()
 
@@ -78,6 +91,8 @@ class FavouritesFragment : Fragment() {
     }
 
     companion object {
+        const val KEY_VACANCY = "KEY_VACANCY"
+        const val CLICK_DEBOUNCE_DAYLY = 0L
         fun newInstance() = FavouritesFragment()
     }
 }
