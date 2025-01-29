e23e134f68dd73c84807788948556447c46a25a8
Egor Skrypchenko <egor.football11@yandex.ru>
2025-01-30 00:10:07 +0300

    Кнопка "Поделиться"

--- app/src/main/java/ru/practicum/android/diploma/presentation/ui/vacancy/VacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/ui/vacancy/VacancyFragment.kt
@@ -1,5 +1,6 @@
 package ru.practicum.android.diploma.presentation.ui.vacancy
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,13 +8,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentVacancyBinding
 
 class VacancyFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = VacancyFragment()
-    }
-
+    private lateinit var binding: FragmentVacancyBinding
     private val viewModel: VacancyViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +24,31 @@ class VacancyFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_vacancy, container, false)
+        binding = FragmentVacancyBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.toolbar.setNavigationOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+
+        binding.btnShare.setOnClickListener {
+            shareVacancy()
+        }
+
+        binding.btnFavorite.setOnClickListener {
+            viewModel.onFavoriteClick()
+        }
+    }
+
+    private fun shareVacancy() {
+        val shareIntent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_SUBJECT, getString(R.string.vacancy))
+        }
+        startActivity(Intent.createChooser(shareIntent, "Поделиться через"))
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/presentation/ui/vacancy/VacancyViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/ui/vacancy/VacancyViewModel.kt
@@ -2,4 +2,9 @@ package ru.practicum.android.diploma.presentation.ui.vacancy
 
 import androidx.lifecycle.ViewModel
 
-class VacancyViewModel : ViewModel()
+class VacancyViewModel : ViewModel() {
+
+    fun onFavoriteClick() {
+        // Логика добавления в избранное
+    }
+}
