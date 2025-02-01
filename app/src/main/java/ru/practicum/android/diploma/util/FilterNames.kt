f14d3bce6510f71744923debcb258c5293fb3a2b
Keithcutt <mayerhofnew@gmail.com>
2025-02-01 21:29:47 +0300

    Epic4-82: enum изменен на объект со строками

--- app/src/main/java/ru/practicum/android/diploma/util/FilterNames.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/FilterNames.kt
@@ -1,12 +1,12 @@
 package ru.practicum.android.diploma.util
 
-enum class FilterNames {
-    COUNTRY_ID,
-    COUNTRY_NAME,
-    REGION_ID,
-    REGION_NAME,
-    INDUSTRY_ID,
-    INDUSTRY_NAME,
-    EXPECTED_SALARY,
-    SHOW_WITHOUT_SALARY
+object FilterNames {
+    const val COUNTRY_ID = "country_id"
+    const val COUNTRY_NAME = "country_name"
+    const val REGION_ID = "region_id"
+    const val REGION_NAME = "region_name"
+    const val INDUSTRY_ID = "industry_id"
+    const val INDUSTRY_NAME = "industry_name"
+    const val EXPECTED_SALARY = "expected_salary"
+    const val SHOW_WITHOUT_SALARY = "show_without_salary"
 }
