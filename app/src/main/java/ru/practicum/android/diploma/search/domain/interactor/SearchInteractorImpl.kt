56d83d0571a018fe9e88e7ba768ea777acff353d
tsyplenkovdo <danil.ciplenkov@yandex.ru>
2025-02-03 13:33:44 +0300

    add-query-parameter

--- app/src/main/java/ru/practicum/android/diploma/search/domain/interactor/SearchInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/domain/interactor/SearchInteractorImpl.kt
@@ -1,14 +1,14 @@
 package ru.practicum.android.diploma.search.domain.interactor
 
 import kotlinx.coroutines.flow.Flow
-import ru.practicum.android.diploma.common.sharedprefs.repository.SharedPrefsRepositoryImpl
+import ru.practicum.android.diploma.common.sharedprefs.repository.SharedPrefsRepository
 import ru.practicum.android.diploma.search.domain.model.SearchQueryParams
 import ru.practicum.android.diploma.search.domain.model.SearchViewState
 import ru.practicum.android.diploma.search.domain.repository.SearchRepository
 
 class SearchInteractorImpl(
     private val searchRepository: SearchRepository,
-    private val sharedPrefsRepositoryImpl: SharedPrefsRepositoryImpl
+    private val sharedPrefsRepositoryImpl: SharedPrefsRepository
 ) : SearchInteractor {
     override fun searchVacancy(text: String, page: Int): Flow<SearchViewState> {
         val filter = sharedPrefsRepositoryImpl.getFilter()
