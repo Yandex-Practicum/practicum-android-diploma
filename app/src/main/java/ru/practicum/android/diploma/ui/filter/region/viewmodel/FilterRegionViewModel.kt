3d0abf0a8d83f5df301dfe4b05991efded50cb35
Faina Shi <shigapovafaina@gmail.com>
2025-02-03 15:22:19 +0300

    Epic5-105: Логика и UI поиска по списку регионов с выбором региона

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/region/viewmodel/FilterRegionViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/region/viewmodel/FilterRegionViewModel.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.launch
 import ru.practicum.android.diploma.domain.Resource
 import ru.practicum.android.diploma.domain.areas.api.AreasInteractor
 import ru.practicum.android.diploma.domain.common.SearchResult
+import ru.practicum.android.diploma.domain.models.Area
 import ru.practicum.android.diploma.domain.models.CountryRegionData
 import ru.practicum.android.diploma.domain.models.Places
 import ru.practicum.android.diploma.util.SingleEventLiveData
@@ -24,6 +25,8 @@ class FilterRegionViewModel(
         MutableLiveData(SearchResult.Loading)
 
     fun searchResultLiveData(): LiveData<SearchResult> = searchResultData
+
+    private var allRegions: List<CountryRegionData>? = null
 
     fun onItemClicked(countryId: String, countryName: String, regionId: String, regionName: String) {
         selectRegionTrigger.value = CountryRegionData(
@@ -44,12 +47,41 @@ class FilterRegionViewModel(
         }
     }
 
+    fun filterRegions(query: String?) {
+        val original = allRegions
+        if (original != null) {
+            val filteredList = if (query.isNullOrEmpty()) {
+                original
+            } else {
+                original.filter { it.regionName.contains(query, ignoreCase = true) }
+            }
+            searchResultData.postValue(SearchResult.GetPlacesContent(filteredList))
+        }
+    }
+
     private fun resultHandle(result: Resource<Places>) {
         if (result.value != null) {
-            searchResultData.postValue(SearchResult.GetPlacesContent(result.value.countries, result.value.others))
-
+            val regionMap = result.value.others.associateBy { it.id } + result.value.countries.associateBy { it.id }
+            val list = result.value.others.map { Pair(it, findCountry(it, regionMap)) }.map {
+                CountryRegionData(
+                    it.second.id,
+                    it.second.name!!,
+                    it.first.id,
+                    it.first.name!!
+                )
+            }
+            allRegions = list
+            searchResultData.postValue(SearchResult.GetPlacesContent(list))
         } else if (result.errorCode != null) {
             searchResultData.postValue(SearchResult.Error)
         }
+    }
+
+    private fun findCountry(region: Area, areas: Map<String, Area>): Area {
+        var area = region
+        while (area.parentId != null) {
+            area = areas[area.parentId]!!
+        }
+        return area
     }
 }
