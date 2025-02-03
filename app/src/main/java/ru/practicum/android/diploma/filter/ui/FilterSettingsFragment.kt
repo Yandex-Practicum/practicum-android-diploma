4efd5fe3b88b1f92e38b88407055b12b85d0d596
David Spiridonov <spiridonovdavid@gmail.com>
2025-02-04 02:02:19 +0300

    detekt

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/FilterSettingsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/FilterSettingsFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.view.isVisible
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
