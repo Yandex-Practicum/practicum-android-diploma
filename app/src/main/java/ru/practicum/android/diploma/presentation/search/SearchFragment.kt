5daa126547a338d88d57b5438ee2ecd038f97dc6
SleeplessShu <di.burmistrov@gmail.com>
2025-04-14 19:01:16 +0200

    34 Создать фрагменты-заглушки

--- app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/search/SearchFragment.kt
@@ -5,16 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+    private lateinit var binding: FragmentSearchBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding = FragmentSearchBinding.bind(view)
+
+        binding.toFiltersButton.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_main_to_navigation_filters)
+        }
+
+        binding.toVacancyButton.setOnClickListener {
+            val args = Bundle().apply { putString("vacancyId", "123") }
+            findNavController().navigate(R.id.action_navigation_main_to_navigation_vacancy, args)
+        }
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/VacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/VacancyFragment.kt
@@ -4,17 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentVacancyBinding
 
 class VacancyFragment : Fragment() {
 
+    private lateinit var binding: FragmentVacancyBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_vacancy, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding = FragmentVacancyBinding.bind(view)
+
+        binding.bToSearch.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        binding.bShare.setOnClickListener {
+            Toast.makeText(
+                requireContext(), "SHARING....", Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        binding.bFavorite.setOnClickListener {
+            Toast.makeText(
+                requireContext(), "ADDING TO DB....", Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
