e04c8997fbbf9f568d60f50df0d69d8e3582b6e7
DariaBandukova <dariabandukova@gmail.com>
2025-04-14 23:21:36 +0300

    ISharingInteractor

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/ISharingInteractor.kt
@@ -0,0 +1,5 @@
+package ru.practicum.android.diploma.domain.api
+
+interface ISharingInteractor {
+    fun share()
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/domain/impl/ISharingInteractorImpl.kt
@@ -0,0 +1,11 @@
+package ru.practicum.android.diploma.domain.impl
+
+import ru.practicum.android.diploma.domain.api.ISharingInteractor
+import ru.practicum.android.diploma.domain.api.ISharingProvider
+import ru.practicum.android.diploma.domain.api.IStorageRepository
+
+class ISharingInteractorImpl(val provider: ISharingProvider): ISharingInteractor {
+    override fun share() {
+        provider.shareVacancy()
+    }
+}
