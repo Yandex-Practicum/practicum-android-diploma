66f2d1652dffd226983ff28428dee1a8ba5b3346
Ekaterina <ekaterinalife92@yandex.ru>
2025-04-15 16:25:34 +0300

    Task-64: Добавлены классы и дто для взаимодействия с сетью - детект устранен

--- app/src/main/java/ru/practicum/android/diploma/data/network/api/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/api/RetrofitNetworkClient.kt
@@ -5,25 +5,25 @@ import ru.practicum.android.diploma.data.dto.main.VacancyLongDtoEntity
 import ru.practicum.android.diploma.data.dto.response.VacancySearchResponseDto
 import ru.practicum.android.diploma.data.network.NetworkChecker
 import ru.practicum.android.diploma.data.network.Response
+import java.io.IOException
 
 class RetrofitNetworkClient(
     private val api: HeadHunterApi,
     private val networkChecker: NetworkChecker
 ) : NetworkClient {
     override suspend fun searchVacancies(filters: Map<String, String>): Response<VacancySearchResponseDto> {
-        return safeCall(networkChecker) {
+        return safeCall {
             api.searchVacancies(filters)
         }
     }
 
     override suspend fun getVacancyDetails(id: String): Response<VacancyLongDtoEntity> {
-        return safeCall(networkChecker) {
+        return safeCall {
             api.getVacancyDetails(id)
         }
     }
 
     private suspend inline fun <T> safeCall(
-        networkChecker: NetworkChecker,
         crossinline apiCall: suspend () -> T
     ): Response<T> {
         return try {
@@ -34,13 +34,24 @@ class RetrofitNetworkClient(
             }
         } catch (e: HttpException) {
             when (e.code()) {
-                400 -> Response.BadRequest
-                404 -> Response.NotFound
-                in 500..599 -> Response.ServerError
+                HTTP_BAD_REQUEST -> Response.BadRequest
+                HTTP_NOT_FOUND -> Response.NotFound
+                in HTTP_INTERNAL_ERROR..HTTP_INTERNAL_ERROR_MAX -> Response.ServerError
                 else -> Response.ServerError
             }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Response.ServerError
         } catch (e: Exception) {
+            e.printStackTrace()
             Response.ServerError
         }
+    }
+
+    private companion object {
+        const val HTTP_BAD_REQUEST = 400
+        const val HTTP_NOT_FOUND = 404
+        const val HTTP_INTERNAL_ERROR = 500
+        const val HTTP_INTERNAL_ERROR_MAX = 599
     }
 }
