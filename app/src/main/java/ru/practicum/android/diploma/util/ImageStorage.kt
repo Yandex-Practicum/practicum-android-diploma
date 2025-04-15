8235c4f162818acb5eb9ed638f8c96352287a9ab
leunamme4 <90042349+leunamme4@users.noreply.github.com>
2025-04-15 22:07:29 +0300

    Task-31: ImageStorage.kt - image storage by uri and getting uri by path

---
+++ app/src/main/java/ru/practicum/android/diploma/util/ImageStorage.kt
@@ -0,0 +1,45 @@
+package ru.practicum.android.diploma.util
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Environment
+import androidx.core.content.FileProvider
+import java.io.File
+import java.io.FileOutputStream
+
+class ImageStorage {
+
+    /**
+     * Сохраняет картинку во внешнее хранилище
+     * @return путь для дальнейшего извлечения сохраненной картирнки
+     */
+    private fun saveImageToPrivateStorage(context: Context, uri: Uri): String {
+        val filePath = File(
+            context.getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+            "icons"
+        )
+        if (!filePath.exists()) {
+            filePath.mkdirs()
+        }
+        val file = File(filePath, "${uri.lastPathSegment}.jpg")
+        val inputStream = context.contentResolver.openInputStream(uri)
+        val outputStream = FileOutputStream(file)
+        BitmapFactory
+            .decodeStream(inputStream)
+            .compress(Bitmap.CompressFormat.JPEG, 30, outputStream)
+
+        return file.absolutePath
+    }
+
+    /**
+     * @return Uri ранее сохраненной картинки по пути ее хранения
+     * @see saveImageToPrivateStorage
+     */
+    private fun getStoredImageUri(imagePath: String, context: Context): Uri {
+        val file = File(imagePath)
+
+        return FileProvider.getUriForFile(context, "${context.packageName}.provider", file)
+    }
+}
