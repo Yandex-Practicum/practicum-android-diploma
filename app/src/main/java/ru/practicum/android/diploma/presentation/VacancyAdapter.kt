34304faf16d39d2b09a13eb966f34c7dcd1b2800
ShvetsovaTatyana <polinko.tatyana@mail.ru>
2025-04-19 19:11:15 +0300

    Task-81 Сделала Adapter и ViewHolder

---
+++ app/src/main/java/ru/practicum/android/diploma/presentation/VacancyAdapter.kt
@@ -0,0 +1,45 @@
+package ru.practicum.android.diploma.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import ru.practicum.android.diploma.databinding.ElementVacancyShortBinding
+import ru.practicum.android.diploma.domain.models.main.VacancyShort
+import ru.practicum.android.diploma.util.extensions.toFormattedString
+
+class VacancyAdapter(
+    private val vacancyList: Array<VacancyShort>,
+    private val onItemClickListener: (VacancyShort) -> Unit
+) : RecyclerView.Adapter<VacancyAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val vacancyItemBinding = ElementVacancyShortBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(vacancyItemBinding, onItemClickListener)
+    }
+
+    override fun getItemCount(): Int {
+        return vacancyList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(vacancyList[position])
+    }
+
+    class ViewHolder(
+        private val binding: ElementVacancyShortBinding,
+        private val onItemClickListener: (VacancyShort) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: VacancyShort) {
+            Glide.with(binding.imageEmployer.context)
+                .load(item.logoUrl?.logo90)
+                .into(binding.imageEmployer)
+            binding.textJobNameAndCity.text = item.name
+            binding.textEmployerName.text = item.employer
+            binding.textSalary.text = item.salary.toFormattedString(itemView.context)
+            itemView.setOnClickListener { onItemClickListener.invoke(item) }
+        }
+
+    }
+}
