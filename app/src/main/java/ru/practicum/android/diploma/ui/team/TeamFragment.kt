f33dde40035bef8d08a2fbfeeff61b4eca3374ef
Alexander <puch399@yandex.ru>
2025-06-07 23:22:17 +1100

    feature 5 Промежуточная фиксация

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/team/TeamFragment.kt
@@ -0,0 +1,15 @@
+package ru.practicum.android.diploma.ui.team
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import ru.practicum.android.diploma.databinding.FragmentTeamBinding
+import ru.practicum.android.diploma.ui.root.BindingFragment
+
+class TeamFragment: BindingFragment<FragmentTeamBinding>() {
+    override fun createBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentTeamBinding {
+        return FragmentTeamBinding.inflate(inflater, container, false)
+    }
+}
