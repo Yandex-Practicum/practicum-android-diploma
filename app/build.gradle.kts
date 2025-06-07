ffec162c60095a8f30786fa9f322cc0b6cfa6909
Viraremi <74133988+Viraremi@users.noreply.github.com>
2025-06-07 18:47:40 +0400

    detekt fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "ru.practicum.android.diploma"
-        minSdk = 28
+        minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
