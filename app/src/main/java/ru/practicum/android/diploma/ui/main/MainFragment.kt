8b7e8aa1d9cc65f53159a0aef7206164840c6c86
Viraremi <74133988+Viraremi@users.noreply.github.com>
2025-06-15 22:01:16 +0400

    feature 12 conflicts

--- app/src/main/java/ru/practicum/android/diploma/ui/main/MainFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/main/MainFragment.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.databinding.FragmentMainBinding
-import ru.practicum.android.diploma.R
-import androidx.navigation.fragment.findNavController
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
@@ -25,35 +22,7 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        initUiToolbar()
-        // системная кн назад
-        requireActivity().onBackPressedDispatcher.addCallback(
-            viewLifecycleOwner,
-            backPressedCallback
-        )
-    }
-
-    private fun initUiToolbar() {
-        // настройка кастомного топбара
-        val toolbar = binding.toolbar
-        toolbar.setupToolbarForSearchScreen()
-        toolbar.setToolbarTitle(getString(R.string.vacancy_search))
-        toolbar.setOnToolbarFilterClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_filterFragment)
-        }
-        /*
-        * !!! после того, как настроены все фильтры
-        * применить toolbar.setFilterState(true) для изменения иконки кнопки
-        */
-
-    }
-
-    //  callback для системной кн назад - выход из приложения
-    private val backPressedCallback = object : OnBackPressedCallback(true) {
-        override fun handleOnBackPressed() {
-            requireActivity().finish()
-        }
+        /* Пока здесь ничего нет */
     }
 
     override fun onDestroyView() {
