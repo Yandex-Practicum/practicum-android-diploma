d6f1c7dfd9fd7942c232879751a14b6a1cf3246f
MakueB <makbones@gmail.com>
2025-06-17 23:25:20 +0300

    feature 25 исправил отображение валюты

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyDetailsMapper.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyDetailsMapper.kt
@@ -1,5 +1,6 @@
 package ru.practicum.android.diploma.ui.vacancy
 
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.domain.vacancy.models.VacancyDetails
 
 class VacancyDetailsMapper(private val htmlParser: HtmlParser) {
@@ -16,10 +17,23 @@ class VacancyDetailsMapper(private val htmlParser: HtmlParser) {
     )
 
     private fun buildSalaryString(salaryFrom: Int?, salaryTo: Int?, currency: String): String {
+        val currencySymbol = when (currency.uppercase()) {
+            "RUB", "RUR" -> "₽"
+            "BYR" -> "Br"
+            "USD" -> "$"
+            "EUR" -> "€"
+            "KZT" -> "₸"
+            "UAH" -> "₴"
+            "AZN" -> "₼"
+            "UZS" -> "сум"
+            "GEL" -> "₾"
+            "KGT" -> "сом"
+            else -> currency
+        }
         return buildList {
             if (salaryFrom != null) add("от $salaryFrom")
             if (salaryTo != null) add("до $salaryTo")
         }.joinToString(" ") +
-            if ((salaryFrom != null || salaryTo != null) && currency.isNotEmpty()) " $currency" else ""
+                if ((salaryFrom != null || salaryTo != null) && currency.isNotEmpty()) " $currencySymbol" else ""
     }
 }
