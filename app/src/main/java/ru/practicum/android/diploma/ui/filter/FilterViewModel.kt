68fa21b14c6237bd852982c7d627cf4216123329
Viraremi <74133988+Viraremi@users.noreply.github.com>
2025-06-22 23:29:12 +0400

    feature 64 viewModel и логика отображения

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/FilterViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/FilterViewModel.kt
@@ -1,6 +1,8 @@
 package ru.practicum.android.diploma.ui.filter
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
@@ -8,12 +10,51 @@ import ru.practicum.android.diploma.domain.filters.AreasInteractor
 import ru.practicum.android.diploma.domain.filters.IndustriesInteractor
 import ru.practicum.android.diploma.domain.models.Areas
 import ru.practicum.android.diploma.domain.models.Industries
+import ru.practicum.android.diploma.ui.filter.model.FilterScreenState
+import ru.practicum.android.diploma.ui.filter.model.SelectedFilters
 import ru.practicum.android.diploma.util.HH_LOG
 
 class FilterViewModel(
     private val interactorAreas: AreasInteractor,
     private val interactorIndustries: IndustriesInteractor
 ) : ViewModel() {
+
+    private val state = MutableLiveData<FilterScreenState>()
+    fun getState(): LiveData<FilterScreenState> = state
+
+    var testModel = SelectedFilters("Россия, Москва", "IT", 999999, true)
+
+    fun getFilters(){
+        // Тут мы достаем сохраненные в SP фильры
+        state.postValue(FilterScreenState.CONTENT(testModel))
+    }
+
+    fun clearPlace() {
+        // TODO
+        testModel = SelectedFilters(null, testModel.industry, testModel.salary, testModel.showNoSalary)
+        state.postValue(FilterScreenState.CONTENT(testModel))
+    }
+
+    fun clearIndustry() {
+        // TODO
+        testModel = SelectedFilters(testModel.place, null, testModel.salary, testModel.showNoSalary)
+        state.postValue(FilterScreenState.CONTENT(testModel))
+    }
+
+    fun clearSalary() {
+        // TODO
+        testModel = SelectedFilters(testModel.place, testModel.industry, null, testModel.showNoSalary)
+        state.postValue(FilterScreenState.CONTENT(testModel))
+    }
+
+    fun setShowNoSalary() {
+        testModel = SelectedFilters(testModel.place, testModel.industry, testModel.salary, !testModel.showNoSalary)
+        state.postValue(FilterScreenState.CONTENT(testModel))
+    }
+
+    fun clearFilters() {
+        TODO("Not yet implemented")
+    }
 
     // Это тестовый запрос
     fun getAreas() {
