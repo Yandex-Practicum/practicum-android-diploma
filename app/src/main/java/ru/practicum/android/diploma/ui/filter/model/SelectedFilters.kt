0fcb7167099a89cfe20dcf277d604a0147aa61ff
MakueB <makbones@gmail.com>
2025-06-24 22:37:12 +0300

    feature 88 начал вынос в отдельную вьюмодель

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/model/SelectedFilters.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/model/SelectedFilters.kt
@@ -1,7 +1,9 @@
 package ru.practicum.android.diploma.ui.filter.model
 
 data class SelectedFilters(
+    val placeId: String?,
     val place: String?,
+    val industryId: String?,
     val industry: String?,
     val salary: Int?,
     val onlyWithSalary: Boolean
