f589cbb95b46f0761e15587f0f4f002fb529c78a
MakueB <makbones@gmail.com>
2025-06-24 22:24:06 +0300

    feature 67 добавил поля в SelectedFilters.kt

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/FilterViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/FilterViewModel.kt
@@ -24,41 +24,41 @@ class FilterViewModel(
     private val state = MutableLiveData<FilterScreenState>()
     fun getState(): LiveData<FilterScreenState> = state
 
-    var testModel = SelectedFilters("Россия, Москва", "IT", 999999, true)
+    var testModel = SelectedFilters("1","Россия, Москва", "1","IT", 999999, true)
 
     fun getFilters() {
         // Тут мы достаем сохраненные в SP фильтры
-        testModel = filterPreferences.loadFilters() ?: SelectedFilters("Россия, Владикавказ", "IT", 999999, true)
+        testModel = filterPreferences.loadFilters() ?: SelectedFilters("1","Россия, Москва", "1","IT", 999999, true)
         state.postValue(FilterScreenState.CONTENT(testModel))
     }
 
     fun clearPlace() {
         // TODO
-        testModel = SelectedFilters(null, testModel.industry, testModel.salary, testModel.onlyWithSalary)
+        testModel = SelectedFilters(null, null, testModel.industryId, testModel.industry, testModel.salary, testModel.onlyWithSalary)
         state.postValue(FilterScreenState.CONTENT(testModel))
     }
 
     fun clearIndustry() {
         // TODO
-        testModel = SelectedFilters(testModel.place, null, testModel.salary, testModel.onlyWithSalary)
+        testModel = SelectedFilters(testModel.placeId,testModel.place, null, null, testModel.salary, testModel.onlyWithSalary)
         state.postValue(FilterScreenState.CONTENT(testModel))
     }
 
     fun clearSalary() {
         // TODO
-        testModel = SelectedFilters(testModel.place, testModel.industry, null, testModel.onlyWithSalary)
+        testModel = SelectedFilters(testModel.placeId, testModel.place, testModel.industryId, testModel.industry, null, testModel.onlyWithSalary)
         state.postValue(FilterScreenState.CONTENT(testModel))
     }
 
     fun setShowNoSalary() {
-        testModel = SelectedFilters(testModel.place, testModel.industry, testModel.salary, !testModel.onlyWithSalary)
+        testModel = SelectedFilters(testModel.placeId, testModel.place, testModel.industryId, testModel.industry, null, !testModel.onlyWithSalary)
         state.postValue(FilterScreenState.CONTENT(testModel))
     }
 
     fun clearFilters() {
         // временно - проверить работу шП
         filterPreferences.clearFilters()
-        testModel = SelectedFilters(null, null, null, false)
+        testModel = SelectedFilters(null, null, null, null, null, false)
         state.postValue(FilterScreenState.CONTENT(testModel))
     }
 
