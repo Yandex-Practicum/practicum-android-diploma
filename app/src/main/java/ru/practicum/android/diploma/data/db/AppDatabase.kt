15741579ed8f89a2bf24d77e72b38c7b2427b8b9
ARKADIY <danilov-av2004@mail.ru>
2025-07-08 00:14:36 +0300

    task 4:fixed reviewdog try 2

--- app/src/main/java/ru/practicum/android/diploma/data/db/AppDatabase.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/AppDatabase.kt
@@ -2,7 +2,6 @@ package ru.practicum.android.diploma.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-
 import ru.practicum.android.diploma.data.db.dao.FavouriteVacancyDao
 import ru.practicum.android.diploma.data.db.entyties.FavouriteVacancy
 
