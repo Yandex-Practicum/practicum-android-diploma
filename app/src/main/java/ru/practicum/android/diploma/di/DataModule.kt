973031f92cc8f87c9075f4617d07235f8c8444cb
ARKADIY <danilov-av2004@mail.ru>
2025-07-07 23:47:36 +0300

    task 4: added database (favouriteVacancy)

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,18 +1,29 @@
 package ru.practicum.android.diploma.di
 
+import androidx.room.Room
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.data.db.AppDatabase
 import ru.practicum.android.diploma.data.models.areas.AreasApi
 import ru.practicum.android.diploma.data.models.industries.IndustriesApi
 import ru.practicum.android.diploma.data.models.vacancies.VacanciesApi
 import ru.practicum.android.diploma.data.models.vacancyDetails.VacancyDetailsApi
 
 val dataModule = module {
+    // retrofit
     single<Retrofit> {
         Retrofit.Builder()
             .baseUrl("https://api.hh.ru/")
             .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    // room
+    single<AppDatabase> {
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "AppDatabase.db")
+            .fallbackToDestructiveMigration(false)
             .build()
     }
 
