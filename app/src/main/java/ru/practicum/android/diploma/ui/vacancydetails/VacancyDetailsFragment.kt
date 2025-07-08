836ae64e9ad2ff6ea877c58eb94a742f70151f46
Viktoria Kan <vikamarkiz2002@gmail.com>
2025-07-09 01:42:41 +0300

    added team screen

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancydetails/VacancyDetailsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancydetails/VacancyDetailsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.VacancyDetailsFragmentBinding
 
 class VacancyDetailsFragment : Fragment() {
@@ -15,6 +16,11 @@ class VacancyDetailsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = VacancyDetailsFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.header.toolbarTitle.text = getString(R.string.vacancy_details)
     }
 
     override fun onDestroyView() {
