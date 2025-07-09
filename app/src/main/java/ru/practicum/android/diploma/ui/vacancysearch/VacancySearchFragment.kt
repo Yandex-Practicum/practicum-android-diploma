4d3e1dd6007c7fb26b75d12979a9d897a0d41b7f
Viktoria Kan <vikamarkiz2002@gmail.com>
2025-07-10 01:23:21 +0300

    added search line, cursor and icons

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancysearch/VacancySearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancysearch/VacancySearchFragment.kt
@@ -1,9 +1,13 @@
 package ru.practicum.android.diploma.ui.vacancysearch
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
@@ -23,13 +27,39 @@ class VacancySearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.header.toolbarTitle.text = getString(R.string.search_vacancy)
+        binding.header.iconFilter.visibility = View.VISIBLE
 
         binding.details.setOnClickListener {
             findNavController().navigate(R.id.action_vacancySearchFragment_to_vacancyDetailsFragment)
         }
-        binding.filter.setOnClickListener {
+        binding.header.iconFilter.setOnClickListener {
             findNavController().navigate(R.id.action_vacancySearchFragment_to_searchFiltersFragment)
         }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s?.isNotEmpty() == true) {
+                    binding.icon.setImageResource(R.drawable.close_24px)
+                    binding.icon.isClickable = true
+                    binding.icon.setOnClickListener {
+                        binding.inputEditText.setText("")
+                        val inputMethodManager =
+                            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                        inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
+                    }
+                } else {
+                    binding.icon.setImageResource(R.drawable.search_24px)
+                    binding.icon.isClickable = false
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        binding.inputEditText.addTextChangedListener(simpleTextWatcher)
     }
 
     override fun onDestroyView() {
