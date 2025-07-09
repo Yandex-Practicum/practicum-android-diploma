dbdeed592585e8c7b841b84eb64306875cdd2cd5
MaxxySE <maximrymin15@gmail.com>
2025-07-09 23:52:14 +0400

    reviewdog edit

--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/FavoriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/FavoriteFragment.kt
@@ -1,32 +1,32 @@
-    package ru.practicum.android.diploma.favorites.ui
+package ru.practicum.android.diploma.favorites.ui
 
-    import android.os.Bundle
-    import android.view.LayoutInflater
-    import android.view.View
-    import android.view.ViewGroup
-    import androidx.fragment.app.Fragment
-    import ru.practicum.android.diploma.databinding.FragmentFavoriteBinding
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.databinding.FragmentFavoriteBinding
 
-    class FavoriteFragment : Fragment() {
+class FavoriteFragment : Fragment() {
 
-        private var _binding: FragmentFavoriteBinding? = null
-        private val binding get() = _binding!!
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
 
-        override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View {
-            _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
-            return binding.root
-        }
-
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-        }
-
-        override fun onDestroyView() {
-            super.onDestroyView()
-            _binding = null
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
