530afd1ca5c8dd3359f28526140ca697642827dc
Vsevolod Sazonov <legoman056@gmail.com>
2025-07-12 17:59:32 +0700

    Эмиты доделаны, примерная структура дата слоя готова

--- app/src/main/java/ru/practicum/android/diploma/search/data/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/repository/SearchRepositoryImpl.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.search.data.repository
 import android.util.Log
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import ru.practicum.android.diploma.search.data.model.VacanciesResponse
 import ru.practicum.android.diploma.search.data.model.VacancyRequest
 import ru.practicum.android.diploma.search.data.network.NetworkClient
 import ru.practicum.android.diploma.search.domain.api.SearchRepository
@@ -24,15 +25,26 @@ class SearchRepositoryImpl(private val networkClient: NetworkClient) : SearchRep
         when (response.resultCode) {
 
             200 -> {
-
+                val data = (response as VacanciesResponse).items.map {
+                    VacancyPreview(
+                        it.id,
+                        it.name,
+                        it.employer.name,
+                        it.salary.from ?: 0,
+                        it.salary.to ?: 0,
+                        it.salary.currency ?: "",
+                        it.employer.logoUrl ?: ""
+                    )
+                }
+                emit(Resource.Success(data))
             }
 
             404 -> {
-
+                emit(Resource.Failed("not found"))
             }
 
             -1 -> {
-
+                emit(Resource.Failed("no internet"))
             }
 
             else -> {
