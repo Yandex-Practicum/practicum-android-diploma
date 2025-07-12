c2457f4e2e2ae74f4332456fefa7254a513e8318
ARKADIY <danilov-av2004@mail.ru>
2025-07-12 15:38:50 +0300

    task-1:layout screen 'Favourite'

--- app/src/main/java/ru/practicum/android/diploma/ui/favouritevacancies/FavouriteVacanciesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/favouritevacancies/FavouriteVacanciesFragment.kt
@@ -5,14 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import androidx.lifecycle.lifecycleScope
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FavouriteVacanciesFragmentBinding
+import ru.practicum.android.diploma.presentation.models.vacancies.VacancyUiModel
+import ru.practicum.android.diploma.ui.vacancysearch.recyclerview.VacancyItemAdapter
+import ru.practicum.android.diploma.util.Debouncer
 
 class FavouriteVacanciesFragment : Fragment() {
 
     private var _binding: FavouriteVacanciesFragmentBinding? = null
     private val binding get() = _binding!!
+
+    private val vacancies = mutableListOf<VacancyUiModel>()
+    private val adapter = VacancyItemAdapter(vacancies)
+
+    private val debounce by lazy {
+        Debouncer(viewLifecycleOwner.lifecycleScope, CLICK_DEBOUNCE_DELAY)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FavouriteVacanciesFragmentBinding.inflate(inflater, container, false)
@@ -24,9 +34,7 @@ class FavouriteVacanciesFragment : Fragment() {
 
         binding.header.toolbarTitle.text = getString(R.string.favorite)
 
-        binding.details.setOnClickListener {
-            findNavController().navigate(R.id.action_favouriteVacanciesFragment_to_vacancyDetailsFragment)
-        }
+
     }
 
     override fun onDestroyView() {
@@ -34,4 +42,7 @@ class FavouriteVacanciesFragment : Fragment() {
         _binding = null
     }
 
+    companion object {
+        private const val CLICK_DEBOUNCE_DELAY = 2000L
+    }
 }
