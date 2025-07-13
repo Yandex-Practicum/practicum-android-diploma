1b443664dcf1079c91c0b1c16f0f276264a9e75f
ARKADIY <danilov-av2004@mail.ru>
2025-07-13 23:04:04 +0300

    fixed resolves

--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -3,9 +3,9 @@ package ru.practicum.android.diploma.di
 import org.koin.dsl.module
 import ru.practicum.android.diploma.domain.models.api.VacanciesInteractor
 import ru.practicum.android.diploma.domain.vacancysearchscreen.impl.VacanciesInteractorImpl
-import ru.practicum.android.diploma.domain.favouriteVacancies.impl.FavouriteVacanciesDbInteractorImpl
-import ru.practicum.android.diploma.domain.favouriteVacancies.repository.FavouriteVacanciesDbRepository
-import ru.practicum.android.diploma.domain.favouriteVacancies.use_cases.FavouriteVacanciesDbInteractor
+import ru.practicum.android.diploma.domain.favouritevacancies.impl.FavouriteVacanciesDbInteractorImpl
+import ru.practicum.android.diploma.domain.favouritevacancies.repository.FavouriteVacanciesDbRepository
+import ru.practicum.android.diploma.domain.favouritevacancies.usecases.FavouriteVacanciesDbInteractor
 
 val interactorModule = module {
     // FavouriteVacancies
--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -3,8 +3,8 @@ package ru.practicum.android.diploma.di
 import org.koin.dsl.module
 import ru.practicum.android.diploma.data.db.AppDatabase
 import ru.practicum.android.diploma.data.favouritevacancies.impl.FavouriteVacanciesDbRepositoryImpl
-import ru.practicum.android.diploma.domain.favouriteVacancies.repository.FavouriteVacanciesDbRepository
 import ru.practicum.android.diploma.data.vacancysearchscreen.impl.VacanciesRepositoryImpl
+import ru.practicum.android.diploma.domain.favouritevacancies.repository.FavouriteVacanciesDbRepository
 import ru.practicum.android.diploma.domain.models.api.VacanciesRepository
 
 val repositoryModule = module {
