8e9731243083fff8529f23efbff60a5f693a9a75
ARKADIY <danilov-av2004@mail.ru>
2025-07-13 22:54:34 +0300

    task 2:fixed resolves

--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -1,11 +1,11 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.domain.models.api.VacanciesInteractor
+import ru.practicum.android.diploma.domain.vacancysearchscreen.impl.VacanciesInteractorImpl
 import ru.practicum.android.diploma.domain.favouriteVacancies.impl.FavouriteVacanciesDbInteractorImpl
 import ru.practicum.android.diploma.domain.favouriteVacancies.repository.FavouriteVacanciesDbRepository
 import ru.practicum.android.diploma.domain.favouriteVacancies.use_cases.FavouriteVacanciesDbInteractor
-import ru.practicum.android.diploma.domain.vacancysearchscreen.impl.VacanciesInteractorImpl
-import ru.practicum.android.diploma.domain.models.api.VacanciesInteractor
 
 val interactorModule = module {
     // FavouriteVacancies
