8413849c3dbef410100f5dd74591aade29d0fe58
Anastasia Tarazevich <56476925+Anastasiyatarazevich@users.noreply.github.com>
2025-07-14 23:17:54 +0300

    Update Vacancy.kt

--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/Vacancy.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/Vacancy.kt
@@ -1,6 +1,6 @@
 package ru.practicum.android.diploma.favorites.ui
 
-//Для разметки, потом будет вынесено отсюда
+// Для разметки, потом будет вынесено отсюда
 data class Vacancy(
     val vacancyName: String,
     val companyName: String,
