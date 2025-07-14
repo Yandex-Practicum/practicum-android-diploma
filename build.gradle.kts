a75499084e6fe5f25b6e9baf6a66bfac0f0a18ae
Кристина <141417474+Kris-found@users.noreply.github.com>
2025-07-14 22:41:35 +0200

    Added plugin safeArgs Для переходов с экранов

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.4" apply false
+    id("com.android.application") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
     id("convention.detekt")
     id("com.google.devtools.ksp") version "2.1.21-2.0.1"
+}
+
+buildscript {
+    dependencies {
+        classpath(libs.navigation.safe.args.gradle.plugin)
+    }
 }
