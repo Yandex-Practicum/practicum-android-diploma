38224b91470de8dd686f534ff45f2d5dd96d9f96
Vsevolod Sazonov <legoman056@gmail.com>
2025-07-15 17:19:16 +0700

    добавил смену иконки поиска и очистки строки ввода

--- app/src/main/java/ru/practicum/android/diploma/search/presenter/MainFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/presenter/MainFragment.kt
@@ -1,12 +1,15 @@
 package ru.practicum.android.diploma.search.presenter
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentMainBinding
 import ru.practicum.android.diploma.search.domain.model.VacancyPreview
 
@@ -31,6 +34,25 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRv()
+
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                updateSearchIcon(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        }
+
+        binding.editTextId.addTextChangedListener(textWatcher)
+        clearEditText()
     }
 
     override fun onDestroyView() {
@@ -45,6 +67,27 @@ class MainFragment : Fragment() {
     private fun initRv() {
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = adapter
+    }
+
+    private fun updateSearchIcon(text: CharSequence?) {
+        if (text.isNullOrBlank()) {
+            binding.searchIcon.setImageResource(R.drawable.search_24px)
+            binding.searchIcon.tag = R.drawable.search_24px
+        } else {
+            binding.searchIcon.setImageResource(R.drawable.cross_light)
+            binding.searchIcon.tag = R.drawable.cross_light
+        }
+    }
+
+
+    private fun clearEditText() {
+        binding.searchIcon.setOnClickListener {
+            when {
+                binding.searchIcon.tag == R.drawable.cross_light -> {
+                    binding.editTextId.text.clear()
+                }
+            }
+        }
     }
 
 }
