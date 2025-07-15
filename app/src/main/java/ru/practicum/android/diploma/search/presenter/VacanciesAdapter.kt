d7d65614b2f09a2e9f65afa8f24a5d2e0fad66d1
Vsevolod Sazonov <legoman056@gmail.com>
2025-07-15 16:28:30 +0700

    добавил адаптер для rv

---
+++ app/src/main/java/ru/practicum/android/diploma/search/presenter/VacanciesAdapter.kt
@@ -0,0 +1,51 @@
+package ru.practicum.android.diploma.search.presenter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.search.domain.model.VacancyPreview
+
+class VacanciesAdapter(
+    private val context: Context,
+    private val vacanciesList: MutableList<VacancyPreview>,
+    private val onVacancyClick: (VacancyPreview) -> Unit
+) : RecyclerView.Adapter<VacanciesAdapter.VacanciesViewHolder>() {
+
+    class VacanciesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageId)
+        val titleTextView: TextView = itemView.findViewById(R.id.titleId)
+        val teamTextView: TextView = itemView.findViewById(R.id.teamId)
+        val salaryTextView: TextView = itemView.findViewById(R.id.salaryId)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VacanciesViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.vacancies_preview, parent, false)
+        return VacanciesViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return vacanciesList.size
+    }
+
+    override fun onBindViewHolder(holder: VacanciesViewHolder, position: Int) {
+
+        val vacancy = vacanciesList[position]
+        Glide.with(context)
+            .load(vacancy.url)
+            .placeholder(R.drawable.placeholder)
+            .error(R.drawable.placeholder)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .into(holder.imageView)
+
+        
+
+    }
+
+}
