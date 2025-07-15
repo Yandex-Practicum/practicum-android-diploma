99c1f98630ecfe141790e61d4b2353012bafc425
Vsevolod Sazonov <legoman056@gmail.com>
2025-07-15 16:48:59 +0700

    инициализация rv

--- app/src/main/java/ru/practicum/android/diploma/search/presenter/MainFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/presenter/MainFragment.kt
@@ -5,12 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import ru.practicum.android.diploma.databinding.FragmentMainBinding
+import ru.practicum.android.diploma.search.domain.model.VacancyPreview
 
 class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+    private val adapter by lazy {
+        VacanciesAdapter(requireContext(), mutableListOf(), ::onVacancyClick)
+    }
+    private val recyclerView: RecyclerView get() = binding.vacanciesRvId
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,10 +30,21 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initRv()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun onVacancyClick(preview: VacancyPreview) {
+
+    }
+
+    private fun initRv() {
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = adapter
+    }
+
 }
