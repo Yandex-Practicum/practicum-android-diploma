1bbb1e0c2ebcc86a2515fff133c7da64f2406739
Vsevolod Sazonov <legoman056@gmail.com>
2025-07-15 16:30:31 +0700

    добавил обработчик клика

--- app/src/main/java/ru/practicum/android/diploma/search/presenter/VacanciesAdapter.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/presenter/VacanciesAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -23,6 +24,7 @@ class VacanciesAdapter(
         val titleTextView: TextView = itemView.findViewById(R.id.titleId)
         val teamTextView: TextView = itemView.findViewById(R.id.teamId)
         val salaryTextView: TextView = itemView.findViewById(R.id.salaryId)
+        val vacancyContainer: ConstraintLayout = itemView.findViewById(R.id.vacancyContainerId)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VacanciesViewHolder {
@@ -44,7 +46,9 @@ class VacanciesAdapter(
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(holder.imageView)
 
-        
+        holder.vacancyContainer.setOnClickListener {
+            onVacancyClick(vacancy)
+        }
 
     }
 
