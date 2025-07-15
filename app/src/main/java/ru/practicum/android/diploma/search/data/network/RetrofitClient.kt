78b9ee5da23b6edbaf0896a2f2fc7f60ac58ce2e
Vsevolod Sazonov <legoman056@gmail.com>
2025-07-16 00:57:00 +0700

    добавил логирование с сервера

--- app/src/main/java/ru/practicum/android/diploma/search/data/network/RetrofitClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/network/RetrofitClient.kt
@@ -1,5 +1,7 @@
 package ru.practicum.android.diploma.search.data.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,11 +11,20 @@ object RetrofitClient {
         Retrofit.Builder()
             .baseUrl("https://api.hh.ru/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okhttpClient)
             .build()
     }
 
     val hhApi: HHApi by lazy {
         client.create(HHApi::class.java)
     }
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val okhttpClient = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
 
 }
