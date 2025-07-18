201d1a169056cb8e1e84eb3bee34f3b2d26bb1c9
ARKADIY <danilov-av2004@mail.ru>
2025-07-18 12:36:52 +0300

    fixed review dog

--- app/src/main/java/ru/practicum/android/diploma/data/db/converter/TypeConverter.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/converter/TypeConverter.kt
@@ -2,7 +2,6 @@ package ru.practicum.android.diploma.data.db.converter
 
 import androidx.room.TypeConverter
 import com.google.gson.Gson
-import com.google.gson.JsonObject
 import com.google.gson.reflect.TypeToken
 import org.json.JSONException
 import org.json.JSONObject
@@ -18,23 +17,23 @@ class TypeConverter {
 
         when (salary) {
             is Salary.NotSpecifies -> {
-                json.put("type", "NotSpecifies")
+                json.put(TYPE, "NotSpecifies")
             }
             is Salary.From -> {
-                json.put("type", "From")
-                json.put("amount", salary.amount)
-                json.put("currency", salary.currency)
+                json.put(TYPE, "From")
+                json.put(AMOUNT, salary.amount)
+                json.put(CURRENCY, salary.currency)
             }
             is Salary.Range -> {
-                json.put("type", "Range")
-                json.put("from", salary.from)
-                json.put("to", salary.to)
-                json.put("currency", salary.currency)
+                json.put(TYPE, "Range")
+                json.put(FROM, salary.from)
+                json.put(TO, salary.to)
+                json.put(CURRENCY, salary.currency)
             }
             is Salary.Fixed -> {
-                json.put("type", "Fixed")
-                json.put("amount", salary.amount)
-                json.put("currency", salary.currency)
+                json.put(TYPE, "Fixed")
+                json.put(AMOUNT, salary.amount)
+                json.put(CURRENCY, salary.currency)
             }
         }
         return json.toString()
@@ -43,21 +42,21 @@ class TypeConverter {
     @TypeConverter
     fun toSalary(json: String): Salary {
         val obj = JSONObject(json)
-        val type = obj.optString("type", null) ?: throw JSONException("Missing 'type' field in Salary JSON")
+        val type = obj.optString(TYPE, null) ?: throw JSONException("Missing 'type' field in Salary JSON")
         return when (type) {
             "NotSpecifies" -> Salary.NotSpecifies
             "From" -> Salary.From(
-                obj.getInt("amount"),
-                obj.getString("currency")
+                obj.getInt(AMOUNT),
+                obj.getString(CURRENCY)
             )
             "Range" -> Salary.Range(
-                obj.getInt("from"),
-                obj.getInt("to"),
-                obj.getString("currency")
+                obj.getInt(FROM),
+                obj.getInt(TO),
+                obj.getString(CURRENCY)
             )
             "Fixed" -> Salary.Fixed(
-                obj.getInt("amount"),
-                obj.getString("currency")
+                obj.getInt(AMOUNT),
+                obj.getString(CURRENCY)
             )
             else -> throw IllegalArgumentException("Unknown salary type")
         }
@@ -86,5 +85,13 @@ class TypeConverter {
             val type = object : TypeToken<List<String>>() {}.type
             gson.fromJson<List<String>>(it, type)
         }
+    }
+
+    companion object {
+        private const val TYPE = "type"
+        private const val AMOUNT = "amount"
+        private const val CURRENCY = "currency"
+        private const val TO = "to"
+        private const val FROM = "from"
     }
 }
