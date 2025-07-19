cd0e2c5c995d71b6bb95a943503b3dbf9c9306ee
Sergey Kravtsov <159822285+KravtsovSO64@users.noreply.github.com>
2025-07-19 23:20:26 +0300

    Update RootActivity.kt

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.practicum.android.diploma.R
 
-class RootActivity: AppCompatActivity() {
+class RootActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
     }
