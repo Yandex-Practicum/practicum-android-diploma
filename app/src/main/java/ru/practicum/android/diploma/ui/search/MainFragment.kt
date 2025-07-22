b3d2acba287763a515b548c00cd7a4981876c50c
Sergey Kravtsov <159822285+KravtsovSO64@users.noreply.github.com>
2025-07-22 21:41:03 +0300

    Update MainFragment.kt

--- app/src/main/java/ru/practicum/android/diploma/ui/search/MainFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/MainFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.databinding.FragmentMainBinding
 
-class MainFragment: Fragment() {
+class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
