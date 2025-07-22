0cfa3c3a37f8c916a0a29e1e429c253b1cf9ac74
Sergey Kravtsov <159822285+KravtsovSO64@users.noreply.github.com>
2025-07-22 21:37:50 +0300

    Update FavouriteFragment.kt

--- app/src/main/java/ru/practicum/android/diploma/ui/favourite/FavouriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/favourite/FavouriteFragment.kt
@@ -13,14 +13,17 @@ class FavouriteFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater, 
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentFavouriteBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
     }
