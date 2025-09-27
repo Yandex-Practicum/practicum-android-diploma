2f6dea8e96b3dcfabd1c4ba30da72b08a1613905
Mutta885 <mutta885@gmail.com>
2025-09-27 05:18:21 +0300

    Настроил AndroidManifest.xml, добавил основные библиотеки.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("ru.practicum.android.diploma.plugins.developproperties")
+    id("com.google.devtools.ksp")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -36,12 +38,39 @@ android {
 
     buildFeatures {
         buildConfig = true
+        viewBinding = true
     }
 }
 
 dependencies {
     implementation(libs.androidX.core)
     implementation(libs.androidX.appCompat)
+
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+    ksp("com.github.bumptech.glide:compiler:4.16.0")
+
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+
+    val roomVersion = "2.7.2"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    ksp("androidx.room:room-compiler:2.7.2")
+    implementation("androidx.room:room-ktx:$roomVersion")
+
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    implementation("io.insert-koin:koin-android:4.0.1")
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.5")
+
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
+
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
     // UI layer libraries
     implementation(libs.ui.material)
