c606fb8c34f9a7e314c632bcb2a2fb1e4850e122
aynur <aynur@yandex.ru>
2025-11-25 12:16:17 +0300

    feat: добавление базовых страниц и правка theme

---
+++ app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
@@ -0,0 +1,24 @@
+package ru.practicum.android.diploma.root.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
+
+class RootActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRootBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.containerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+}
