a96640ddc7272f276151117bf82b543113e002b7
German <calletutor@gmail.com>
2025-12-20 12:21:12 +0300

    Добавил color.kt, theme.kt, typography.kt

---
+++ app/src/main/java/ru/practicum/android/diploma/theme/color.kt
@@ -0,0 +1,11 @@
+package ru.practicum.android.diploma.theme
+
+import androidx.compose.ui.graphics.Color
+
+val black = Color(0xFF1A1B22)
+val white = Color(0xFFFDFDFD)
+val blue = Color(0xFF3772E7)
+val red = Color(0xFFF56B6C)
+val darkGrey = Color(0x801A1B22)
+val grey = Color(0xFFAEAFB4)
+val lightGrey = Color(0xFFE6E8EB)
