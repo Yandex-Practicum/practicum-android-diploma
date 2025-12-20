f7cabda1d847ee993dc805460825bfd444a17d23
German <calletutor@gmail.com>
2025-12-20 12:41:58 +0300

    Добавил константы для цветов (detekt)

--- app/src/main/java/ru/practicum/android/diploma/theme/Сolor.kt
+++ app/src/main/java/ru/practicum/android/diploma/theme/Сolor.kt
@@ -2,10 +2,18 @@ package ru.practicum.android.diploma.theme
 
 import androidx.compose.ui.graphics.Color
 
-val black = Color(0xFF1A1B22)
-val white = Color(0xFFFDFDFD)
-val blue = Color(0xFF3772E7)
-val red = Color(0xFFF56B6C)
-val darkGrey = Color(0x801A1B22)
-val grey = Color(0xFFAEAFB4)
-val lightGrey = Color(0xFFE6E8EB)
+const val BLACK = 0xFF1A1B22
+const val WHITE = 0xFFFDFDFD
+const val BLUE = 0xFF3772E7
+const val RED = 0xFFF56B6C
+const val DARK_GREY = 0x801A1B22
+const val GREY = 0xFFAEAFB4
+const val LIGHT_GREY = 0xFFE6E8EB
+
+val black = Color(BLACK)
+val white = Color(WHITE)
+val blue = Color(BLUE)
+val red = Color(RED)
+val darkGrey = Color(DARK_GREY)
+val grey = Color(GREY)
+val lightGrey = Color(LIGHT_GREY)
