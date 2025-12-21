db33e20d83cf1f89b5b065cabd8e259d1ed2be3f
Gleb Rudko <glebrudko@inbox.ru>
2025-12-21 12:47:41 +0300

    add retrofit, network client, repository, interactor

--- app/src/main/java/ru/practicum/android/diploma/search/utils/NetworkConnectionChecker.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/utils/NetworkConnectionChecker.kt
@@ -15,9 +15,9 @@ class NetworkConnectionChecker(private val appContext: Context) {
         val network = connectivityManager.activeNetwork ?: return false
         val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
         return capabilities.run {
-            hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
-                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
-                    hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+                hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
         }
     }
 }
