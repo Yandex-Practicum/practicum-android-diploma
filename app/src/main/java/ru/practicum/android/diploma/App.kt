2985ed2bafba8dfb8d57dacd6d49a1c4d00b318a
German <calletutor@gmail.com>
2025-12-22 23:55:31 +0300

    для проверки сборки на гитхаб

--- app/src/main/java/ru/practicum/android/diploma/App.kt
+++ app/src/main/java/ru/practicum/android/diploma/App.kt
@@ -2,6 +2,7 @@ package ru.practicum.android.diploma
 
 import android.app.Application
 
+// пустой комментарий для теста сборки на гитхаб
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
