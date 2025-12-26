68d5839bf0a20cf318cf72a2ab7646e6152c401c
German <calletutor@gmail.com>
2025-12-26 20:45:55 +0300

    начал верстку экрана деталей вакансии

--- app/src/main/java/ru/practicum/android/diploma/vacancy/details/presentation/ui/VacancyDetailsScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/details/presentation/ui/VacancyDetailsScreen.kt
@@ -3,31 +3,60 @@ package ru.practicum.android.diploma.vacancy.details.presentation.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import ru.practicum.android.diploma.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VacancyDetailsScreen(
     onBack: () -> Unit,
 ) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.title_vacancy_details))
+                },
+                navigationIcon = {
+                    IconButton(onClick = onBack) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Назад"
+                        )
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
         ) {
-            Text(text = stringResource(id = R.string.title_vacancy_details))
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Text(text = stringResource(id = R.string.title_vacancy_details))
 
-            Button(onClick = onBack) {
-                Text(text = "Переход назад")
+                Button(onClick = onBack) {
+                    Text(text = "Переход назад")
+                }
             }
         }
     }
