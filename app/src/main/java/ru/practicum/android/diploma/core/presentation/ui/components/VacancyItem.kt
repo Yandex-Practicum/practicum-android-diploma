907450f6bc1b1f21a295d5201d231043ef81ac17
Gleb Rudko <glebrudko@inbox.ru>
2026-01-03 03:25:53 +0300

    update coil, add viewmodel functions for textfield, update di and logo

--- app/src/main/java/ru/practicum/android/diploma/core/presentation/ui/components/VacancyItem.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/presentation/ui/components/VacancyItem.kt
@@ -1,5 +1,6 @@
 package ru.practicum.android.diploma.core.presentation.ui.components
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,25 +9,32 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import coil.compose.AsyncImage
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.imageLoader
+import coil3.network.NetworkHeaders
+import coil3.network.httpHeaders
+import coil3.request.ImageRequest
+import coil3.util.DebugLogger
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.core.presentation.ui.theme.VacancySearchAppTheme
 import ru.practicum.android.diploma.core.presentation.ui.theme.dp12
 import ru.practicum.android.diploma.core.presentation.ui.theme.dp16
-import ru.practicum.android.diploma.core.presentation.ui.theme.dp48
 
 @Composable
 fun VacancyItem(
@@ -75,19 +83,28 @@ fun VacancyItem(
 
 @Composable
 private fun VacancyLogo(logoUrl: String?) {
-    if (!logoUrl.isNullOrEmpty()) {
-        AsyncImage(
-            model = logoUrl,
-            contentDescription = null,
-            modifier = Modifier.size(dp48)
-        )
-    } else {
-        Icon(
-            painter = painterResource(R.drawable.logo_placeholder),
-            contentDescription = null,
-            tint = Color.Unspecified
-        )
-    }
+    val imageLoader = LocalContext.current.imageLoader.newBuilder()
+        .logger(DebugLogger())
+        .build()
+    val url = logoUrl?.indexOfLast { it == '/' }?.plus(1) ?: 0
+    val header = NetworkHeaders.Builder().set("User-Agent", "Mozilla/5.0").build()
+    val imageRequest = ImageRequest
+        .Builder(LocalContext.current)
+        .data(logoUrl)
+        .httpHeaders(header)
+        .build()
+    AsyncImage(
+        model = imageRequest,
+        contentDescription = null,
+        imageLoader = imageLoader,
+        modifier = Modifier
+            .clip(RoundedCornerShape(12.dp))
+            .border(1.dp, MaterialTheme.colorScheme.surfaceVariant, RoundedCornerShape(12.dp))
+            .size(48.dp),
+        contentScale = ContentScale.Crop,
+        placeholder = painterResource(R.drawable.logo_placeholder),
+        error = painterResource(R.drawable.logo_placeholder)
+    )
 }
 
 @Composable
