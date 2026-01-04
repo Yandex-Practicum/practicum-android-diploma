b8ef37827680f6d8b47b548780c423aa7eb50e51
SemyonKardashin <kardashin.s.n@gmail.com>
2026-01-04 17:25:59 +0500

    Test branch. Поправил прогресс-бар при пагинации

--- app/src/main/java/ru/practicum/android/diploma/core/presentation/ui/components/VacanciesList.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/presentation/ui/components/VacanciesList.kt
@@ -1,6 +1,8 @@
 package ru.practicum.android.diploma.core.presentation.ui.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -9,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -61,7 +64,14 @@ fun VacanciesList(
             )
         }
         if (isLoading) {
-            item { Loading(Modifier.size(80.dp)) }
+            item {
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Loading(Modifier.size(80.dp))
+                }
+            }
         }
     }
 
