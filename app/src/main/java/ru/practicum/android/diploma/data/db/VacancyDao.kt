2e88d1b71783cd1e166181d8762490f49c7e11a4
VasilyKoshlakov <KoshlakovVV@yandex.ru>
2026-01-26 14:21:40 +0300

    Epic 0. Task 4. Entity + DAO. fixes5

---
+++ app/src/main/java/ru/practicum/android/diploma/data/db/VacancyDao.kt
@@ -0,0 +1,22 @@
+package ru.practicum.android.diploma.data.db
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import ru.practicum.android.diploma.data.db.VacancyEntity
+
+@Dao
+interface VacancyDao {
+
+    @Delete
+    suspend fun deleteVacancy(vacancy: VacancyEntity)
+
+    @Query("SELECT * FROM favorite_vacancies WHERE id = :id")
+    suspend fun getVacancyById(id: String): VacancyEntity?
+
+    @Query("SELECT COUNT(*) FROM favorite_vacancies WHERE id = :id")
+    suspend fun isFavorite(id: String): Int
+
+    @Query("DELETE FROM favorite_vacancies WHERE id = :id")
+    suspend fun deleteById(id: String)
+}
