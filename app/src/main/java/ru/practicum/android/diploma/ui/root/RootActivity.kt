7ad94314f34804d2edb410bcb71ec5bf32ab6c10
MaxZlorad <zlorad@yinbox.ru>
2026-01-27 13:56:24 +0400

    Task10 корректировки

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.BuildConfig
@@ -15,6 +17,7 @@ class RootActivity : AppCompatActivity() {
 
     private var _binding: ActivityRootBinding? = null
     private val binding get() = _binding!!
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +35,7 @@ class RootActivity : AppCompatActivity() {
         // Получаем NavController из NavHostFragment
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
         // Устанавливаем toolbar как ActionBar
         setSupportActionBar(binding.toolbar)
@@ -40,44 +43,43 @@ class RootActivity : AppCompatActivity() {
         // Связываем BottomNavigationView с NavController
         binding.bottomNavigationView.setupWithNavController(navController)
 
-        // Управление видимостью нижнего меню и toolbar (+ начальная title)
-        fun updateUi(destinationId: Int?) {
-            // Заголовок из nav_graph.xml
-            navController.addOnDestinationChangedListener { _, destination, _ ->
-                // Заголовок из nav_graph.xml
-                val currentDestination = navController.currentDestination
-                binding.toolbar.title = currentDestination?.label ?: getString(R.string.app_name)
-
-                val isBottomNavVisible = when (destination.id) {
-                    R.id.searchFragment,
-                    R.id.favoritesFragment,
-                    R.id.teamFragment -> true
-                    else -> false
-                }
-
-                // Управление иконкой "назад"
-                if (isBottomNavVisible) {
-                    // Только текст
-                    binding.toolbar.navigationIcon = null
-                } else {
-                    // + стрелка назад
-                    binding.toolbar.setNavigationIcon(R.drawable.ic_arrow_back_24)
-                    binding.toolbar.setNavigationOnClickListener { navController.navigateUp() }
-                }
-
-                // Видимость нижнего меню и разделителя
-                binding.bottomNavigationView.isVisible = isBottomNavVisible
-                binding.dividerLine.isVisible = isBottomNavVisible
-            }
-        }
-
         // Слушаем переключение экранов
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            updateUi(destination.id)
+            updateUi(destination)
         }
 
         // Применяем сразу для стартового экрана (после полной готовности)
-        binding.root.post { updateUi(navController.currentDestination?.id) }
+        binding.root.post {
+            navController.currentDestination?.let { updateUi(it) }
+        }
+    }
+
+    // Управление видимостью нижнего меню и toolbar (+ начальная title)
+    private fun updateUi(destination: NavDestination) {
+        // Заголовок из nav_graph.xml
+        binding.toolbar.title = destination.label ?: getString(R.string.app_name)
+
+        val isBottomNavVisible = when (destination.id) {
+            R.id.searchFragment,
+            R.id.favoritesFragment,
+            R.id.teamFragment -> true
+            else -> false
+        }
+
+        // Управление иконкой "назад"
+        if (isBottomNavVisible) {
+            // Только текст
+            binding.toolbar.navigationIcon = null
+        } else {
+            // + стрелка назад
+            binding.toolbar.setNavigationIcon(R.drawable.ic_arrow_back_24)
+            binding.toolbar.setNavigationOnClickListener {
+                navController.navigateUp() }
+        }
+
+        // Видимость нижнего меню и разделителя
+        binding.bottomNavigationView.isVisible = isBottomNavVisible
+        binding.dividerLine.isVisible = isBottomNavVisible
     }
 
     private fun networkRequestExample(accessToken: String) {
