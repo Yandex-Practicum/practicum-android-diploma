1d0347dab3d7297874cd62f69056314723fc14cd
Andrei Sharov <sharov89@gmail.com>
2026-01-29 02:49:09 +0300

    ep1 task2 vacancies request data/domain layer fixing detekt

--- app/src/main/java/ru/practicum/android/diploma/domain/impl/SearchVacanciesInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/impl/SearchVacanciesInteractorImpl.kt
@@ -1,7 +1,6 @@
 package ru.practicum.android.diploma.domain.impl
 
 import kotlinx.coroutines.flow.Flow
-
 import ru.practicum.android.diploma.domain.api.SearchVacanciesInteractor
 import ru.practicum.android.diploma.domain.api.SearchVacanciesRepository
 import ru.practicum.android.diploma.domain.models.Vacancy
@@ -11,6 +10,7 @@ class SearchVacanciesInteractorImpl(private val repository: SearchVacanciesRepos
     override suspend fun searchVacancies(expression: String): Flow<SearchVacanciesInteractor.Resource<List<Vacancy>>> {
         return repository.searchVacancies(sanitizeText(expression))
     }
+
     private fun sanitizeText(text: String): String {
         return text.replace(Regex("[^\\p{L}\\p{N}.&\\-'/ ]+"), "")
             .replace(Regex("\\s+"), " ")
