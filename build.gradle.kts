36a089a22d29d11ef06aad6430d04b0f0a2344b8
elkabelaya <elkabelaya@github.com>
2026-05-14 22:25:02 +0300

    #2 Настройка build.gradle-файлов и AndroidManifest.xml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.12.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
     id("convention.detekt")
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.devtools.ksp) apply false
 }
