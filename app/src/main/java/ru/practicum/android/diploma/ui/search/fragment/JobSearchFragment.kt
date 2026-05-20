1c3b228282d522d46e9055f4eadf44b7c774e5d9
valtherys <melikhovainna@gmail.com>
2026-05-20 23:01:08 +0300

    feat: добавила элементы выдачи непустого воискового запроса [задача 16]

--- app/src/main/java/ru/practicum/android/diploma/ui/search/fragment/JobSearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/fragment/JobSearchFragment.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.presentation.search.viewmodel.JobSearchViewModel
 import ru.practicum.android.diploma.ui.search.screen.JobSearchScreen
 import ru.practicum.android.diploma.ui.theme.AppTheme
 
 class JobSearchFragment : Fragment() {
+    private val viewModel: JobSearchViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,9 +27,20 @@ class JobSearchFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+
             setContent {
                 AppTheme {
-                    JobSearchScreen()
+                    val state = viewModel.vacancies.collectAsState().value
+
+                    JobSearchScreen(
+                        modifier = Modifier.padding(
+                            horizontal = 16.dp
+                        ),
+                        vacancies = state,
+                        onClick = {
+                            findNavController().navigate(R.id.action_jobSearchFragment_to_vacancyFragment)
+                        }
+                    )
                 }
             }
         }
