4f1de537506c229343216689f7ff68fcb405fe00
elkabelaya <elkabelaya@github.com>
2026-05-20 10:32:53 +0300

    #16 Search: Screen layout, input and list item UI

---
+++ app/src/main/java/ru/practicum/android/diploma/core/ui/utils/Image.kt
@@ -0,0 +1,30 @@
+package ru.practicum.android.diploma.core.ui.utils
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+
+@Composable
+fun Image(
+    painter: Painter,
+    modifier: Modifier = Modifier,
+    alignment: Alignment = Alignment.Center,
+    contentScale: ContentScale = ContentScale.Fit,
+    alpha: Float = DefaultAlpha,
+    colorFilter: ColorFilter? = null,
+) {
+    Image(
+        painter,
+        null,
+        modifier,
+        alignment,
+        contentScale,
+        alpha,
+        colorFilter
+    )
+}
--- app/src/main/java/ru/practicum/android/diploma/search/di/SearchModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/di/SearchModule.kt
@@ -2,11 +2,15 @@ package ru.practicum.android.diploma.search.di
 
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
+import ru.practicum.android.diploma.core.domain.mocks.mockList
+import ru.practicum.android.diploma.core.domain.models.Vacancy
 import ru.practicum.android.diploma.search.ui.SearchViewModel
-import ru.practicum.android.diploma.search.ui.SearchViewModelImpl
+import ru.practicum.android.diploma.search.ui.SearchViewState
+import ru.practicum.android.diploma.search.ui.mock.SearchViewModelMock
 
 val searchModule = module {
     viewModel<SearchViewModel> {
-        SearchViewModelImpl()
+        // SearchViewModelImpl() //TODO temporary mock
+        SearchViewModelMock(mockState = SearchViewState.Data(Vacancy.mockList()))
     }
 }
