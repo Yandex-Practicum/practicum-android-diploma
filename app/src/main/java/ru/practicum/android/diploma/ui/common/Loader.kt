b5d245df0098b8c4c00b5c7ecbb82628a658485b
valtherys <melikhovainna@gmail.com>
2026-05-21 14:33:29 +0300

    Merge develop into feat_16

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/common/Loader.kt
@@ -0,0 +1,30 @@
+package ru.practicum.android.diploma.ui.common
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.practicum.android.diploma.ui.theme.AppTheme
+
+@Composable
+fun Loader(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxWidth(), contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun LoaderPreview() {
+    AppTheme {
+        Loader()
+    }
+}
