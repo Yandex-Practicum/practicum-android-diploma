73bc4073191088717845555c27b34e93d7ca8e12
Александр <rainfire491@gmail.com>
2026-05-22 15:17:39 +1100

    rm import annotations.SerializedName

--- app/src/main/java/ru/practicum/android/diploma/search/data/dto/EmployerDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/dto/EmployerDto.kt
@@ -1,7 +1,5 @@
 package ru.practicum.android.diploma.search.data.dto
 
-import com.google.gson.annotations.SerializedName
-
 data class EmployerDto (
     private val id: String,
     private val name: String,
