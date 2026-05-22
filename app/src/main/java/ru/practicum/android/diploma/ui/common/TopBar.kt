edd1025b96e65d4f8da8ac9f470828fb0b081441
masha.kasatkina <mariia.kasatkina@dif.tech>
2026-05-22 13:23:55 +0400

    done

--- app/src/main/java/ru/practicum/android/diploma/ui/common/TopBar.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/common/TopBar.kt
@@ -1,17 +1,22 @@
 package ru.practicum.android.diploma.ui.common
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -23,19 +28,22 @@ import ru.practicum.android.diploma.R
 @Composable
 fun TopBar(
     text: String,
+    navIconVisible: Boolean,
+    endFirstIconVisible: Boolean,
+    endFirstIconId: Int = R.drawable.ic_share,
+    endSecondIconVisible: Boolean,
+    endSecondIconId: Int = R.drawable.ic_like_empty
 ) {
     Row(
         modifier = Modifier
+            .fillMaxWidth()
             .windowInsetsPadding(WindowInsets.statusBars)
-            .height(64.dp)
+            .height(64.dp),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
-        Image(
-            painter = androidx.compose.ui.res.painterResource(id = R.drawable.ic_back),
-            contentDescription = null,
-            modifier = Modifier
-                .align(Alignment.CenterVertically)
-                .padding(start = 12.dp)
-        )
+        if(navIconVisible) {
+            IconImage(R.drawable.ic_back)
+        }
         Text(
             text = text,
             style = TextStyle(
@@ -46,7 +54,34 @@ fun TopBar(
                 fontSize = 22.sp,
                 fontWeight = FontWeight(500)
             ),
-            modifier = Modifier.align(Alignment.CenterVertically).padding(start = 12.dp),
+            modifier = Modifier.padding(start = 12.dp),
+        )
+        if (endFirstIconVisible || endSecondIconVisible) {
+            Spacer(modifier = Modifier.weight(1f))
+        }
+        if (endFirstIconVisible) {
+            IconImage(resId = endFirstIconId)
+        }
+        if (endSecondIconVisible) {
+            IconImage(resId = endSecondIconId)
+        }
+    }
+}
+
+@Composable
+fun IconImage(
+    resId: Int,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .height(48.dp)
+            .width(48.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        Image(
+            painter = painterResource(id = resId),
+            contentDescription = null,
         )
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/search/SearchScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/SearchScreen.kt
@@ -1,20 +1,15 @@
 package ru.practicum.android.diploma.ui.search
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
@@ -24,19 +19,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
@@ -49,43 +40,51 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.ui.common.ErrorChip
+import ru.practicum.android.diploma.ui.common.IconImage
 import ru.practicum.android.diploma.ui.common.TopBar
+import ru.practicum.android.diploma.ui.theme.AppTheme
 import ru.practicum.android.diploma.ui.theme.Blue
 
 @Composable
 fun SearchScreen(
-    viewModel: VacancySearchViewModel,
-    onItemClick: (Vacancy) -> Unit,
+    searchText: String = "",
+    errorVisible: Boolean = false,
+//  viewModel: VacancySearchViewModel,
+//  onItemClick: (Vacancy) -> Unit,
 ) {
-
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusRequester = remember { FocusRequester() }
-    val uiState by viewModel.uiState.collectAsState()
+    val isPreview = LocalInspectionMode.current
+//  val uiState by viewModel.uiState.collectAsState()
     val interactionSource = remember { MutableInteractionSource() }
     val searchFieldStyle = TextStyle(
         color = MaterialTheme.colorScheme.onBackground,
-        fontFamily = FontFamily(Font(R.font.ys_display_medium)),
+        fontFamily = FontFamily(Font(R.font.ys_display_regular)),
         fontSize = 19.sp,
         fontWeight = FontWeight(400)
     )
+    val isSearchTextEmpty = searchText.isEmpty()
 
     LaunchedEffect(Unit) {
-        focusRequester.requestFocus()
+        if (!isPreview) {
+            focusRequester.requestFocus()
+        }
     }
 
     Scaffold(
         topBar = {
             TopBar(
-                text = stringResource(R.string.search_screen_title)
+                text = stringResource(R.string.search_screen_title),
+                navIconVisible = true,
+                endFirstIconVisible = true,
+                endSecondIconVisible = true
             )
         }
     ) { paddingValues ->
         Column(
-            modifier = Modifier
-                .padding(paddingValues)
+            modifier = Modifier.padding(paddingValues)
         ) {
-            val corner = dimensionResource(R.dimen.search_text_edit_corner_radius)
-            val fieldShape = RoundedCornerShape(corner)
+            val fieldShape = RoundedCornerShape(8.dp)
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -95,20 +94,15 @@ fun SearchScreen(
                     .background(MaterialTheme.colorScheme.surfaceContainer),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Image(
-                    painter = painterResource(R.drawable.ic_search),
-                    contentDescription = null,
-                    modifier = Modifier.padding(start = 15.dp)
-                )
-                Spacer(Modifier.width(8.dp))
                 BasicTextField(
-                    value = uiState.text,
+                    value = searchText,//uiState.text,
                     onValueChange = { newText ->
-                       onSearchTextChange(newText)
+                      // onSearchTextChange(newText)
                     },
                     modifier = Modifier
                         .weight(1f)
                         .fillMaxHeight()
+                        .padding(start = 20.dp)
                         .focusRequester(focusRequester),
                     singleLine = true,
                     textStyle = searchFieldStyle,
@@ -126,48 +120,49 @@ fun SearchScreen(
                             Modifier.fillMaxSize(),
                             contentAlignment = Alignment.CenterStart
                         ) {
-                            if (uiState.text.isEmpty()) {
+                            if (isSearchTextEmpty) {//(uiState.text.isEmpty()) {
                                 Text(
                                     text = stringResource(R.string.search_input_hint),
-                                    style = searchFieldStyle.copy(MaterialTheme.colorScheme.inverseOnSurface),
-                                    maxLines = 1
+                                    style = searchFieldStyle.copy(
+                                        color = MaterialTheme.colorScheme.inverseOnSurface
+                                    ),
+                                    maxLines = 1,
+                                    modifier = Modifier.align(Alignment.CenterStart),
                                 )
                             }
                             innerTextField()
                         }
                     }
                 )
-                if (uiState.clearIconVisibility && uiState.text.isNotEmpty()) {
-                    Image(
-                        painter = painterResource(R.drawable.ic_cross),
-                        contentDescription = null,
-                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground),
-                        modifier = Modifier
-                            .padding(start = 4.dp, end = 12.dp)
-                            .size(12.dp)
-                            .clickable {
-                                onSearchTextChange("")
-                                viewModel.setText("")
-                                viewModel.lastSearchText = ""
-                                keyboardController?.hide()
-                            }
-                    )
-                }
-                    ErrorChip(
-                        iconId = uiState.errorIcon,
-                        visible = uiState.errorVisible,
-                        textId = uiState.errorText,
-                    )
+                IconImage(
+                    resId = if (isSearchTextEmpty) R.drawable.ic_search else R.drawable.ic_cross,
+                    modifier = Modifier.padding(end = 4.dp)
+                )
             }
+            ErrorChip(
+                iconId = R.drawable.no_internet_error,//uiState.errorIcon,
+                visible = errorVisible, //uiState.errorVisible,
+                textId = R.string.no_internet_error //uiState.errorText,
+            )
         }
     }
 }
 
 @Composable
-@Preview
-fun SearchScreenPreview() = SearchScreen(
-    viewModel = VacancySearchViewModel(),
-    items = emptyList(),
-    onItemClick = {}
-)
+@Preview(showBackground = true)
+fun SearchScreenPreview() {
+    AppTheme {
+        SearchScreen()
+    }
+}
 
+@Composable
+@Preview(showBackground = true)
+fun SearchScreenErrorPreview() {
+    AppTheme {
+        SearchScreen(errorVisible = true)
+//      viewModel = VacancySearchViewModel(),
+//      items = emptyList<>(),
+//      onItemClick = {}
+    }
+}
