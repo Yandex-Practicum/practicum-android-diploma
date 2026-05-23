5ba3c9c6ee5f6681f30c2c939ddc4a657a8509b2
SERYOGA-cloud <gnedovsky.sergey.ekb@gmail.com>
2026-05-23 17:50:03 +0700

    Добавил RootActivity, BottomNavigationView и экраны-заглушки

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/screens/TeamFragment.kt
@@ -0,0 +1,39 @@
+package ru.practicum.android.diploma.ui.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+
+class TeamFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+
+            setContent {
+                TeamScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun TeamScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("Экран команды")
+    }
+}
