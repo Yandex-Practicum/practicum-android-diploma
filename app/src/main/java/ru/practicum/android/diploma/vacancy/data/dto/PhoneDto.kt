a7a1ab3d2169161f840c40423264169ce91258d8
elkabelaya <elkabelaya@github.com>
2026-05-24 14:53:36 +0300

    40 Vacancy: Data layer (API, DTOs, repository impl) + Domain + DI

---
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/data/dto/PhoneDto.kt
@@ -0,0 +1,8 @@
+package ru.practicum.android.diploma.vacancy.data.dto
+
+data class PhoneDto(
+    val country: String?,
+    val city: String?,
+    val number: String?,
+    val comment: String?
+)
