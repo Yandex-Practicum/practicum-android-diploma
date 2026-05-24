d9272e3e470066f60cf32bbdc41608e2ff46a250
masha.kasatkina <mariia.kasatkina@dif.tech>
2026-05-24 17:55:50 +0400

    feat_27: errors

--- app/src/main/java/ru/practicum/android/diploma/domain/api/VacancyDetailsInteractor.kt
+++
@@ -1,7 +0,0 @@
-package ru.practicum.android.diploma.domain.api
-
-import ru.practicum.android.diploma.domain.models.detail.GetVacancyDetailsResponse
-
-interface VacancyDetailsInteractor {
-    suspend fun getVacancyDetails(id: String): GetVacancyDetailsResponse
-}
--- app/src/main/java/ru/practicum/android/diploma/domain/models/detail/GetVacancyDetailsResponse.kt
+++
@@ -1,7 +0,0 @@
-package ru.practicum.android.diploma.domain.models.detail
-
-sealed interface GetVacancyDetailsResponse {
-    data class Success(val result: VacancyDetail) : GetVacancyDetailsResponse
-
-    data object Error : GetVacancyDetailsResponse
-}
