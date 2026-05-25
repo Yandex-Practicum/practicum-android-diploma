79bfa5003bc11abb4f5ad0a7b3f2e142322e5968
valtherys <melikhovainna@gmail.com>
2026-05-25 15:41:56 +0300

    feat: добавлены viewModel, state, actions и навигация экрана VacancyScreen [#26, 28]

--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -1,10 +1,16 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.domain.api.VacancyActionInteractor
 import ru.practicum.android.diploma.domain.impl.SearchInteractor
+import ru.practicum.android.diploma.domain.impl.VacancyActionInteractorImpl
 
 val interactorModule = module {
     factory {
         SearchInteractor(get())
+    }
+
+    single<VacancyActionInteractor> {
+        VacancyActionInteractorImpl()
     }
 }
