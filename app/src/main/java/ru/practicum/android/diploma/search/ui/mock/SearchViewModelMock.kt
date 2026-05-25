267749ef142e71b308730ff588586f29b3695f87
elkabelaya <elkabelaya@github.com>
2026-05-25 12:31:02 +0300

    #51 Foundation: Refactoring on iteraction 1 review

--- app/src/main/java/ru/practicum/android/diploma/search/ui/mock/SearchViewModelMock.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/mock/SearchViewModelMock.kt
@@ -9,20 +9,16 @@ import ru.practicum.android.diploma.search.ui.SearchViewState
 class SearchViewModelMock(
     mockState: SearchViewState,
     query: String = "",
-    showClearButton: Boolean = false,
     isFiltered: Boolean = false
 ) : SearchViewModel() {
     private val _state = MutableStateFlow<SearchViewState>(SearchViewState.Default)
     override var state: StateFlow<SearchViewState> = _state.asStateFlow()
-    private val _showClearButton = MutableStateFlow<Boolean>(false)
-    override var showClearButton: StateFlow<Boolean> = _showClearButton.asStateFlow()
     private val _isFiltered = MutableStateFlow<Boolean>(false)
     override var isFiltered: StateFlow<Boolean> = _isFiltered.asStateFlow()
     init {
         _state.value = mockState
         this.query.value = query
         _isFiltered.value = isFiltered
-        _showClearButton.value = showClearButton
     }
 
     override fun onQueryChanged(query: String) {
