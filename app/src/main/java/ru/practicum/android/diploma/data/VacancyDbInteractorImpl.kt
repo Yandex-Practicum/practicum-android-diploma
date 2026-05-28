28ae6e0a38ef56b73eeca321f144f37e425793e2
Inna Melikhova <melikhovainna@gmail.com>
2026-05-28 17:39:44 +0300

    chore: разрешение конфликтов [#45]

--- app/src/main/java/ru/practicum/android/diploma/data/VacancyDbInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/VacancyDbInteractorImpl.kt
@@ -4,19 +4,11 @@ import ru.practicum.android.diploma.domain.api.VacancyDbInteractor
 import ru.practicum.android.diploma.domain.models.VacancyDetail
 
 class VacancyDbInteractorImpl : VacancyDbInteractor {
-<<<<<<< feat_45
     override suspend fun addVacancyToFavorites(vacancy: VacancyDetail) {}
 
     override suspend fun deleteVacancyFromFavorites(vacancyId: String) {}
 
     override suspend fun checkVacancyIsFavorite(vacancyId: String): Boolean {
-=======
-    override fun addVacancyToFavorites(vacancy: VacancyDetail) {}
-
-    override fun deleteVacancyFromFavorites(vacancyId: String) {}
-
-    override fun checkVacancyIsFavorite(vacancyId: String): Boolean {
->>>>>>> develop
         return false
     }
 }
