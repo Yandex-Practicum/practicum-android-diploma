27633dbf07b088a7ff4500c1eaa610ce710ae49a
valtherys <melikhovainna@gmail.com>
2026-05-28 14:37:00 +0300

    chore: исправления detekt [#44]

--- app/src/main/java/ru/practicum/android/diploma/domain/api/VacancyDbInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/VacancyDbInteractor.kt
@@ -1,13 +1,9 @@
 package ru.practicum.android.diploma.domain.api
 
-import kotlinx.coroutines.flow.Flow
-import ru.practicum.android.diploma.domain.models.Vacancy
 import ru.practicum.android.diploma.domain.models.VacancyDetail
 
 interface VacancyDbInteractor {
     fun addVacancyToFavorites(vacancy: VacancyDetail)
     fun deleteVacancyFromFavorites(vacancyId: String)
-//    fun observeFavoriteVacancies(): Flow<Vacancy>
-//    fun observeFavoriteVacancy(vacancyId: String): Flow<VacancyDetail>
     fun checkVacancyIsFavorite(vacancyId: String): Boolean
 }
