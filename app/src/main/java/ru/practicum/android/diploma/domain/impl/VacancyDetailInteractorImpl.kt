24291f9917be5f23ad62fb74f6a542173c1062d4
masha.kasatkina <mariia.kasatkina@dif.tech>
2026-05-28 19:18:02 +0400

    feat_50: fix

--- app/src/main/java/ru/practicum/android/diploma/domain/impl/VacancyDetailInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/impl/VacancyDetailInteractorImpl.kt
@@ -1,10 +1,8 @@
 package ru.practicum.android.diploma.domain.impl
 
-import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.domain.api.VacancyDetailInteractor
 import ru.practicum.android.diploma.domain.api.VacancyDetailsRepository
 import ru.practicum.android.diploma.domain.models.GetVacancyDetailsResponse
-import ru.practicum.android.diploma.domain.models.VacancyDetail
 
 class VacancyDetailInteractorImpl(
     private val vacancyDetailsRepository: VacancyDetailsRepository,
@@ -14,21 +12,5 @@ class VacancyDetailInteractorImpl(
             return GetVacancyDetailsResponse.Error
         }
         return vacancyDetailsRepository.getVacancyDetails(id)
-    }
-
-    override fun getFavoriteVacancies(): Flow<List<VacancyDetail>> {
-        return vacancyDetailsRepository.getFavoriteVacancies()
-    }
-
-    override fun getFavoriteVacancyById(id: String): Flow<VacancyDetail> {
-        return vacancyDetailsRepository.getFavoriteVacancyById(id)
-    }
-
-    override suspend fun addVacancyToFavorites(vacancyDetail: VacancyDetail) {
-        return vacancyDetailsRepository.addVacancyToFavorites(vacancyDetail)
-    }
-
-    override suspend fun deleteVacancyFromFavorites(id: String) {
-        return vacancyDetailsRepository.deleteVacancyFromFavorites(id)
     }
 }
