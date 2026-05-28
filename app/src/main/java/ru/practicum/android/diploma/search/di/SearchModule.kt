27cd743c6326891576a8cb42ca4b2556d5a34349
roynys_pc <vadinload56@gmail.com>
2026-05-28 17:36:06 +0300

    фикс

--- app/src/main/java/ru/practicum/android/diploma/search/di/SearchModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/di/SearchModule.kt
@@ -2,22 +2,13 @@ package ru.practicum.android.diploma.search.di
 
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
-import ru.practicum.android.diploma.core.domain.models.Vacancy
-import ru.practicum.android.diploma.core.ui.preview.mockList
 import ru.practicum.android.diploma.search.data.SearchRepositoryImpl
 import ru.practicum.android.diploma.search.domain.api.SearchInteractor
 import ru.practicum.android.diploma.search.domain.api.SearchRepository
 import ru.practicum.android.diploma.search.domain.impl.SearchInteractorImpl
-import ru.practicum.android.diploma.search.ui.SearchViewModel
-import ru.practicum.android.diploma.search.ui.SearchViewState
-import ru.practicum.android.diploma.search.ui.mock.SearchViewModelMock
+import ru.practicum.android.diploma.search.ui.SearchViewModelImpl
 
 val searchModule = module {
-    viewModel<SearchViewModel> {
-        // SearchViewModelImpl() //TODO temporary mock
-        // пока не реализуем логику SearchViewModelImpl
-        SearchViewModelMock(mockState = SearchViewState.Data(Vacancy.mockList()))
-    }
 
     viewModel {
         SearchViewModelImpl(get())
@@ -27,7 +18,7 @@ val searchModule = module {
         SearchRepositoryImpl(get(), get())
     }
 
-    single<SearchInteractor> {
+    factory<SearchInteractor> {
         SearchInteractorImpl(get())
     }
 }
