81acdd59c5a4c3e335181f232461312c7056e89c
masha.kasatkina <mariia.kasatkina@dif.tech>
2026-05-29 16:00:46 +0400

    feat_50: review fix

--- app/src/main/java/ru/practicum/android/diploma/data/VacancyDbRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/VacancyDbRepositoryImpl.kt
@@ -1,26 +1,27 @@
 package ru.practicum.android.diploma.data
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
 import ru.practicum.android.diploma.data.db.dao.FavoriteVacancyDao
 import ru.practicum.android.diploma.data.db.mapper.toDb
 import ru.practicum.android.diploma.data.db.mapper.toVacancyDetail
 import ru.practicum.android.diploma.domain.api.VacancyDbRepository
 import ru.practicum.android.diploma.domain.models.VacancyDetail
-import kotlinx.coroutines.flow.map
 
 class VacancyDbRepositoryImpl(
     val dao: FavoriteVacancyDao
 ) : VacancyDbRepository {
 
     override fun observeFavoriteVacancyById(id: String): Flow<VacancyDetail> =
-        dao.observeFavoriteVacancy(id).map { entity ->
-            entity.toVacancyDetail()
-        }
+        dao.observeFavoriteVacancy(id)
+            .map { entity -> entity.toVacancyDetail() }
+            .distinctUntilChanged()
 
     override fun observeFavoriteVacancies(): Flow<List<VacancyDetail>> =
-        dao.observeFavoriteVacancies().map { entities ->
-        entities.map { it.toVacancyDetail() }
-    }
+        dao.observeFavoriteVacancies()
+            .map { entities -> entities.map { it.toVacancyDetail() } }
+            .distinctUntilChanged()
 
 
     override suspend fun addVacancyToFavorites(vacancy: VacancyDetail) {
