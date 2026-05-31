4b57926058b943d6997fe933f866ce7c0eab52d5
masha.kasatkina <mariia.kasatkina@dif.tech>
2026-05-31 13:27:58 +0400

    feat_68: two tests work fine

--- app/src/androidTest/java/ru/practicum/android/diploma/SearchScreenUiTest.kt
+++ app/src/androidTest/java/ru/practicum/android/diploma/SearchScreenUiTest.kt
@@ -9,15 +9,37 @@ import ru.practicum.android.diploma.common.BaseUiTest
 class SearchScreenUiTest: BaseUiTest() {
 
     @Test
-    fun searchScreenUiElementsTest() {
+    fun searchScreenUiElementsTest() =
         run {
             with(screens) {
                 with(searchScreen) {
                     step("Text field is diaplsyed") {
                         textField.assertIsDisplayed()
                     }
+                    step("Vacancies list is displayed") {
+                        vacanciesList.assertIsDisplayed()
+                    }
                 }
             }
         }
-    }
+
+    @Test
+    fun searchScreenVacancyClearTextEdit() =
+        run {
+            with(screens) {
+                with(searchScreen) {
+                    step("Enter text in search field") {
+                        textField.performClick()
+                        textField.performTextInput("Android")
+                    }
+                    step("Clear text in search field") {
+                        clearButton.assertIsDisplayed()
+                        clearButton.performClick()
+                    }
+                    step("Search field is empty") {
+                        textField.assertTextEquals("")
+                    }
+                }
+            }
+        }
 }
--- app/src/androidTest/java/ru/practicum/android/diploma/common/BaseUiTest.kt
+++ app/src/androidTest/java/ru/practicum/android/diploma/common/BaseUiTest.kt
@@ -1,10 +1,11 @@
 package ru.practicum.android.diploma.common
 
 import androidx.compose.ui.test.junit4.v2.createAndroidComposeRule
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import org.junit.Rule
 import ru.practicum.android.diploma.ui.root.RootActivity
 
-open class BaseUiTest {
+open class BaseUiTest : TestCase() {
 
     @get:Rule
     val composeRule = createAndroidComposeRule<RootActivity>()
