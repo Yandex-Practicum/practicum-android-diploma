f943f61caa7fa71381df67f0844908b4293c50af
denzelandrioki <denzelandrioki@gmail.com>
2026-06-03 14:51:01 +0300

    refactor_103: use FilterItem for region list rows
    Replace RegionListItem with common FilterItem per review feedback.

--- app/src/main/java/ru/practicum/android/diploma/ui/filtration/region/screen/components/RegionList.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filtration/region/screen/components/RegionList.kt
@@ -1,25 +1,18 @@
 package ru.practicum.android.diploma.ui.filtration.region.screen.components
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import ru.practicum.android.diploma.R
-import ru.practicum.android.diploma.ui.common.IconImage
+import ru.practicum.android.diploma.ui.common.FilterItem
 import ru.practicum.android.diploma.ui.filtration.region.action.ChooseRegionAction
 import ru.practicum.android.diploma.ui.filtration.region.model.RegionUi
 import ru.practicum.android.diploma.ui.theme.Dimens
 
 private val RegionListTopPadding = 8.dp
-private val RegionListItemVerticalPadding = 12.dp
 
 @Composable
 fun RegionList(
@@ -40,36 +33,10 @@ fun RegionList(
             items = regions,
             key = { region -> region.id },
         ) { region ->
-            RegionListItem(
-                region = region,
-                onClick = { onAction(ChooseRegionAction.RegionClicked(region)) },
+            FilterItem(
+                title = region.name,
+                onItemClick = { onAction(ChooseRegionAction.RegionClicked(region)) },
             )
         }
-    }
-}
-
-@Composable
-fun RegionListItem(
-    region: RegionUi,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .clickable(onClick = onClick)
-            .padding(vertical = RegionListItemVerticalPadding),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Text(
-            text = region.name,
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onBackground,
-            modifier = Modifier.weight(1f),
-        )
-        IconImage(
-            resId = R.drawable.ic_arrow_forward,
-            color = MaterialTheme.colorScheme.onBackground,
-        )
     }
 }
